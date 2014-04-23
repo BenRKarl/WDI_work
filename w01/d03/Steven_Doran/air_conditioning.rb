19e7186ba4055166e5f5bf1f784411a1bf24cfdd
pantsonsteven <pantsonsteven@gmail.com>
2014-04-23 09:43:42 -0400

    d03 morning assignment; made air_conditioning.rb

---
+++ w01/d03/Steven_Doran/air_conditioning.rb
@@ -0,0 +1,18 @@
+puts "Yo. What's the temperature?"
+curr_temp = gets.chomp.to_i
+
+puts "Word. Is the A/C working (Y/N)?"
+ac = gets.chomp.downcase
+
+puts "Rad. What temperature would you like it to be?"
+desired_temp = gets.chomp.to_i
+
+if (ac == "y" || ac == "yes") && curr_temp > desired_temp
+  puts "Turn on the A/C please."
+elsif (ac == "n" || ac == "no") && curr_temp > desired_temp
+  puts "Fix the A/C now! It's hot!"
+elsif (ac == "n" || ac == "no") && curr_temp < desired_temp
+  puts "Fix the A/C whenever you have the chance... It's cool..."
+else 
+  puts "Huh?"
+end
