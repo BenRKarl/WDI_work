72b8a8f251749337209d0a1e239f01cf558ae97c
ntuvera <ntuvera@gmail.com>
2014-04-23 13:34:21 -0400

    commit of pre-lunch files made

--- w01/d03/Nathaniel_Tuvera/air_conditioning.rb
+++ w01/d03/Nathaniel_Tuvera/air_conditioning.rb
@@ -2,16 +2,20 @@ print "What is the current temperature (in F)? "
 currtemp = gets.chomp.to_i
 print "What would you like the temperature to be (in F)? "
 settemp = gets.chomp.to_i
-print "Is the Airconditioning currently working? (Y/N)"
-status = gets.chomp.to_s
-if status = "Y" && currtemp > settemp
+print "Is the Airconditioning currently working? (y/n)"
+status = gets.chomp.downcase[0]
+if status = "y" && currtemp > settemp
   puts "Turn on the A/C, please."
-elsif status = "Y" && currtemp < settemp
+elsif status = "y" && currtemp < settemp
   puts "Turn off the A/C please."
-elsif status = "Y" && curretemp = settemp
+elsif status = "y" && curretemp = settemp
   puts "Do Nothing."
-elsif status = "N" && currtemp < settemp
+elsif status = "n" && currtemp <= settemp
   puts "Fix the A/C whenever you have the chance... It's cool..."
-elsif status = "N" && currtemp >= settemp
+elsif status = "n" && currtemp > settemp
   puts "Fix the A/C NAOW! It's hot!"
 end
+
+#def airconditiong (currtemp, settemp, status)
+#
+#end
