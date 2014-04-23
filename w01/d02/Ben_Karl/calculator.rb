62f3a7b60967683021f2419ffe23f0843bc7a3b1
Ben Karl <benrkarl@gmail.com>
2014-04-22 21:24:34 -0400

    Added functionality to advanced_calc method

--- w01/d02/Ben_Karl/calculator.rb
+++ w01/d02/Ben_Karl/calculator.rb
@@ -1,17 +1,12 @@
 # A user should be given a menu of operations
 # A user should be able to choose from the menu
 def menu
-
-  #commenting out since menu should be part of commit 3
-=begin
   puts "Would you like do to (b)asic math or (a)dvanced math?"
   choice = gets.chomp
   if choice == "b"
-  #run the basic_calc method with new variables
+  basic_calc
   else
-  #run the advanced cal method with new variables
-=end
-
+  advanced_calc
 end
 
 # A user should be able to enter numbers to perform the operation on
@@ -23,37 +18,55 @@ def basic_calc
   when choice == "a"
     puts "What would you like to add?"
     print "First number: "
-    num1 = gets.chomp.to_i
+    num1 = gets.chomp
     print "Ok, now the second number: "
-    num2 = gets.chomp.to_i
+    num2 = gets.chomp
     puts "Your maths equal #{num1 + num2}"
   when choice == "s"
     puts "What would you like to subtract?"
     print "First number: "
-    num1 = gets.chomp.to_i
+    num1 = gets.chomp
     print "Ok, now the second number: "
-    num2 = gets.chomp.to_i
+    num2 = gets.chomp
     puts "Your maths equal #{num1 - num2}"
   when choice == "m"
     puts "What would you like to multiply?"
     print "First number: "
-    num1 = gets.chomp.to_i
+    num1 = gets.chomp
     print "Ok, now the second number: "
-    num2 = gets.chomp.to_i
+    num2 = gets.chomp
     puts "Your maths equal #{num1 * num2}"
   when choice == "d"
     puts "What would you like to divide?"
     print "First number: "
-    num1 = gets.chomp.to_i
+    num1 = gets.chomp
     print "Ok, now the second number: "
-    num2 = gets.chomp.to_i
+    num2 = gets.chomp
     puts "Your maths equal #{num1 / num2}"
   end
 end
 
 def advanced_calc
   print "(p)ower, (s)qrt: "
-
+  choice = gets.chomp
+  case
+  when choice == "p"
+    puts "What would you like to exponentiate?"
+    print "Base number: "
+    num1 = gets.chomp
+    print "Raised to the power of: "
+    num2 = gets.chomp
+    puts "Your maths equal #{num1**num2}"
+  when choice == "s"
+    print "What would you like to get the square root of?: "
+    num1 = gets.chomp
+    if num1 < 0
+      puts "You can't take the square root of a negative number..."
+      advanced_calc
+    else
+    puts "The square root of #{num1} is #{Math.sqrt(num1)}"
+    end
+  end
 end
 
 response = menu
