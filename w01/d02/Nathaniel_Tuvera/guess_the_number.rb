96639b192e68271e71e0c6df440962a0e815bc9a
ntuvera <ntuvera@gmail.com>
2014-04-22 20:04:19 -0400

    part 1 and 2 of w01d02 hw

---
+++ w01/d02/Nathaniel_Tuvera/guess_the_number.rb
@@ -0,0 +1,19 @@
+puts "I want to play a game..."
+puts "I have chosen a number between 1 and 10."
+puts "Now, guess what it is."
+guess = gets.chomp.to_i
+
+answer = rand(11)
+tries = 1
+#puts answer - will remove, answer output for testing
+while guess != answer
+  if guess < answer
+    puts "a little higher"
+  else guess > answer
+    puts "a little lower"
+  end
+  puts "Keep trying... guess again."
+  tries += 1
+guess = gets.chomp.to_i
+  end
+puts "Congrats, you win. It took you #{tries}."
