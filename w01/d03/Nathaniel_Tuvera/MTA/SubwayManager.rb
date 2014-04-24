2d30851cab74bfc85596285201c350553ed4c0c7
ntuvera <ntuvera@gmail.com>
2014-04-24 08:39:49 -0400

    w01_d03_submission

--- w01/d03/Nathaniel_Tuvera/MTA/SubwayManager.rb
+++ w01/d03/Nathaniel_Tuvera/MTA/SubwayManager.rb
@@ -11,7 +11,14 @@ class SubwayManager
 
   def menu
     travel_plan = {}
-
+    puts "start_station"
+    travel_plan[:start_station] = gets.chomp
+    puts "start_train"
+    travel_plan[:start_train] = gets.chomp
+    puts "end_station"
+    travel_plan[:end_station] = gets.chomp
+    puts "end_train"
+    travel_plan[:end_train] = gets.chomp
     # prompts (puts) and allow the user to enter information (gets)
     # consider defining values keys for :start_train, :start_station, :stop_train, :stop_station
 
@@ -19,10 +26,14 @@ class SubwayManager
   end
 
   def travel_distance(travel_plan)
+      # total_length_of_trip = index value of :start_station - index value of :end_station
+      n = ['ts', '34th', '28th-n', '23rd-n', 'us']
+      # total_length_of_trip = n.index(travel_plan[":start_station"]) - n.index[":end_station"]
+      total_length_of_trip = (n.index(travel_plan[:start_station]) - n.index(travel_plan[:end_station])).abs
 
-    # Calculate the total_length_of_trip here!
-
-    return total_length_of_trip
+      # n = ['ts', '34th', '28th-n', '23rd-n', 'us']
+      # total_length_of_trip = n.index('ts') - n.index('us')
+    # return total_length_of_trip.abs
   end
 
   def help
