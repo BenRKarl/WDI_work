6f52671b7d0bd9a734c1d33d6b851a190c512afa
wakelank <wakelank@gmail.com>
2014-04-24 18:29:01 -0400

    apartment and building classes pass test

--- w01/d04/Wake_Lankard/rental_app/building.rb
+++ w01/d04/Wake_Lankard/rental_app/building.rb
@@ -1,3 +1,20 @@
 class Building
 
+  attr_accessor :address, :style, :floors, :apartments
+
+  def initialize (address, style, floors)
+    @address = address
+    @style = style
+    @floors = floors
+    @apartments = {}
+  end
+
+  def to_s
+    "This building has #{apartment_count} apartments."
+  end
+
+  def apartment_count
+    return 0 if @apartments.empty?
+    @apartments.length
+  end
 end
