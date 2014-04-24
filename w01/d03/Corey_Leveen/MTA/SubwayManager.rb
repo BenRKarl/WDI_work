24092fc4d1a795c52541089751499283a9f4f313
mntj <mntj11@gmail.com>
2014-04-24 08:19:11 -0400

    More subways

--- w01/d03/Corey_Leveen/MTA/SubwayManager.rb
+++ w01/d03/Corey_Leveen/MTA/SubwayManager.rb
@@ -7,7 +7,22 @@ class SubwayManager
     @network[:n] = n
     @network[:l] = l
     @network[:s] = s
-    n_l = []
+
+    n_lno = ['ts', '34th', '28th-n', '23rd-n', 'us', '8th', '6th']
+    n_lso = ['ts', '34th', '28th-n', '23rd-n', 'us', '3rd', '1st']
+    n_s = ['ts', '34th', '28th-n', '23rd-n', 'us', '23rd-s', '28th-s', '33rd', 'gc']
+
+    lno = ['8th', '6th', 'us']
+    lno_n = ['8th', '6th', 'us', '23rd-n', '28th-n', '34th', 'ts']
+    lno_s = []
+
+    lso = ['3rd', '1st']
+    lso_n = ['1st', '3rd', 'us', '23rd-n', '28th-n', '34th', 'ts']
+    lso_s = []
+
+    s_n = []
+    s_lno = []
+    s_lso = []
   end
 
   def menu
@@ -37,16 +52,19 @@ class SubwayManager
 
     #Get the user's desired final station
     puts "Please enter a desired station."
-    stop_station = gets.chomp
+    stop_station = gets.chomp.downcase!
 
     #Figure out what train it's on
     case stop_station
     when 'ts', '34th', '28th-n', '23rd-n'
       stop_train = n
         puts "You are going to the 'n' train."
-      when '8th', '6th', 'us', '3rd', '1st'
-        stop_train = l
-        puts "You are going to the 'l' train."
+      when '8th', '6th', 'us'
+        stop_train = lno
+        puts "You are going to the 'l' north train."
+      when '3rd', '1st'
+        stop_train = lso
+        puts "You are going to the 'l' south train."
       when 'gc', '33rd', '28th-s', '23rd-s'
         stop_train = s
         puts "You are going to the 's' train."
@@ -58,20 +76,36 @@ class SubwayManager
     if start_train == n
       if stop_train == n
         train_route = n
-      elsif stop_train == l
-        train_route = n_l
+      elsif stop_train == lno
+        train_route = n_lno
+      elsif stop_train == lso
+        train_route = n_lso
       elsif stop_train == s
         train_route = n_s
       end
     end
 
-    if start_train == l
-      if stop_train == l
+    if start_train == lno
+      if stop_train == lno
+        train_route = lno
+      elsif stop_train == lso
         train_route = l
       elsif stop_train == n
-        train_route = l_n
+        train_route = lno_n
       elsif stop_train == s
-        train_route = l_s
+        traoun_route = lno_s
+      end
+    end
+
+    if start_train == lso
+      if stop_train == lso
+        train_route = lso
+      elsif stop_train == lno
+        train_route = l
+      elsif stop_train == n
+        train_route = lso_n
+      elsif stop_train == s
+        train_route = lso_s
       end
     end
 
@@ -80,8 +114,10 @@ class SubwayManager
         train_route = s
       elsif stop_train == n
         train_route = s_n
-      elsif stop_train == l
-        train_route = s_l
+      elsif stop_train == lno
+        train_route = s_lno
+      elsif stop_train == lso
+        train_route = s_lso
       end
     end
 
