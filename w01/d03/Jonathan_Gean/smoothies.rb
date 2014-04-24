9d9e416e6e74ea6dc2b86237f4a469296cd8d639
jonathangean <jonathangean@gmail.com>
2014-04-23 21:52:12 -0400

    Your Name:JonathanGean
    Subject Line:d03
    
    What Happened:

--- w01/d03/Jonathan_Gean/smoothies.rb
+++ w01/d03/Jonathan_Gean/smoothies.rb
@@ -4,38 +4,47 @@
 # http://ruby-doc.org/core-2.0/Array.html
 
 # Every Morning I make a smoothie with the follow ingredients:
-smoothie_ingredients = {
-  'flax seeds' => '1 tbsp',
-  'chia seeds' => '1 tbsp',
-  'coconut flakes' => '1 tbsp',
-  'spirulina' => '1 tsp',
-  'pumpkin seeds' => '1 tbsp',
-  'oatmeal' => '1/4 cup',
-  'coco powder' => '1 tbsp',
-  'peanut butter' => '1 tbsp',
-  'almonds' => '1/4 cup',
-  'walnuts' => '1/4 cup',
-  'spinach' => '1/4 cup',
-  'greek yogurt' => '1/4 cup',
-  'nutrional yeast' => '1 tbsp',
-  'brussel sprouts' => '1/4 cup',
-  'asparagus' => '1/4 cup',
-  'kale' => '1/4 cup',
-  'brocoli rabe' => '1/4 cup',
-  'blue berries' => '1/4 cup',
-  'chopped banana' => '1/2 cup',
-  'straw berries' => '1/4 cup',
-  'mango' => '1/4 cup',
-  'hemp milk' => '1 cup'
-}
+
 
 # Write a function called blend.
 # It should take all the smoothie ingredients (not the measurements) and chop up and mix all the characters
 # and output a mixed string of characters
 # Be sure to remove the spaces, as we don't want any air bubbles in our smoothie!
+smoothie_ingredients = {
+    'flax seeds' => '1 tbsp',
+    'chia seeds' => '1 tbsp',
+    'coconut flakes' => '1 tbsp',
+    'spirulina' => '1 tsp',
+    'pumpkin seeds' => '1 tbsp',
+    'oatmeal' => '1/4 cup',
+    'coco powder' => '1 tbsp',
+    'peanut butter' => '1 tbsp',
+    'almonds' => '1/4 cup',
+    'walnuts' => '1/4 cup',
+    'spinach' => '1/4 cup',
+    'greek yogurt' => '1/4 cup',
+    'nutrional yeast' => '1 tbsp',
+    'brussel sprouts' => '1/4 cup',
+    'asparagus' => '1/4 cup',
+    'kale' => '1/4 cup',
+    'brocoli rabe' => '1/4 cup',
+    'blue berries' => '1/4 cup',
+    'chopped banana' => '1/2 cup',
+    'straw berries' => '1/4 cup',
+    'mango' => '1/4 cup',
+    'hemp milk' => '1 cup'
+}
+
+
+def string_shuffle(s)
+    s.split("").shuffle.join
+end
 
 def blend(smoothie_ingredients)
+    string_shuffle(smoothie_ingredients.keys.shuffle.join("").gsub(/\s+/, ""))
 end
+
+puts blend(smoothie_ingredients)
 
 
 # create a class called Blender
@@ -45,5 +54,28 @@ end
 # Blend the the smoothie array
 
 class Blender
+    attr_accessor :power
+    def initialize
+       @power = "off"
+   end
+   def blending(smoothie_ingredients)
+       if @power == "on"
+        def string_shuffle(s)
+            s.split("").shuffle.join
+        end
+
+        def blend(smoothie_ingredients)
+            string_shuffle(smoothie_ingredients.keys.shuffle.join("").gsub(/\s+/, ""))
+        end
+
+        puts blend(smoothie_ingredients)
+    elsif
+     puts "Power isn't on"
+ end
 end
+end
+blender1 = Blender.new
+blender1.blending(smoothie_ingredients)
+blender1.power = "on"
+blender1.blending(smoothie_ingredients)
 
