faf95d34fc820f2f7aca07b399fe32a50921f43e
wendy <wendy.cortese@gmail.com>
2014-04-23 23:54:32 -0400

    smoothie work

---
+++ w01/d03/Wendy_Cortese/smoothies.rb
@@ -0,0 +1,84 @@
+# docs you may enjoy
+# http://www.ruby-doc.org/core-2.0/Hash.html
+# http://ruby-doc.org/core-2.0/String.html
+# http://ruby-doc.org/core-2.0/Array.html
+
+# Every Morning I make a smoothie with the follow ingredients:
+smoothie_ingredients = {
+  'flax seeds' => '1 tbsp',
+  'chia seeds' => '1 tbsp',
+  'coconut flakes' => '1 tbsp',
+  'spirulina' => '1 tsp',
+  'pumpkin seeds' => '1 tbsp',
+  'oatmeal' => '1/4 cup',
+  'coco powder' => '1 tbsp',
+  'peanut butter' => '1 tbsp',
+  'almonds' => '1/4 cup',
+  'walnuts' => '1/4 cup',
+  'spinach' => '1/4 cup',
+  'greek yogurt' => '1/4 cup',
+  'nutrional yeast' => '1 tbsp',
+  'brussel sprouts' => '1/4 cup',
+  'asparagus' => '1/4 cup',
+  'kale' => '1/4 cup',
+  'brocoli rabe' => '1/4 cup',
+  'blue berries' => '1/4 cup',
+  'chopped banana' => '1/2 cup',
+  'straw berries' => '1/4 cup',
+  'mango' => '1/4 cup',
+  'hemp milk' => '1 cup'
+}
+
+# Write a function called blend.
+
+# It should take all the smoothie ingredients (not the measurements) and chop up and mix all the characters
+# and output a mixed string of characters
+# Be sure to remove the spaces, as we don't want any air bubbles in our smoothie!
+
+def blend(smoothie_ingredients)
+  mixed_blend = []
+  while smoothie_ingredients.length > 0
+    rand_index = rand(smoothie_ingredients.length)
+    curr_index = 0
+    new_blend = []
+
+    smoothie_ingredients.each do |ingredients|
+      if curr_index == rand_index
+        shuf.push item
+      else
+        new_blend.push ingredients
+      end
+      curr_index = curr_index + 1
+
+      def output(mixed_blend)
+        puts mixed_blend.to_s.join
+      end
+end
+
+class Blender
+
+  smoothie_arr = ["strawberry", "blueberry", "banana", "apple"]
+
+  def initalize
+    @strawberry = "strawberry"
+    @blueberry = "blueberry"
+    @banana = "banana"
+    @apple = "apple"
+  end
+
+  def smoothie_arr.each
+      puts "#{smoothie_arr}.to_s"
+       if #blender on
+       else
+      puts "turn on blender"
+  end
+
+# create a class called Blender
+# It should have a method that takes an array of ingredients and returns a mixed string of characters.
+# Give the blender an on and off switch and only allow the blender to function when it's on.
+# FOR SAFETY'S SAKE When you create a new blender by default it should be off.
+# Blend the the smoothie array
+
+
+end
+
