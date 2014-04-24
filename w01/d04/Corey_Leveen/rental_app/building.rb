91ccb083db3dc3132edf7b3ed3a26f62c3646dd3
mntj <mntj11@gmail.com>
2014-04-24 18:49:29 -0400

    apartments

--- w01/d04/Corey_Leveen/rental_app/building.rb
+++ w01/d04/Corey_Leveen/rental_app/building.rb
@@ -1,3 +1,9 @@
 class Building
+  attr_accessor :address, :style, :floors
+  def initialize(address, style, floors)
+    @address = address
+    @style = style
+    @floors = floors
+  end
 
 end
--- w01/d04/Corey_Leveen/rental_app/person.rb
+++ w01/d04/Corey_Leveen/rental_app/person.rb
@@ -1,3 +1,17 @@
 class Person
+  attr_accessor :name, :age, :income, :profile
+  def initialize(name, age, income)
+    @name = name
+    @age = age.to_i
+    @income = income.to_i
+    @profile = profile
+  end
+
+  def person
+    puts "#{name} is #{age} years old and earns $#{income} per year."
+  end
+
+
 
 end
+
