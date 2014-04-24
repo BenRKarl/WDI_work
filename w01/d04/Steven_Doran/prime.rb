279a0e3b22c05435e7314b37cb80aa21da928ce3
pantsonsteven <pantsonsteven@gmail.com>
2014-04-24 09:56:59 -0400

    morning assignemnts

--- w01/d04/Steven_Doran/prime.rb
+++ w01/d04/Steven_Doran/prime.rb
@@ -9,7 +9,21 @@ def prime?(number)
     divisor += 1
   end
 
-return true
+ true
+
+end
+
+
+#OR
+
+def prime_2?(number)
+  return false if number <= 1
+
+  for divisor in (2..Math.sqrt(number))
+    return false if number % divisor == 0
+  end
+
+  true
 
 end
 
@@ -19,3 +33,5 @@ puts "Please enter a number:"
 number = gets.chomp.to_i
   
 puts prime?(number)
+
+puts prime_2?(number)
