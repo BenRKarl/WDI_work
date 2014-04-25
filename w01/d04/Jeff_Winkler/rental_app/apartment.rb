3cd3cdf84f2b5fe5564270c7fd1a02a16faf06fa
jwink <jeff_winkler@yahoo.com>
2014-04-24 20:07:24 -0400

    rental assignment

---
+++ w01/d04/Jeff_Winkler/rental_app/apartment.rb
@@ -0,0 +1,26 @@
+
+class Apartment
+
+  attr_accessor :unit, :rent, :sqft, :bedrooms, :bathrooms, :tenants
+  def initialize(unit, rent, sqft, bedrooms, bathrooms)
+    @unit = unit
+    @rent = rent
+    @sqft = sqft
+    @bedrooms = bedrooms
+    @bathrooms = bathrooms
+    @tenants = []
+  end
+
+  def occupied?
+    if @tenants.count > 0
+      true
+    else
+      false
+    end
+  end
+
+  def to_s
+    "#{unit} costs $#{rent} a month, has #{sqft} sqft, has #{bedrooms} bedrooms and #{bathrooms} bathrooms."
+  end
+
+end
