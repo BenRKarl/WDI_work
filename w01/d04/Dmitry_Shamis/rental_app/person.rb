54b7c9c40afc08ab9ae96b2415593e9284b0505d
dshamis317 <dshamis317@gmail.com>
2014-04-24 23:58:29 -0400

    Rental_app assignment for Homework w01d04

---
+++ w01/d04/Dmitry_Shamis/rental_app/person.rb
@@ -0,0 +1,18 @@
+class Person
+  attr_accessor :name, :age, :income,
+
+  def initialize(name, age, income)
+    @name = name
+    @age = age
+    @income = income
+  end
+
+  # def new_person
+  #   new_person
+  # end
+
+  def to_s
+    return "#{@name} is #{@age} years old and earns #{@income} per year."
+  end
+
+end
