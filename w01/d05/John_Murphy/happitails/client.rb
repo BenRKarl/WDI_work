22da51d46497c10a7c6c39d736a0a69590d6dcab
jdwmurph <jdwmurph@gmail.com>
2014-04-27 19:23:44 -0400

    Week 1 weekend homework complete.

--- w01/d05/John_Murphy/happitails/client.rb
+++ w01/d05/John_Murphy/happitails/client.rb
@@ -1,4 +1,30 @@
 class Client
 
+attr_accessor :name, :age, :pets
 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @pets = {}
+  end
+
+  def to_s
+    "#{@name} is a #{@age} year old with #{@pets.count} pets"
+  end
+
+  def pet_count
+    @pets.count
+  end
+
+  def display_pets
+    @pets.each {|k,v| puts k}
+  end
+
+  def give_away_pet(name)
+    @pets.delete(name)
+  end
+
+  def accept_pet(name, something)
+    @pets[name] = something
+  end
 end
