c149996fb73aa325acbff181faf51a3071ec692b
jwink <jeff_winkler@yahoo.com>
2014-04-28 16:44:49 -0400

    End class Monday 4/28

--- w02/d01/Jeff_Winkler/hard_of_hearing/grandma.rb
+++ w02/d01/Jeff_Winkler/hard_of_hearing/grandma.rb
@@ -1,3 +1,39 @@
 class Grandma
 
+  def initialize
+    @deaf = true
+  end
+
+  def listen(input="")
+    if input == input.downcase
+      puts "WHAT's THAT? COME AGAIN, SONNY!"
+    elsif input == input.upcase
+      puts "NO, NOT SINCE 1929!"
+    else
+
+
+    end
+  end
+
+
+def loud_enough?(input)
+       ucount = 0
+       slen = input.length
+       for num in 1..slen do
+         if input[num-1]==input[num-1].upcase
+           ucount +=1
+           #puts ucount
+         end
+       end
+       if ucount > slen/2.0
+        #puts "here"
+         return true
+       else
+        return false
+      end
+  end
+
+
 end
+
+
