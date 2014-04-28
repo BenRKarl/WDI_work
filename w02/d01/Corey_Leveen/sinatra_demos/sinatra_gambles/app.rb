d1b3d07b4015ed82c90104ba6e592ac7ca83aba3
mntj <mntj11@gmail.com>
2014-04-28 16:38:46 -0400

    sinatra_gambles

--- w02/d01/Corey_Leveen/sinatra_demos/sinatra_gambles/app.rb
+++ w02/d01/Corey_Leveen/sinatra_demos/sinatra_gambles/app.rb
@@ -4,3 +4,14 @@ require 'sinatra/reloader'
 get '/' do
   erb :games
 end
+
+get '/toss' do
+  @result = ['Heads', 'Tails'].sample
+  erb :toss
+end
+
+get '/dice_roll' do
+  @result = rand(6)+1
+  erb :dice_roll
+end
+
