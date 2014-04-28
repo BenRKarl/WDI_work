75540e1436f88602c1390b90225268f799d4f744
mntj <mntj11@gmail.com>
2014-04-28 19:58:30 -0400

    more sinatra and deleted rps directory

--- w02/d01/Corey_Leveen/sinatra_demos/sinatra_gambles/app.rb
+++ w02/d01/Corey_Leveen/sinatra_demos/sinatra_gambles/app.rb
@@ -15,3 +15,36 @@ get '/dice_roll' do
   erb :dice_roll
 end
 
+get '/magic8ball/:question' do
+  @question = params[:question]
+
+  @fortune =
+  ["It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"].sample
+
+erb :magic8ball
+end
+
+get 'rps/:choice' do
+@choice = params[:choice]
+computer_choice = ["Rock", "Paper", "Scissors"].sample
+erb :rps
+end
