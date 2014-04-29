d9ceea2fe94638502da3880a937163ac333fdcd7
glonsberry <gardnerlonsberry@gmail.com>
2014-04-28 20:31:43 -0400

    still working on hw

---
+++ w02/d01/Gardner_Lonsberry/sinatra_demos/adventure_app/app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :map
+end
+
+get '/location/:local' do
+  @local = params[:local]
+  erb :location
+end
+
+get '/location/:local' do
+  @local = params[:local]
+  @found_item = ['gold', 'magic spells', 'soup'].sample
+    erb :location
+end
+
