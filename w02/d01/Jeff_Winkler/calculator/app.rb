728b23ba4cedae54b56dd8eded670d74574139bf
jwink <jeff_winkler@yahoo.com>
2014-04-28 21:24:06 -0400

    w02 d01 hwk first pass

--- w02/d01/Jeff_Winkler/calculator/app.rb
+++ w02/d01/Jeff_Winkler/calculator/app.rb
@@ -25,5 +25,22 @@ end
 
 
 get '/:function/:a/:b' do
+  @function = params[:function]
+  @num1 = params[:a].to_f
+  @num2 = params[:b].to_f
+  case @function
+  when "add"
+    @result = @num1+@num2
+  when "subtract"
+    @result = @num1-@num2
+  when "multiply"
+    @result = @num1*@num2
+  when "divide"
+    @result = @num1/@num2
+  when "square_root"
+    @result = "Please enter only one number for square root in url."
+  when "powers"
+    @result = @num1**@num2
+  end
   erb :result
 end
