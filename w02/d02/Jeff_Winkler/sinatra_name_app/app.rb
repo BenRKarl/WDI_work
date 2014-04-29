9490d156e401d03a89f429decda85e19141ed6cc
jwink <jeff_winkler@yahoo.com>
2014-04-29 12:12:26 -0400

    names app midday 4/29

---
+++ w02/d02/Jeff_Winkler/sinatra_name_app/app.rb
@@ -0,0 +1,16 @@
+
+require 'faker'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  @name = Faker::Name.name
+  erb :index
+end
+
+get '/name_list/:num_names' do
+  @names=[]
+  num_names=params[:num_names].to_i
+  num_names.times {@names << Faker::Name.name}
+  erb :names_list
+end
