5ccfa8d13fbec73d18721e5639ebe73d49a44569
mntj <mntj11@gmail.com>
2014-04-30 11:09:38 -0400

    sinatra_stuff and rectangles

--- w02/d03/Corey_Leveen/sinatra_stuff/app.rb
+++ w02/d03/Corey_Leveen/sinatra_stuff/app.rb
@@ -1,11 +1,23 @@
 require 'bundler/setup'
 Bundler.require
 
-get '/' do
-  @width = rand(400)
-  @height = rand(400)
-  @color1 = rand(255)
-  @color2 = rand(255)
-  @color3 = rand(255)
-  erb :rectangles
-end
+  get '/rectangle' do
+    @width = rand(400)
+    @height = rand(400)
+    @color1 = rand(255)
+    @color2 = rand(255)
+    @color3 = rand(255)
+    erb :rectangles
+  end
+
+  get '/rectangle/:num' do
+    @num = params[:num]
+    @width = @num
+    @height = @num
+    @color1 = @num
+    @color2 = @num
+    @color3 = @num
+    erb :rectangles
+
+  end
+
