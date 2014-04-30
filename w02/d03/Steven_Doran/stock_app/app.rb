4535b75862f417a88498343efbde69576e682d07
pantsonsteven <pantsonsteven@gmail.com>
2014-04-30 18:57:04 -0400

    stock app finished; pretty rudimentary

--- w02/d03/Steven_Doran/stock_app/app.rb
+++ w02/d03/Steven_Doran/stock_app/app.rb
@@ -1,7 +1,21 @@
 require 'bundler/setup'
 Bundler.require
 
-get '/stock/:ticker' do 
+get '/' do 
+  erb :index
+end
+
+post '/stocks' do 
   ticker = params[:ticker]
-  data = YahooFinance.historical_quotes(ticker, Time::now-(24*60*60*10), Time::now)
+  redirect "/stocks?ticker=#{ticker}"
+end
+
+
+get '/stocks' do 
+  ticker = params[:ticker]
+  data = YahooFinance.quotes([ticker], [:symbol, :ask, :name])
+  @symbol = data[0].symbol
+  @value = data[0].ask
+  @name = data[0].name
+  erb :show
 end
