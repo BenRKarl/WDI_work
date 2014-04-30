e43f1ff7b4d77e40f0f779ef73af49489384ce37
mntj <mntj11@gmail.com>
2014-04-30 16:01:06 -0400

    color_app

--- w02/d03/Corey_Leveen/post_101_name/app.rb
+++ w02/d03/Corey_Leveen/post_101_name/app.rb
@@ -4,3 +4,13 @@ Bundler.require
 get '/' do
   erb :index
 end
+
+post '/names' do
+  person_name = params[:user_name]
+  redirect "/names?being_name=#{person_name}"
+end
+
+get '/names' do
+  @name = params[:being_name]
+  erb :show
+end
