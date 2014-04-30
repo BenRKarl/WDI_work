6418f2cc6e1d103c8e6849ea8d4f0fe913547700
pantsonsteven <pantsonsteven@gmail.com>
2014-04-30 16:53:00 -0400

    afternoon files w02_d03

---
+++ w02/d03/Steven_Doran/stock_app/app.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+Bundler.require
+
+get '/stock/:ticker' do 
+  ticker = params[:ticker]
+  data = YahooFinance.historical_quotes(ticker, Time::now-(24*60*60*10), Time::now)
+end
