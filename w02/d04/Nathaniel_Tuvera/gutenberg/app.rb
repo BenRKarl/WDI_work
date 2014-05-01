4bba882aedb2ae33b58a7773b812c7257c97d919
ntuvera <ntuvera@gmail.com>
2014-05-01 17:00:42 -0400

    gutenberg

--- w02/d04/Nathaniel_Tuvera/gutenberg/app.rb
+++ w02/d04/Nathaniel_Tuvera/gutenberg/app.rb
@@ -1,15 +1,44 @@
 require 'bundler/setup'
 Bundler.require
 
+get '/id' do
+end
+
 get '/' do
   erb :root
 end
 
-get 'books/:name' do
+get '/books/:name' do
   @name = params[:name]
+  book_name = "books/#{@name}"
+  @word_counts = word_counts(book_name)
   erb :show
 end
 
-
-get '/id' do
+def word_counts(book_name)
+  # {"the" => 198003, ...}
+  word_counts = {}
+  words(book_name).each do |word|
+    if word_counts.has_key?(word)
+      word_counts[word] += 1
+    else
+      word_counts[word] = 1
+    end
+  end
+  word_counts
 end
+
+def words(book_name)
+  book_text(book_name).split(" ")
+end
+
+def book_text(book_name)
+  # File.read(book_name) do |f|
+  #   f.read
+  f = File.open(book_name, 'r')
+  text = f.read
+  f.close
+  text
+end
+
+
