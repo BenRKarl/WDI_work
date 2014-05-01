baca442e64c7b19c4dfce157506dc43c58326d46
atrapp <andrea.trapp@gmx.net>
2014-05-01 08:47:45 -0400

    w02 d03 homework and exercises

--- w02/d03/Andrea_Trapp/post_101_name/app.rb
+++ w02/d03/Andrea_Trapp/post_101_name/app.rb
@@ -4,3 +4,23 @@ Bundler.require
 get '/' do 
 	erb :index
 end
+
+post '/names' do 
+
+	person_name = params[:user_name]
+
+	redirect '/names?being_name=' + person_name
+	# redirect "/names?being_name=#{person_name}"
+
+
+
+	# puts "I am a friendly string"
+	# binding.pry
+	# puts "I am also a super nice string"
+end
+
+
+get '/names' do 
+	@name = params[:being_name]
+	erb :show
+end
