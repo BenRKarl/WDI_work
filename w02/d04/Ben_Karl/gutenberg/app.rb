c53b0cd111397e53fe2afce051db1b1d959b5198
Ben Karl <benrkarl@gmail.com>
2014-05-01 16:01:18 -0400

    committing at start of gutenberg project.

---
+++ w02/d04/Ben_Karl/gutenberg/app.rb
@@ -0,0 +1,14 @@
+require "bundler/setup"
+Bundler.require
+
+#Random book
+
+#Or select a specific book
+#Get text of book
+#Parse text
+#Store the results in a new file
+
+get "/" do
+  erb :root
+end
+
