abb1c2952cdfecebeaee0961798da76c74304a91
jdwmurph <jdwmurph@gmail.com>
2014-05-01 17:11:02 -0400

    Shakespeare word-counter is done.

--- w02/d04/John_Murphy/gutenberg/app.rb
+++ w02/d04/John_Murphy/gutenberg/app.rb
@@ -12,6 +12,44 @@ get '/' do
 end
 
 get '/books/:name' do
-  @name = params[:name]
+  @book_name = params[:name]
+  book_name = "books/#{@book_name}"
+  @word_counts = sort_counts(book_name)
   erb :show
 end
+
+
+def word_counts(book_name)
+  word_counts = {}
+  words = []
+
+  words(book_name).each do |word|
+    if word_counts.has_key?(word)
+      word_counts[word] += 1
+    else
+      word_counts[word] = 1
+    end
+  end
+  word_counts
+end
+
+def words(book_name)
+    book_text(book_name).split(' ')
+end
+
+def book_text(book_name)
+  f = File.open(book_name, 'r')
+  text = f.read
+  f.close
+  text
+
+  # File.read(book_name) do |f|
+  #   f.read
+  # end
+end
+
+def sort_counts(book_name)
+  word_counts(book_name).sort_by {|word, count| word}
+end
+
+
