685bef81f64ed6c42c116b622a9ef8c03ea684e8
pantsonsteven <pantsonsteven@gmail.com>
2014-05-01 17:08:39 -0400

    added downcase

--- w02/d04/Steven_Doran/gutenberg/app.rb
+++ w02/d04/Steven_Doran/gutenberg/app.rb
@@ -6,6 +6,50 @@ get '/' do
 end
 
 get '/books/:name' do 
-  params[:name]
-
+  @name = params[:name]
+  book_name = "books/#{@name}"
+  @word_count = sort_count(book_name)
+  erb :show
 end
+
+def sort_count(book_name)
+  word_count(book_name).sort_by { |word, counts| -counts }
+end
+
+def word_count(book_name)
+  word_count = {}
+  words(book_name).each do |word|
+    if word_count.has_key?(word)
+      word_count[word] += 1
+    else
+      word_count[word] = 1
+    end
+  end
+  word_count
+end
+
+def words(book_name)
+  book_text(book_name).split(' ').map { |word| word.downcase}
+end
+
+def book_text(book_name)
+  f = File.open(book_name, 'r')
+  text = f.read
+  f.close
+  text
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
