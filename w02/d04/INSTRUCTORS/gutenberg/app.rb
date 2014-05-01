7ed88ab80a904a8f928919b839f1adff278e9ee2
Aldric Giacomoni <trevoke@gmail.com>
2014-05-01 17:37:57 -0400

    Store parsed data

--- w02/d04/INSTRUCTORS/gutenberg/app.rb
+++ w02/d04/INSTRUCTORS/gutenberg/app.rb
@@ -1,14 +1,25 @@
 require 'bundler/setup'
 Bundler.require
 
+# NOTES
+# Now the "books" and the "parse_data" directory are part
+# of the domain knowledge, so they should be extracted,
+
+BOOK_DIR = "books"
+PARSE_DATA_DIR = "parse_data"
+
+[BOOK_DIR, PARSE_DATA_DIR].each do |directory|
+  next if File.directory?(directory)
+  Dir.mkdir(directory)
+end
+
 get '/' do
   erb :root
 end
 
 get '/books/:name' do
   @name = params[:name]
-  book_name = "books/#{@name}"
-  @word_counts = sort_counts(book_name)
+  @word_counts = sort_counts(@name)
   erb :show
 end
 
@@ -17,15 +28,41 @@ def sort_counts(book_name)
 end
 
 def word_counts(book_name)
-  word_counts = {}
-  words(book_name).each do |word|
-    if word_counts.has_key?(word)
-      word_counts[word] += 1
-    else
-      word_counts[word] = 1
-    end
+  if parsed_file_exists?(book_name)
+    word_counts = read_parse_data(book_name)
+  else
+    word_counts = count_words(book_name)
+    store_word_counts(book_name, word_counts)
   end
   word_counts
+end
+
+def count_words(book_name)
+  word_counts = Hash.new { |hash, key| hash[key] = 0 }
+  words(book_name).each do |word|
+    word_counts[word] += 1
+  end
+  word_counts
+end
+
+def store_word_counts(book_name, word_counts)
+  file_path = File.join(PARSE_DATA_DIR, book_name)
+  File.open(file_path, 'w') do |f|
+    word_counts.each do |word, count|
+      f << "#{word}: #{count}\n"
+    end
+  end
+end
+
+def parsed_file_exists?(book_name)
+
+  file_path = File.join(PARSE_DATA_DIR, book_name)
+  return File.exists?(file_path)
+end
+
+def read_parse_data(book_name)
+  file_path = File.join(PARSE_DATA_DIR, book_name)
+  File.readlines(file_path).map { |line| line.split(": ") }
 end
 
 def words(book_name)
@@ -33,7 +70,8 @@ def words(book_name)
 end
 
 def book_text(book_name)
-  f = File.open(book_name, 'r')
+  book_path = File.join(BOOK_DIR, book_name)
+  f = File.open(book_path, 'r')
   text = f.read
   f.close
   text
