fe2289767de845508ad27112f6e73dfb6888c335
pantsonsteven <pantsonsteven@gmail.com>
2014-05-01 18:09:04 -0400

    completed superhero app

---
+++ w02/d04/Steven_Doran/superhero.rb
@@ -0,0 +1,33 @@
+module Superhero
+  def shoot_web
+    "Shoots a web."
+  end
+  def fly
+    "Flies away."
+  end
+  def invisible
+    "Disappears before your eyes."
+  end
+  def claws
+    "I'm Wolverine!"
+  end
+  def bjork
+    ["Singing is like a celebration of oxygen.", "Football is a fertility festival. Eleven sprem trying to get into the egg. Ifeel sorry for the goalkeeper.", "People are always asking me about eskimos, but there are no eskimos in Iceland.", "Come on, I'm from Iceland; I don't do hip-hop.", "I've been traveling in Guatemala in the rainforest, and here all these houses are made of sticks. It seems so easy to make one.", "I'm a grateful... grapefruit."].sample
+  end
+  end
+
+  class Human
+    def initialize(name, age, gender)
+      @name = name
+      @age= age
+      @gender = gender
+    end
+  end
+
+
+Archie = Human.new("Archie", 17, "male").extend(Superhero).claws
+Betty = Human.new("Betty", 16, "female").extend(Superhero).invisible
+Jughead = Human.new("Jughead", 17, "male").extend(Superhero).bjork
+Veronica = Human.new("Jughead", 17, "female").extend(Superhero).fly
+
+
