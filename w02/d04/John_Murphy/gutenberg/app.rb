207095a4e829084dc0ac5c3bc9e37465ebc0f6f2
jdwmurph <jdwmurph@gmail.com>
2014-05-01 15:59:08 -0400

    working on gutenberg parser

---
+++ w02/d04/John_Murphy/gutenberg/app.rb
@@ -0,0 +1,12 @@
+require 'bundler/setup'
+Bundler.require
+
+get '/' do
+
+#   [4] pry(main)> id = GutenbergBookFinder.random_id
+# 33291
+# [5] pry(main)> id = GutenbergBookFinder.book_info(33291)
+# []
+# [6] pry(main)> id = GutenbergBookFinder.book_text(33291)
+  erb :root
+end
