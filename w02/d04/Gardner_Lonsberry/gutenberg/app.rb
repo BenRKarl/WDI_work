1d75b44b60b553c029c6bad8abb995c286341acb
glonsberry <gardnerlonsberry@gmail.com>
2014-05-01 17:10:52 -0400

    shakespeare book

--- w02/d04/Gardner_Lonsberry/gutenberg/app.rb
+++ w02/d04/Gardner_Lonsberry/gutenberg/app.rb
@@ -7,5 +7,26 @@ end
 
 get '/books/:name' do
   @name = params[:name]
-  erb :show
+  @word_counts = word_counts(book_name)
+ erb :show
 end
+
+def sort_counts(book_name)
+	  word_counts(book_name).sort_by { |word, counts| -word}
+
+def word_counts(book_name)
+  word_counts = {}
+  words(book_name).each do
+    |word|
+    if word_counts.has_key?
+    (word)
+    word_counts[word] += 1
+    else 
+    word_counts(word) = 1
+    end 
+end
+word_counts
+
+def words(book_name)
+	book_text(book_name).split 
+
