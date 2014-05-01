a3c4f3dd67625750d47569bab7cc0bebe3ecae55
wakelank <wakelank@gmail.com>
2014-05-01 00:47:24 -0400

    sinatra stock app complete

--- w02/d03/Wake_Lankard/stock_app/app.rb
+++ w02/d03/Wake_Lankard/stock_app/app.rb
@@ -3,21 +3,34 @@ Bundler.require
 
 
 get '/' do
-  #welcome page
-end
-
-
-
-get '/stock' do 
-  @ticker = params[:ticker]
-  @price = params[:price]
+  @ticker = ""
+  @price = ""
+  @volume_arr = []
 
   erb :index
 end
 
+#I could only get the array to the index.erb with the post method.  
+#I tried to put the values in the query string in the get method, but couldn't work it out.
+
+# get '/stock' do 
+#   @ticker = params[:ticker]
+#   @price = params[:price]
+
+#   erb :index
+# end
+
 post '/stock' do
-  ticker = params[:ticker]
-  data = YahooFinance.historical_quotes(ticker, Time::now-(24*60*60*10), Time::now)
-  price = data[0].open
-  redirect '/stock?ticker=' + ticker + '&price=' + price
+
+  @volume_arr = []
+
+  @ticker = params[:ticker]
+  data = YahooFinance.historical_quotes(@ticker, Time::now-(24*60*60*10), Time::now)
+  @price = data[0].open
+  volume_coef = data[0].volume.to_i / 100
+
+  data.each { |x| @volume_arr << x.volume.to_i / volume_coef }
+
+  erb :index
+  
 end
