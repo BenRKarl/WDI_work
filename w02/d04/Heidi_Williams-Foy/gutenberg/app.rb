90d4ed5d2fdb41d6b9ee69eae7611cc1b682ae4a
HeidiW <foywilliams@gmail.com>
2014-05-01 16:46:48 -0400

    a dusty old book progress

--- w02/d04/Heidi_Williams-Foy/gutenberg/app.rb
+++ w02/d04/Heidi_Williams-Foy/gutenberg/app.rb
@@ -5,3 +5,32 @@ get '/' do
 	erb :root
 end
 
+get '/books/:name' do
+	@name = params[:name]
+	word_counts(book_name)
+	erb :show
+end
+
+def word_counts(book_name)
+	# { "the" => 198003, ... }
+	word_counts = {}
+	words(book_name).each do |word|
+		if word_counts.has.key?(word)
+			word_counts[word] +=1
+		else
+			word_counts[word] = 1
+		end
+	end
+	word_counts
+end
+
+def words(book_name)
+	book_text(book_name).split(' ')
+end
+
+def book_text(book_name)
+	f = File.open(book_name, 'r')
+	text = f.read
+	f.close
+	end
+end
