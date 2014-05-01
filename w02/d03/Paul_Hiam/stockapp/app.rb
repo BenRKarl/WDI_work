dde80e35165e1bd78ab61dddb7d7e3f44c2e1f26
paulh1am <pshiam@gmail.com>
2014-05-01 09:01:39 -0400

    w2d4 am

--- w02/d03/Paul_Hiam/stockapp/app.rb
+++ w02/d03/Paul_Hiam/stockapp/app.rb
@@ -1,9 +1,22 @@
 require 'bundler/setup'
 Bundler.require
 
-get '/stock/:ticker' do
-  ticker = params[:ticker]
-  data = YahooFinance.historical_quotes(ticker, Time::now-(24*60*60*10), Time::now)
-  data[0].open
+
+
+get '/stock/:sym' do
+  @stock = params[:sym].to_s
+
+ data = YahooFinance.historical_quotes(@stock, Time::now-(24*60*60*10), Time::now)
+
+
+ #binding.pry
+
+  @length = data.length
+  
+  @close1 =  data[0].close.to_i
+  #@date = data[0].trade_date
+  
+  
+  #.open - was on data
  erb :ticker 
 end
