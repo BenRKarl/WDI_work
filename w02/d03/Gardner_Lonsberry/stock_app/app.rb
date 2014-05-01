61c59afae20167abf61d75a057fc3fa0c0c9d94a
glonsberry <gardnerlonsberry@gmail.com>
2014-05-01 08:55:59 -0400

    w02_d03_submission

--- w02/d03/Gardner_Lonsberry/stock_app/app.rb
+++ w02/d03/Gardner_Lonsberry/stock_app/app.rb
@@ -6,27 +6,23 @@ get '/' do
 end
 
 post '/stocks' do
-  query = params.map do |key, value|  "#{key}=#{value}"
-  end.join("&")
-  redirect "/stocks?#{query}"
-end
-
-get '/stocks' do
-  @ticker = params["stock_name"] 
-end
+  ticker = params[:ticker]
+  redirect "/stocks?query=#{ticker}"
+  erb :show
+ end
 
 # post '/stocks' do
-#   ticker_name = params[:stock_name]
-#   redirect "/stocks?stock_report=#{ticker_name}"
-#   erb :show
+#   query = params.map do |ticker1, ticker2|  "#{ticker1}=#{ticker2}"
+#   redirect "/stocks/query?=#{ticker}"
 # end
 
 
-get '/stocks' do
-  ticker = params[:ticker_name]
-  data   = YahooFinance.historical_quotes(ticker_name, Time::now-(24*60*60*10)
-  print_data =  data[0].symbol + " value is: " + data[0].ask
-  erb: show
+get '/stocks/query?=#{ticker}' do
+def finance(ticker)
+  data = YahooFinance.historical_quotes(ticker, Time::now-(24*60*60*10), Time::now)
+  data[0].open
+  erb :index
+ end
 end
 
 
