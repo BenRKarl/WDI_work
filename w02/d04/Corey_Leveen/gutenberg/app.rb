7c2bc9bf49ac01395d3192bb18d67ba26337c7a6
mntj <mntj11@gmail.com>
2014-05-01 17:14:34 -0400

    more gutenberg

--- w02/d04/Corey_Leveen/gutenberg/app.rb
+++ w02/d04/Corey_Leveen/gutenberg/app.rb
@@ -7,13 +7,35 @@ end
 
 get '/books/:name' do
   @name = params[:name]
+  book_name = "books/#{@name}"
+  @word_counts = word_counts(book_name)
   erb :show
 end
 
+def sort_counts(book_name)
+  word_counts(book_name).sort_by {|word, counts| counts}
+end
 
+def word_counts(book_name)
+    word_counts = {}
+    words(book_name).each do |word|
+      if word_counts.has_key?(word)
+      word_counts[word] += 1
+    else
+      word_counts[word] = 1
+    end
+  end
+  word_counts
+end
 
-# get '/random' do
-# end
+def words(book_name)
+  book_text(book_name).split(' ').map { |word| word.downcase }
+end
 
-# get '/id' do
-# end
+def book_text(book_name)
+  f = File.open(book_name, 'r')
+  text = f.read
+  f.close
+  text
+end
+
