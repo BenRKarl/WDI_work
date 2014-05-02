3ebf8153137d02b59f77fc52f4b75ff55ccbe9dc
Ben Karl <benrkarl@gmail.com>
2014-05-01 22:54:30 -0400

    Final homework submission, Week 2, Day 4.

--- w02/d04/Ben_Karl/receipt_generator/app.rb
+++ w02/d04/Ben_Karl/receipt_generator/app.rb
@@ -8,11 +8,11 @@ end
 post "/index" do
   @company = params[:company]
   @service = params[:service]
-  @cost = params[:cost]
+  @cost = "%.2f" % params[:cost].to_f
   File.open("receipt.txt", "w") do |line|
     line.puts "- Comany Name: #{@company}\n"
-    line.puts "- Service Provided: #{@service}\n"
-    line.puts "- Total Cost: #{@cost}\n"
+    line.puts "- Type of Service: #{@service}\n"
+    line.puts "- Total Cost: $#{@cost}\n"
   end
   redirect "/index"
 end
@@ -21,7 +21,11 @@ get "/index" do
   erb :result
 end
 
-# File.open('test.rb', 'w') do |f2|
-#   # use "\n" for two lines of text
-#   f2.puts "Created by Satish\nThank God!"
-# end
+post "/receipt" do
+  redirect "/receipt"
+end
+
+get "/receipt" do
+  @receipt_content = File.open("receipt.txt", "r")
+  erb :receipt
+end
