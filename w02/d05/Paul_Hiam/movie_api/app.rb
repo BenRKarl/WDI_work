48abc7bd9fabcfb9a121993f24f5cba76e749197
paulh1am <pshiam@gmail.com>
2014-05-02 12:20:40 -0400

    codealong omdb not working

--- w02/d05/Paul_Hiam/movie_api/app.rb
+++ w02/d05/Paul_Hiam/movie_api/app.rb
@@ -1,2 +1,17 @@
 require 'bundler/setup'
 Bundler.require
+
+get '/' do
+	"yo"
+	erb :root
+end
+
+get '/movie' do
+	movie_name = params[:movie_name].gsub(" ", "%20")
+	movie = HTTParty.get("http://www.omdbapi.com/?t="+movie_name)
+	movie_hash = JSON.parse(movie)
+	@title = movie_hash["Title"]
+	@plot = movie_hash["Plot"]
+	binding.pry
+	
+end
