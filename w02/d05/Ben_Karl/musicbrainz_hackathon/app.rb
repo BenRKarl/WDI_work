0d07d2df00d9331304af6c5987455277b5c7d091
Ben Karl <benrkarl@gmail.com>
2014-05-02 17:24:30 -0400

    have functioning base logic, need to style

--- w02/d05/Ben_Karl/musicbrainz_hackathon/app.rb
+++ w02/d05/Ben_Karl/musicbrainz_hackathon/app.rb
@@ -1,18 +1,6 @@
 require "bundler/setup"
 Bundler.require
 
-module Musicbrainz
-
-  def self.find(artist_name)
-    #call to API
-
-    artist_name = artist_name.gsub(" ", "%20")
-    url = "http://musicbrainz.org/ws/2/artist?query=" + artist_name
-    response = HTTParty.get(url)
-    response["metadata"]["artist_list"]["artist"]
-  end
-end
-
 get "/" do
   redirect "/index"
 end
@@ -22,15 +10,20 @@ get "/index" do
 end
 
 post "/show" do
-  artist = params[:artist]
-  # @artist_info = Musicbrainz.find(artist)
-  # @name = @artist_info["name"]
-  # @country = @artist_info["area"]["name"]
-
-  binding.pry
-  redirect "show"
+  artist = params[:artist].gsub(' ', '%20').downcase
+  redirect "/show?artist=#{artist}"
 end
 
 get "/show" do
+  artist = params[:artist]
+  @artist_info = MusicBrainz::Artist.find_by_name(artist)
+  @name = @artist_info.name
+  @dob = @artist_info.date_begin
+  @country = @artist_info.country
+  @death = @artist_info.date_end #NIL IF ALIVE
+  @type = @artist_info.type
+  @wiki_page = @artist_info.urls[:wikipedia]
+  @discography = @artist_info.urls[:discogs]
+  @facebook_page = @artist_info.urls[:social_network]
   erb :show
 end
