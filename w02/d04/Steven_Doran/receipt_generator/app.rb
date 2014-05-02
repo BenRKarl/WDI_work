49131a334cb6b9da3f0af9b99d584b0c71e0d073
pantsonsteven <pantsonsteven@gmail.com>
2014-05-01 20:16:12 -0400

    broke large post function into two smaller read and write methods

--- w02/d04/Steven_Doran/receipt_generator/app.rb
+++ w02/d04/Steven_Doran/receipt_generator/app.rb
@@ -5,15 +5,24 @@ get '/' do
   erb :index
 end
 
-post '/receipts' do 
+def write
   company = params[:company]
   goods = params[:goods]
   cost = params[:cost]
   receipt_write = File.open("files/receipts.txt", "w")
   receipt_write.write("- Company Name: #{company}\n- Good/Service Provided: #{goods}\n- Cost: #{cost}\n- Thank you for your patronage\n")
   receipt_write.close
+end
+
+def read
   receipt_read = File.open("files/receipts.txt", "r")
   @r = receipt_read.readlines
-  
+  receipt_read.close
+end
+
+
+post '/receipts' do 
+  write
+  read
   erb :receipts
 end
