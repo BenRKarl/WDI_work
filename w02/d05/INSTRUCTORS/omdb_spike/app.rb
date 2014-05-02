269b93561fec7e3ad2edeaeb0b7ab2a0f2f82b94
mattmarcello <matt.marcello@gmail.com>
2014-05-02 12:19:22 -0400

    wow factor reached

--- w02/d05/INSTRUCTORS/omdb_spike/app.rb
+++ w02/d05/INSTRUCTORS/omdb_spike/app.rb
@@ -2,9 +2,15 @@ require 'bundler'
 Bundler.require
 
 get '/' do
-  "hi, i am at the root"
+  erb :root
 end
 
 get '/movie' do
-  "showtime"
+  movie_name = params[:movie_name].gsub(" ", "%20")
+  movie = HTTParty.get("http://www.omdbapi.com/?t="+movie_name)
+  movie_hash = JSON.parse(movie)
+  @title        = movie_hash["Title"]
+  @plot         = movie_hash["Plot"]
+  @poster_url   = movie_hash["Poster"]
+  erb :show
 end
