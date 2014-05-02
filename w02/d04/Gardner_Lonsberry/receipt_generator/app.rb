8e7c31ac72f61bdc36b8a701adb719b6270824aa
glonsberry <gardnerlonsberry@gmail.com>
2014-05-02 01:20:08 -0400

    half way through receipts

--- w02/d04/Gardner_Lonsberry/receipt_generator/app.rb
+++ w02/d04/Gardner_Lonsberry/receipt_generator/app.rb
@@ -1,8 +1,20 @@
 require 'bundler/setup'
 Bundler.require
 
+def append_receipt
+  open('receipts.txt', 'a') { |f|
+  f << "Four score\n"
+  f << "and seven\n"
+  f << "years ago\n"
+}
+end
+
 
 get '/' do
-erb :index
+  erb :index
 end
+
+post '/receipts' do
+  redirect '/'
+ end
 
