423142d81c93c1a708160b119a0b1e1790350a3b
jwink <jeff_winkler@yahoo.com>
2014-05-03 19:00:07 -0400

    musicology

--- w02/d05/Jeff_Winkler/musicbrainz_hackathon/app.rb
+++ w02/d05/Jeff_Winkler/musicbrainz_hackathon/app.rb
@@ -11,44 +11,24 @@ end
 post '/artists' do
   artist_name = params[:artist].gsub(' ','%20')
   redirect "/artists?artist=#{artist_name}"
-
 end
 
 get '/artists' do
   artist_name = params[:artist]
   @artist_data = Musicbrains.find(artist_name)
-
   artist_id = @artist_data['id']
-
+  #need something if artist_id is null
   @release_group_data = Musicbrains.rg_data(artist_id)
-
-
-
-
-  #@test = MusicBrainz::Artist.find_by_name(artist_name)
-  #@name = @test.release_groups
-  #binding.pry
   erb :show
-    #info = Musicbrains.find(artist_name)
-    #info.to_s
-
 end
 
 get '/release/:rgid' do
   rg_id = params[:rgid]
   @release = Musicbrains.choose_one_release(rg_id)
-
+  #need something if @release is nil
   release_id = @release['id']
-
   @track_list = Musicbrains.track_list(release_id)
-
-
-  #binding.pry
-
   @artwork = Musicbrains.artwork(@release)
-
-
-
   erb :release
-
 end
+
