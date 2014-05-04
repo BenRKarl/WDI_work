2d78083c318fd2388f8e6691638a1407f7384f71
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-04 19:50:00 -0400

    Your Name: KristenTonga
    Subject Line: Sunday hw 2
    
    What Happened: Got the code working to display place, thought I'd better save. Now for styling, and then maps!!

--- w02/d05/Kristen_Tonga/musicbrainz_hackathon/app.rb
+++ w02/d05/Kristen_Tonga/musicbrainz_hackathon/app.rb
@@ -10,6 +10,29 @@ get '/' do
 end
 
 get '/artist' do
-  @artist = Musicbranz.find(params[:artist])
+  @artist = params[:artist]
+
+  #find artist data in Musicbrains
+    url = "http://musicbrains.org/ws/2/artist?query="+(@artist).to_s
+    response = HTTParty.get(url)
+    data =response['metadata']['artist_list']['artist']
+
+  #isolate down to countries
+    countries = data.map {|artist| artist['country']}
+    @undefin = countries.count(nil)
+    countries.delete(nil)
+    @countries = countries
+
+  #and sort!!
+  country_count = {}
+  countries.each do |country|
+    if country_count.has_key?(country)
+      country_count[country] += 1
+    else
+      country_count[country] = 1
+    end
+  end
+  @country_count = country_count
+
   erb :show
 end
