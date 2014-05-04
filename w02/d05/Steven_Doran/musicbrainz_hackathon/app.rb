fe4112e6a83b5dd4c5f457832f52eb13b7d8915f
pantsonsteven <pantsonsteven@gmail.com>
2014-05-04 13:42:26 -0400

    getting error in MusicBrainz gemfile

--- w02/d05/Steven_Doran/musicbrainz_hackathon/app.rb
+++ w02/d05/Steven_Doran/musicbrainz_hackathon/app.rb
@@ -4,11 +4,18 @@ Bundler.require
 require './lib/artist'
 require './lib/musicbrainz'
 
+MusicBrainz.configure do |c|
+  # Application identity (required)
+  c.app_name = "My Music App"
+  c.app_version = "1.0"
+  c.contact = "support@mymusicapp.com"
+end
+
 get '/' do
   erb :index
 end
 
 get '/artist' do 
-  @artist = Musicbrainz.find(params[:artist])
+  @artist = Musicbrainz.find(params[:artist_name])
   erb :show
 end
--- w02/d05/Steven_Doran/musicbrainz_hackathon/lib/musicbrainz.rb
+++ w02/d05/Steven_Doran/musicbrainz_hackathon/lib/musicbrainz.rb
@@ -1,16 +1,13 @@
 module Musicbrainz
+  
   include MusicBrainz
-
-  MusicBrainz.configure do |c|
-    c.app_name = "My Music App"
-    c.app_version = "1.0"
-    c.contact = "support@mymusicapp.com"
-  end
 
   def self.find(artist)
     artist_info = MusicBrainz::Artist.find_by_name(artist)
     album_names = []
-    artist_info.release_groups.each { |album| album_names << album.title } # array of album names
+    album_list = artist_info.release_groups  
+    album_list.each { |album| album_names << album.title } # array of album names
     artist = Artist.new(artist_info, album_names)
   end
+
 end
