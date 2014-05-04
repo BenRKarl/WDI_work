8f1b682b07e0f7e12af592a204f2789882a727d3
atrapp <andrea.trapp@gmx.net>
2014-05-04 08:46:49 -0400

    project musicbrainz_hackathon: added documentation on search the musicbrainz database

--- w02/d05/Andrea_Trapp/musicbrainz_hackathon/app.rb
+++ w02/d05/Andrea_Trapp/musicbrainz_hackathon/app.rb
@@ -18,25 +18,93 @@ end
 
 
 # ---------------------
-# Navigation through Musicbrainz data structure
-# execute .keys on each step - if possible - to
-# move forward until the information is found
+# Navigation through Musicbrainz data structure using 'pry'
+# first require 'musicbrainz' and 'httparty' to be able to execute
+# > response = HTTParty.get("http://musicbrainz.org/ws/2/artist?query=nena")
+# Info source for creating the above url under following webpage
+# > http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2/Search
+# > https://musicbrainz.org/doc/Development/JSON_Web_Service
+# then take response and subsequently execute .keys on each step - if possible - 
+# to move forward until the information is found. See below:
 # ---------------------
+# pry
+# require 'musicbrainz'
+# require 'httparty'
+# response = HTTParty.get("http://musicbrainz.org/ws/2/artist?query=nena")
 # response.keys
+
+	# [0] "metadata"
+
 # response['metadata'].keys
+
+	# [0] "artist_list",
+	# [1] "created"
+
 # response['metadata']['artist_list'].keys
+
+	# [0] "artist",
+	# [1] "count",
+	# [2] "offset"
+
 # response['metadata']['artist_list']['artist'].keys
 
-# Result > [8] pry(Musicbrainz)> response['metadata']['artist_list']['artist'].keys
-		 # NoMethodError: undefined method `keys' for #<Array:0x007ff02aa673c8>
-		 # from (pry):19:in `find'
+	# [8] pry(Musicbrainz)> response['metadata']['artist_list']['artist'].keys
+	# NoMethodError: undefined method `keys' for #<Array:0x007ff02aa673c8>
+	# from (pry):19:in `find'
 
 # response['metadata']['artist_list']['artist'][0]
 # response['metadata']['artist_list']['artist'][0].keys
 
+    # [0] "name",
+    # [1] "sort_name",
+    # [2] "country",
+    # [3] "area",
+    # [4] "disambiguation",
+    # [5] "life_span",
+    # [6] "tag_list",
+    # [7] "id",
+    # [8] "type",
+    # [9] "score"
+
 # response['metadata']['artist_list']['artist'][0]["disambiguation"]
 
-# Result > "Japanese trance artist"
+	# "Japanese trance artist"
+
+
+
+
+
+# Find artist for a given release
+# -------------------------------
+# http://musicbrainz.org/ws/2/release/?query=99+red+ballons
+
+# # > response = HTTParty.get("http://musicbrainz.org/ws/2/release/?query=99+red+ballons")
+# [8] pry(main)> response['metadata']['release_list']['release'][0]['artist_credit']['name_credit']
+# {
+#     "artist" => {
+#                   "name" => "Nena",
+#              "sort_name" => "Nena",
+#         "disambiguation" => "the band, until 1987",
+#                     "id" => "c954d136-c7fd-4fd9-8bb0-fb0491fc6a02"
+#     }
+# }
+
+
+# Find artist for a given release-group (album)
+# ---------------------------------------------
+# http://musicbrainz.org/ws/2/release-group/?query=99+luftballons
+
+# # > response = HTTParty.get("http://musicbrainz.org/ws/2/release-group/?query=99+luftballons")
+# [34] pry(main)> response['metadata']['release_group_list']['release_group'][0]['artist_credit']['name_credit']
+# {
+#     "artist" => {
+#                   "name" => "Nena",
+#              "sort_name" => "Nena",
+#         "disambiguation" => "the band, until 1987",
+#                     "id" => "c954d136-c7fd-4fd9-8bb0-fb0491fc6a02"
+#     }
+# }
+
 
 
 # -----------------------
--- w02/d05/Andrea_Trapp/musicbrainz_hackathon/lib/musicbrainz.rb
+++ w02/d05/Andrea_Trapp/musicbrainz_hackathon/lib/musicbrainz.rb
@@ -2,12 +2,20 @@ module Musicbrainz
   def self.find(artist_name)
   	artist_name = artist_name.gsub(' ', '%20')
  	url = "http://musicbrainz.org/ws/2/artist?query=" + artist_name
+ 	# works also: url = "http://musicbrainz.org/search?query=#{artist_name}&type=artist"
+ 	# works also: url = "http://musicbrainz.org/artist/c954d136-c7fd-4fd9-8bb0-fb0491fc6a02"
+ 	# query releases from Nena http://musicbrainz.org/ws/2/release/?query=99+red+ballons
 	response = HTTParty.get(url)	
 	response['metadata']['artist_list']['artist'] # returns a list of artists
-	# response = {"name" => "Ramstein", "country" => "DE"} 
+	
+	# response = MusicBrainz::Artist.find_by_name(artist_name)
+		# Exception at /artists
+		# Configuration missing
+		# file: configuration.rb location: config line: 47
+
+	# response = {"name" => "Ramstein", "country" => "DE"} # not in database
   end 
 end
-
 
 
 # ---------------------------------
