4848e2852df21ad085d5d23635b222c89784d237
mntj <mntj11@gmail.com>
2014-05-06 09:40:59 -0400

    finish submarines

--- w03/d02/Corey_Leveen/yellow_submarine/app.rb
+++ w03/d02/Corey_Leveen/yellow_submarine/app.rb
@@ -7,7 +7,7 @@ class Sailor
 end
 
 class Submarine
-attr_accessor :color
+attr_accessor :color, :sailors
 
   def initialize(color='yellow')
     @color = color
@@ -22,3 +22,11 @@ attr_accessor :color
     @sailors.delete(@sailors.sample)
   end
 end
+
+
+proto = ['Jeff Winkler','John Murphy','Nessa Nguyen','Jeff Drakos','Rebecca Strong','Gardner Lonsberry' ,'Jonathan Gean','Nathaniel Tuvera','Tim Hannes','Aziz Hasanov','Chris Heuberger','Dmitry Shamis' ,'Corey Leveen','Paul Hiam','Steven Doran','Ben Karl','Kristen Tonga','Wake Lankard','Carlos Pichardo' ,'Paul Gasberra','Andrea Trapp','Heidi Williams-Foy' ]
+the_sub = Submarine.new
+proto.each do |x|
+  the_sub.accept_sailor(Sailor.new(x))
+end
+
