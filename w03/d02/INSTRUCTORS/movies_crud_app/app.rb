16a414ba17458d4e38a6b73a2b5752a0aa1de29b
amadden80 <amadden80@gmail.com>
2014-05-06 14:42:58 -0400

    movies

--- w03/d02/INSTRUCTORS/movies_crud_app/app.rb
+++ w03/d02/INSTRUCTORS/movies_crud_app/app.rb
@@ -14,8 +14,22 @@ get '/' do
   erb :index
 end
 
+# index
 get '/movies' do
   @movies = Movie.all
   erb :movies
 end
 
+# new
+get '/movies/new' do
+  erb :new
+end
+
+# create
+post '/movies' do
+  title = params['movie_title']
+  director = params['movie_director']
+  screenwriter = params['movie_screenwriter']
+  Movie.create({:title => title, :director => director, :screenwriter => screenwriter})
+  redirect '/movies'
+end
