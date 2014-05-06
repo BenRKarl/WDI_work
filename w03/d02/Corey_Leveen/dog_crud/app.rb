c9dc789302d60daa9a85479be5117e0e9fd877dc
mntj <mntj11@gmail.com>
2014-05-06 18:12:37 -0400

    added more to dogs

--- w03/d02/Corey_Leveen/dog_crud/app.rb
+++ w03/d02/Corey_Leveen/dog_crud/app.rb
@@ -2,3 +2,30 @@ require 'bundler'
 Bundler.require
 
 require_relative 'models/dog'
+require_relative './config'
+
+#redirect
+get '/' do
+  redirect '/dogs'
+end
+
+#index
+get '/dogs' do
+  @dogs = Dog.all
+  erb :index
+end
+
+#new
+get 'dogs/new' do
+  erb :new
+end
+
+#create
+post '/dogs' do
+  #dog = Dog.create(name: params['name'])
+end
+
+#update
+
+
+#delete
