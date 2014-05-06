0e02a16962866f9e40a8b33c5df33b237aeeb212
thannes30 <thannes30@yahoo.com>
2014-05-06 12:34:35 -0400

    finished movies crud app

--- w03/d02/Tim_Hannes/morning/movies_crud_app/app.rb
+++ w03/d02/Tim_Hannes/morning/movies_crud_app/app.rb
@@ -16,7 +16,32 @@ get '/' do
   erb :index #refer to index.erb
 end
 
+# index
 get '/movies' do
   @movies = Movie.all #display all movies
   erb :movies #refer to movies.erb
+end
+
+# new
+get '/movies/new' do  #adding a new movie
+  erb :new
+end
+
+# create
+# post '/movies' do
+#   # create a new movie and add it
+#   # based on the info entered in the form.
+#   title = params['movie_title']
+#   director = params['movie_director']
+#   screenwriter = params['movie_screenwriter']
+#   Movie.create({:title => title, :director => director, :screenwriter => screenwriter})
+#   # the above uses .create from AR to create a new item in db using params from the form.
+#   redirect '/movies'
+# end
+post '/movies' do
+  title = params['movie_title']
+  director = params['movie_director']
+  screenwriter = params['movie_screenwriter']
+  Movie.create({:title => title, :director => director, :screenwriter => screenwriter})
+  redirect '/movies'
 end
