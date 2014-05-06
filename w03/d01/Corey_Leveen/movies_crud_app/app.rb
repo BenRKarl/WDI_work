33cd9c0fb056a759f91e7882b76efc96df790c22
mntj <mntj11@gmail.com>
2014-05-06 12:13:14 -0400

    movies_crud_app semi-working

--- w03/d01/Corey_Leveen/movies_crud_app/app.rb
+++ w03/d01/Corey_Leveen/movies_crud_app/app.rb
@@ -1,6 +1,22 @@
+#corey's version
 require 'bundler'
 Bundler.require
 
+require_relative 'models/movie'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :database => 'movies_app'
+  )
+
+
 get '/' do
   erb :index
+  redirect '/movies'
 end
+
+get '/movies' do
+  @movies = Movie.all
+  erb :movies
+end
+
