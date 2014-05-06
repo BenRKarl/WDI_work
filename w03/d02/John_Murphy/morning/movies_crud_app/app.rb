11fc8f93e52ca2549dc37ad73a6036b1c3b474e9
jdwmurph <jdwmurph@gmail.com>
2014-05-06 12:13:46 -0400

    Connection established with Movies database

--- w03/d02/John_Murphy/morning/movies_crud_app/app.rb
+++ w03/d02/John_Murphy/morning/movies_crud_app/app.rb
@@ -1,6 +1,19 @@
 require 'bundler'
 Bundler.require
 
+require './models/movie'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :database => 'movies_app'
+  )
+
 get '/' do
   erb :index
+end
+
+get '/movies' do
+  @movies = Movie.all
+ erb :movies
+
 end
