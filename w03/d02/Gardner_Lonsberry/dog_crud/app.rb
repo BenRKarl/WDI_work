f090c6743f552ab1cdc768d9719686b125734f90
glonsberry <gardnerlonsberry@gmail.com>
2014-05-06 18:17:00 -0400

    second commit, about to try running server

--- w03/d02/Gardner_Lonsberry/dog_crud/app.rb
+++ w03/d02/Gardner_Lonsberry/dog_crud/app.rb
@@ -2,3 +2,49 @@ require 'bundler'
 Bundler.require
 
 require_relative 'models/dog'
+
+# index
+get '/dogs' do 
+  @dogs = Dog.all
+  erb :index
+end
+
+# new
+get '/dogs/new' do
+  erb :new
+end
+
+
+# create
+post '/dogs'
+  dog = Dog.create(name: params ['name'])
+  dog.update_image
+  dog.save
+  redirect "/dogs/#{ dog.id }"
+end
+
+# show
+get '/dogs/:id' do
+  @dog = Dog.find(params[:id])
+  erb: show
+
+
+#  edit
+
+get 'dogs/:id/edit' do
+  @dog = Dog.find(params[:id])
+  erb: edit
+
+put '/dogs/:id' do
+  dog = Dog.find(params[:id])
+  dog.name = params[:name]
+  dog.update_image
+  dog.save
+  redirect "dogs/#{ dog.id }"
+end
+
+delete '/dogs/:id' do
+  Dog.delete(params[:id])
+  redirect '/dogs'
+end
+
