add8ccd3415db5e2255f4220aa9040c50ae169f1
paulh1am <pshiam@gmail.com>
2014-05-06 15:29:09 -0400

    crud movie view, add

--- w03/d02/Paul_Hiam/movies_crud_app/app.rb
+++ w03/d02/Paul_Hiam/movies_crud_app/app.rb
@@ -1,7 +1,30 @@
 require 'bundler'
 Bundler.require
 
-get '/' do
+require_relative 'models/movie'
 
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :database => 'movies_app'
+  )
+
+get '/' do
 erb :index
+end
+
+get '/movies' do
+  @movies = Movie.all
+erb :movies
+end
+
+get '/movies/new' do
+ erb :new
+end
+post '/movies' do
+  #create new movie based on form entry
+  title = params['movie_title']
+  director = params['movie_director']
+  screenwriter = params['movie_screenwriter']
+  Movie.create({:title => title, :director => director, :screenwriter => screenwriter})
+  redirect '/movies'
 end
