17065213cfcae03c16b8576aa38a197aa04ae55f
paulh1am <pshiam@gmail.com>
2014-05-06 18:10:15 -0400

    dog sinatra setup

--- w03/d02/Paul_Hiam/dog_crud/app.rb
+++ w03/d02/Paul_Hiam/dog_crud/app.rb
@@ -3,3 +3,18 @@ Bundler.require
 
 require_relative 'models/dog'
 require_relative './config'
+
+get '/' do
+redirect '/dogs'
+  end
+
+#index
+get '/dogs' do
+  @dogs = Dog.all
+  erb :index
+end
+
+#create
+
+
+#show
