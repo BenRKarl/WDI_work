29743f5c4f03800c82779dd6905ae2ed9f70948f
amadden80 <amadden80@gmail.com>
2014-05-06 15:21:31 -0400

    CRUD app born for movies

--- w03/d02/INSTRUCTORS/movies_crud_app/app.rb
+++ w03/d02/INSTRUCTORS/movies_crud_app/app.rb
@@ -11,14 +11,18 @@ ActiveRecord::Base.establish_connection(
 
 
 get '/' do
-  erb :index
+  redirect '/movies'
 end
+
 
 # index
 get '/movies' do
   @movies = Movie.all
-  erb :movies
+  erb :index
 end
+
+
+
 
 # new
 get '/movies/new' do
@@ -31,5 +35,38 @@ post '/movies' do
   director = params['movie_director']
   screenwriter = params['movie_screenwriter']
   Movie.create({:title => title, :director => director, :screenwriter => screenwriter})
+  redirect '/movies'
+end
+
+
+
+
+# show
+get '/movies/:id' do
+  @movie = Movie.find(params[:id])
+  erb :show
+end
+
+
+# edit
+get '/movies/:id/edit' do
+  @movie = Movie.find(params[:id])
+  erb :edit
+end
+
+# update
+put '/movies/:id' do
+  movie = Movie.find(params[:id])
+  movie.title = params['movie_title']
+  movie.director = params['movie_director']
+  movie.screenwriter = params['movie_screenwriter']
+  movie.save
+  redirect "/movies/#{movie.id}"
+end
+
+
+# delete
+delete '/movies/:id' do
+  Movie.delete(params[:id])
   redirect '/movies'
 end
