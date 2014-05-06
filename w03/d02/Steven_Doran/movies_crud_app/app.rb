d6ca8d6514d06d62b85655b8f465de93912435e6
pantsonsteven <pantsonsteven@gmail.com>
2014-05-06 13:21:05 -0400

    completed app

--- w03/d02/Steven_Doran/movies_crud_app/app.rb
+++ w03/d02/Steven_Doran/movies_crud_app/app.rb
@@ -16,3 +16,17 @@ get '/movies' do
   @movies = Movie.all
   erb :movies
 end
+
+get '/movies/new' do 
+  erb :new
+end
+
+post '/movies' do 
+  title = params['movie_title']
+  director = params['movie_director']
+  screenwriter = params['movie_screenwriter']
+  Movie.create({title: title, director: director, screenwriter: screenwriter})
+  redirect '/movies'
+end
+
+
