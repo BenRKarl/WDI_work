9e6b63ab6417e68a016c88d66eb297f00f46d07f
jwink <jeff_winkler@yahoo.com>
2014-05-05 21:06:18 -0400

    lunch bot

---
+++ w03/d01/Jeff_Winkler/lunch_bot/app.rb
@@ -0,0 +1,49 @@
+
+require 'bundler/setup'
+Bundler.require
+
+require_relative 'models/lunch_methods'
+
+get '/' do
+  redirect '/lunches'
+end
+
+get '/lunches' do
+  @all_lunches = disp_all
+  erb :index
+end
+
+get '/lunches/new' do
+  erb :show
+end
+
+get '/lunches/random' do
+  lunch_hash = disp_all.sample
+  id = lunch_hash["id"]
+  picked_count = lunch_hash["picked_count"].to_i
+  new_count = picked_count+1
+  count_it(id, new_count)
+
+  lunch_hash["picked_count"] = new_count
+  @all_lunches = []
+  @all_lunches[0] = lunch_hash
+
+  erb :index
+end
+
+post '/lunches/create' do
+  lunch = params["lunch"]
+  resto = params["resto"]
+
+  new_lunch(lunch, resto)
+
+  redirect '/lunches'
+
+
+
+end
+
+
+
+
+
