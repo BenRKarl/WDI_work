3489587b7ee4008a678925029107e12ba0baf529
paulh1am <pshiam@gmail.com>
2014-05-06 19:23:57 -0400

    hw pass 1 done

--- w03/d02/Paul_Hiam/dog_crud/app.rb
+++ w03/d02/Paul_Hiam/dog_crud/app.rb
@@ -15,6 +15,40 @@ get '/dogs' do
 end
 
 #create
-
+post '/dogs' do
+  dog = Dog.create(name: params['name'], breed: params['breed'], age: params['age'])
+  dog.save
+  redirect "/dogs/#{ dog.id }"
+end
+get '/dogs/new' do
+  erb :new
+end
 
 #show
+get '/dogs/:id' do
+  @dog = Dog.find(params[:id])
+  erb :show
+end
+
+#edit
+get '/dogs/:id/edit' do
+  @dog = Dog.find(params[:id])
+  @dog.name = params[:name]
+  @dog.breed = params[:breed]
+  @dog.age = params[:age]
+  erb :edit
+end
+
+put '/dogs/:id' do
+  dog = Dog.find(params[:id])
+  dog.name = params[:name]
+  dog.breed = params[:breed]
+  dog.age = params[:age]
+  dog.save
+  redirect "/dogs/#{ dog.id }"
+end
+
+delete '/dogs/:id' do
+  Dog.delete(params[:id])
+  redirect '/dogs'
+end
