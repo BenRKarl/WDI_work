f874dfbe780c391915037a7363d5ff846e1a4723
wakelank <wakelank@gmail.com>
2014-05-06 17:54:14 -0400

    set up CRUD skeliton in app.rb

--- w03/d02/Wake_Lankard/wdi_dogs/app.rb
+++ w03/d02/Wake_Lankard/wdi_dogs/app.rb
@@ -1,3 +1,37 @@
 require 'bundler'
-require_relative '/config.rb'
+require_relative './config.rb'
+require_relative 'models/dog'
 Bundler.require
+
+# index
+get '/' do
+  
+end
+
+get '/dogs' do
+end
+
+# new
+get '/dogs/new' do
+end
+
+# create
+post '/dogs' do
+end
+
+#show
+get '/dogs/:id' do
+end
+
+#edit
+get '/dogs/:id/edit' do
+end
+
+put 'dogs/:id' do
+  
+end
+
+#delete
+delete '/dogs/:id' do
+
+end
