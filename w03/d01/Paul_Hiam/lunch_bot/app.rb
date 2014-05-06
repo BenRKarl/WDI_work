c6959008836862e66a805ccfb5e1ace632546271
paulh1am <pshiam@gmail.com>
2014-05-05 22:03:50 -0400

    HW done, no styles : (

--- w03/d01/Paul_Hiam/lunch_bot/app.rb
+++ w03/d01/Paul_Hiam/lunch_bot/app.rb
@@ -31,7 +31,7 @@ post '/lunches/create' do
 	lunch       = params[:lunch_name] 
 	restaurant  = params[:restaurant_name]
 	connection  = PG.connect(:dbname => 'wdi_lunch')
-	
+
 	sql_statement = "INSERT INTO lunches (lunch_name, restaurant) VALUES('#{lunch}', '#{restaurant}');"
 
 	connection.exec(sql_statement)
@@ -42,5 +42,27 @@ post '/lunches/create' do
 end
 
 post '/lunches/random' do
-	redirect '/lunches/random'
+	connection = PG.connect(:dbname => 'wdi_lunch')
+  sql_statement = 'SELECT * FROM lunches;'
+  response = connection.exec(sql_statement)
+
+  @lunchset = response.map do|lunch| 
+  	{'lunch_name' => lunch['lunch_name'], 'picked_count'=> lunch['picked_count'], 'restaurant'=> lunch['restaurant']}
+    end
+  @r_num_top = @lunchset.length
+  @r_num    = rand(@r_num_top) - 1
+  @random  = @lunchset[@r_num]
+  @name = @random['lunch_name'].to_s
+
+  connection.exec(
+  	"UPDATE lunches SET picked_count= picked_count + 1 WHERE lunch_name='#{@name}';"
+  	)
+  
+  #
+  # @random = response2.map do|lunch| 
+  # 	{'lunch_name' => lunch['lunch_name'], 'picked_count'=> lunch['picked_count'], 'restaurant'=> lunch['restaurant']}
+  # end
+
+  connection.close
+	erb :random
 end
