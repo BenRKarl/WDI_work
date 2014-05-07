25c60451c55819af19ef7df17146a6b8bb954981
thannes30 <thannes30@yahoo.com>
2014-05-07 08:54:37 -0400

    tuesday homework

--- w03/d02/Tim_Hannes/morning/movies_crud_app/models/movie.rb
+++ w03/d02/Tim_Hannes/morning/movies_crud_app/models/movie.rb
@@ -1,3 +1,3 @@
-class Movie < ActiveRecord::Base #this is MOVIE with NO S instead of movies because the table has an amount of movies. inherits ActiveRecord
+class Movie < ActiveRecord::Base #this is MOVIE with NO S instead of movieS because the table has an amount of movies. inherits ActiveRecord
 
 end
