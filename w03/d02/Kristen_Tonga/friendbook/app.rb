bbdf9ce88bc00c62d17bfa9a566cc76ffb37024f
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-07 10:45:21 -0400

    Your Name: Kristen Tonga
    Subject Line: two days worth
    
    What Happened: included is friendbook, incomplete, and morning exercises squares and kittens.

--- w03/d02/Kristen_Tonga/friendbook/app.rb
+++ w03/d02/Kristen_Tonga/friendbook/app.rb
@@ -4,3 +4,43 @@ Bundler.require
 require_relative 'models/friend'
 require_relative './config'
 
+#index
+get '/friends' do
+  @friends = Friend.all
+  erb :index
+end
+
+#create
+get '/friends/new' do
+  erb :new
+end
+
+post '/friends' do
+  friend = Friend.create['username']
+  friend.update_image
+  friend.save
+  redirect "/friends/#{ friend.id }"
+end
+
+#show
+get '/friends/:id' do
+  @friend = Friend.find(params[:id])
+  erb :show
+end
+
+#edit
+get '/friends/:id/edit' do
+  @friend = Friend.find(params[:id])
+  erb :edit
+end
+
+put '/friends/:id' do
+  friend = Friend.find(params[:id])
+  friend.username = params[:username]
+  friend.update_image
+  friend.save
+  redirect "/friends/#{ friend.id }"
+end
+
+
+
