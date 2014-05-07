6bd3749f33183ffb93149736d08002ef403bc08d
ntuvera <ntuvera@gmail.com>
2014-05-07 16:16:04 -0400

    broken bloops

---
+++ w03/d03/Nathaniel_Tuvera/bloop_app/app.rb
@@ -0,0 +1,58 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'models/user'
+require_relative 'models/bloop'
+
+require_relative 'config.rb'
+
+get '/' do
+  @users = User.all
+  erb :index
+end
+
+get '/users/new' do
+  erb :'users/new'
+end
+post '/users' do
+  username = params[:username]
+  new_user = User.create({username: username})
+  redirect "/users/#{ new_user.id }"
+end
+
+
+# show
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'users/show'
+end
+
+
+get "/users/:id/bloops/new" do
+  # @user_id = User.find(params[:id])
+  @user    = User.find(params[:id])
+  erb :'bloops/new'
+end
+
+get 'users/:id/bloops/bloop_id' do
+  @message  =Bloops.all
+  erb :show
+  end
+
+post '/users/:id/bloops' do
+  user      = User.find(params[:id])
+
+  message   = params[:message]
+  new_bloop = Bloop.create({message: message})
+
+  # "UPDATE bloops SET user_id={#user.id} WHERE id=#{ new_bloop.id}""
+  user.bloops << new_bloop         # activerecord syntax to UPDATE, NOT inserting an ARRAY
+
+  redirect "/users/#{params[:id]}"
+end
+
+delete '/users/:user_id/bloops/:bloop_id' do
+  # Bloop.delete(params[:bloop_id])
+  cow_id = params
+  redirect "/users/#{params[:user_id]}"
+end
