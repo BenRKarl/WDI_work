144a1d48dc58df7be5c5db77357da293122aa028
glonsberry <gardnerlonsberry@gmail.com>
2014-05-06 21:41:52 -0400

    working dog app

--- w03/d02/Gardner_Lonsberry/dog_crud/app.rb
+++ w03/d02/Gardner_Lonsberry/dog_crud/app.rb
@@ -4,8 +4,13 @@ Bundler.require
 require_relative 'models/dog'
 require_relative './config'
 
+
+get '/' do
+  redirect "/dogs"
+end
+
 # index
-get '/dogs' do 
+get '/dogs' do
   @dogs = Dog.all
   erb :index
 end
@@ -15,14 +20,13 @@ get '/dogs/new' do
   erb :new
 end
 
-
 # create
 post '/dogs' do
-  dog = Dog.create(name: params['name'])
-  # dog.update_image
+  dog = Dog.create({name: params['name'], age: params['age'], breed: params['breed']})
   dog.save
   redirect "/dogs/#{ dog.id }"
 end
+
 
 # show
 get '/dogs/:id' do
@@ -30,19 +34,19 @@ get '/dogs/:id' do
   erb :show
 end
 
-#  edit
+# edit
 
 get '/dogs/:id/edit' do
   @dog = Dog.find(params[:id])
   erb :edit
 end
 
+
 put '/dogs/:id' do
   dog = Dog.find(params[:id])
   dog.name = params[:name]
   dog.age = params[:age]
   dog.breed = params[:breed]
-  # dog.update_image
   dog.save
   redirect "/dogs/#{ dog.id }"
 end
@@ -51,4 +55,5 @@ delete '/dogs/:id' do
   Dog.delete(params[:id])
   redirect '/dogs'
 end
+
 
