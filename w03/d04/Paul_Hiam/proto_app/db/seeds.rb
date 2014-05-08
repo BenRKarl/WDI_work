7a949c548a2b8ead8e96d173538f0c18b6320614
paulh1am <pshiam@gmail.com>
2014-05-08 18:30:41 -0400

    protos made

--- w03/d04/Paul_Hiam/proto_app/db/seeds.rb
+++ w03/d04/Paul_Hiam/proto_app/db/seeds.rb
@@ -1,3 +1,10 @@
+protos = ['Jeff Winkler','John Murphy','Nessa Nguyen','Jeff Drakos','Rebecca Strong','Gardner Lonsberry' ,'Jonathan Gean','Nathaniel Tuvera','Tim Hannes','Aziz Hasanov','Chris Heuberger','Dmitry Shamis' ,'Corey Leveen','Paul Hiam','Steven Doran','Ben Karl','Kristen Tonga','Wake Lankard','Carlos Pichardo' ,'Paul Gasberra','Andrea Trapp','Heidi Williams-Foy'
+]
+protos.each { |p| Proto.create(name: p)}
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
