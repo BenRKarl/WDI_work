db466a58681ce4c97a43f5323fe7f5d82797d761
ntuvera <ntuvera@gmail.com>
2014-05-08 07:15:39 -0400

    hw_w03_d03 submission

--- w03/d03/Nathaniel_Tuvera/bloop_app/app.rb
+++ w03/d03/Nathaniel_Tuvera/bloop_app/app.rb
@@ -6,43 +6,41 @@ require_relative 'models/bloop'
 
 require_relative 'config.rb'
 
+# index
 get '/' do
   @users = User.all
   erb :index
 end
 
+# new users - GET
 get '/users/new' do
   erb :'users/new'
 end
+
+# create new users - POST
 post '/users' do
   username = params[:username]
   new_user = User.create({username: username})
   redirect "/users/#{ new_user.id }"
 end
 
-
-# show
+# show users by ID
 get '/users/:id' do
   @user = User.find(params[:id])
   erb :'users/show'
 end
 
 
-get "/users/:id/bloops/new" do
+get '/users/:id/bloops/new' do
   # @user_id = User.find(params[:id])
-  @user    = User.find(params[:id])
+  @user = User.find(params[:id])
   erb :'bloops/new'
 end
 
-get 'users/:id/bloops/bloop_id' do
-  @message  =Bloops.all
-  erb :show
-  end
-
 post '/users/:id/bloops' do
-  user      = User.find(params[:id])
+  user = User.find(params[:id])
 
-  message   = params[:message]
+  message = params[:message]
   new_bloop = Bloop.create({message: message})
 
   # "UPDATE bloops SET user_id={#user.id} WHERE id=#{ new_bloop.id}""
@@ -52,7 +50,7 @@ post '/users/:id/bloops' do
 end
 
 delete '/users/:user_id/bloops/:bloop_id' do
-  # Bloop.delete(params[:bloop_id])
-  cow_id = params
+  bloop_id = params[:bloop_id]
+  Bloop.delete(bloop_id)
   redirect "/users/#{params[:user_id]}"
 end
