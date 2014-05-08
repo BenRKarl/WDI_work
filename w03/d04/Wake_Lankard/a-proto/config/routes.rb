4cf9ca8f93e7fb325e3752dfed7d2339bf8765f2
wakelank <wakelank@gmail.com>
2014-05-08 19:24:21 -0400

    proto complete

--- w03/d04/Wake_Lankard/a-proto/config/routes.rb
+++ w03/d04/Wake_Lankard/a-proto/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/protos', :action => "index", :controller => "protos"
+#  get '/protos', :action => "index", :controller => "protos"
 
-   get '/', :action => "welcome", :controller => "protos"
+  get '/protos' =>      'protos#index'
+  get '/' =>            'protos#welcome'
+  get '/protos/:id' =>   'protos#show'
 
-   get '/protos/:id', :action => "show", :controller => "protos"
+   # get '/', :action => "welcome", :controller => "protos"
+
+   # get '/protos/:id', :action => "show", :controller => "protos"
 
 
 
--- w03/d04/Wake_Lankard/a-proto/db/migrate/20140508193723_create_protos.rb
+++ w03/d04/Wake_Lankard/a-proto/db/migrate/20140508193723_create_protos.rb
@@ -2,6 +2,8 @@ class CreateProtos < ActiveRecord::Migration
   def change
     create_table :protos do |t|
       t.string :name
+      t.string :height
+      t.string :width
     end
   end
 end
