8f8d62968f1f10668d53e0ba936ce1dc80c8a8e4
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-09 15:52:38 -0400

    Rails kittens: convert sqlite to postgresql

--- w03/d04/Nessa_Nguyen/rails_kittens/db/seeds.rb
+++ w03/d04/Nessa_Nguyen/rails_kittens/db/seeds.rb
@@ -1,1 +1,8 @@
+Kitten.delete_all
 
+10.times do 
+	width  = rand(100..300)
+	height = rand(100..300)	
+	url 	 = "http://placekitten.com/#{width}/#{height}"
+	Kitten.create({width: width, height: height, url: url})
+end
