8591781676042f463a63c3d4495f27d3bd8f6789
paulh1am <pshiam@gmail.com>
2014-05-09 00:56:10 -0400

    kitten up

--- w03/d04/Paul_Hiam/proto_app/config/routes.rb
+++ w03/d04/Paul_Hiam/proto_app/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
  
 #get '/protos', :action => "index", :controller => "protos"
+get '/' => "protos#welcome"
 get '/protos' => "protos#index"
 
-get '/' => "protos#welcome"
+
 
 get '/protos/:id' => "protos#show"
 end
