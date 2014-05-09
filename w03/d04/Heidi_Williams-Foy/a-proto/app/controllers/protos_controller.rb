f57fb37b3999316e4c95b72e61791b1f663b2181
HeidiW <foywilliams@gmail.com>
2014-05-08 22:11:31 -0400

    Protos Rail App

--- w03/d04/Heidi_Williams-Foy/a-proto/app/controllers/protos_controller.rb
+++ w03/d04/Heidi_Williams-Foy/a-proto/app/controllers/protos_controller.rb
@@ -2,4 +2,10 @@ class ProtosController <ApplicationController
 	def index
 		@protos = Proto.all
 	end
+	def welcome
+		@message = 'Welcome Protos!'
+	end
+	def show
+		@proto = Proto.find_by_id(params[:id])
+	end
 end
--- w03/d04/Heidi_Williams-Foy/a-proto/config/routes.rb
+++ w03/d04/Heidi_Williams-Foy/a-proto/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- get '/protos', :action => "index", :controller => "protos"
+ #get '/protos', :action => "index", :controller => "protos"
+ get '/protos', to: 'protos#index'
+ get '/', to: 'protos#welcome'
+ get "/protos/:id", to: 'protos#show'
 end
 
 
