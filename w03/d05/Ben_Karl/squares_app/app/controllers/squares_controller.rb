c57992a9d5510f36e3c7d6ecd903d0ed710cd2fa
Ben Karl <benrkarl@gmail.com>
2014-05-10 14:03:00 -0400

    Finish squares app.

--- w03/d05/Ben_Karl/squares_app/app/controllers/squares_controller.rb
+++ w03/d05/Ben_Karl/squares_app/app/controllers/squares_controller.rb
@@ -16,4 +16,20 @@ class SquaresController < ApplicationController
     @square = Square.find(params[:id])
   end
 
+  def edit
+    @square = Square.find(params[:id])
+  end
+
+  def update
+    square = Square.find(params[:id])
+    square.update(params.require(:square).permit(:side_length, :border_radius, :color))
+    redirect_to "/squares/#{ square.id }"
+  end
+
+  def destroy
+    square = Square.find(params[:id])
+    square.delete
+    redirect_to "/squares"
+  end
+
 end
--- w03/d05/Ben_Karl/squares_app/config/routes.rb
+++ w03/d05/Ben_Karl/squares_app/config/routes.rb
@@ -7,9 +7,17 @@ Rails.application.routes.draw do
     #display a list of all squares
 
   get "squares/new" => 'squares#new'
+    #hosts form to submit information for a new square
 
   post "squares" => 'squares#create'
+    #generates post request and creates new square in database
 
   get "squares/:id" => 'squares#show'
+
+  get "squares/:id/edit" => 'squares#edit'
+
+  put "squares/:id" => "squares#update"
+
+  delete 'squares/:id' => 'squares#destroy'
 
 end
