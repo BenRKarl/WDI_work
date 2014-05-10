f99652086260cdc207c444bab30dcdc285129ea5
ntuvera <ntuvera@gmail.com>
2014-05-10 17:19:32 -0400

    add edit form and functionality

--- w03/d05/Nathaniel_Tuvera/squares_on_rails_app/app/controllers/squares_controller.rb
+++ w03/d05/Nathaniel_Tuvera/squares_on_rails_app/app/controllers/squares_controller.rb
@@ -9,16 +9,22 @@ class SquaresController < ApplicationController
 
   def create
     new_square = Square.create({:side_length => params[:side], :border_radius => params[:border], :color => params[:color]})
-    redirect_to "/squares"
+    redirect_to "/squares/#{new_square.id}"
   end
 
-  def show
-  end
 
   def edit
+    @square = Square.find(params[:id])
   end
 
   def update
+    edited_square = Square.find(params[:id])
+    edited_square.update({:side_length => params[:side], :border_radius => params[:radius], :color => params[:color]})
+    redirect_to "/squares/#{edited_square.id}"
+  end
+
+ def show
+    @square = Square.find(params[:id])
   end
 
   def destroy
--- w03/d05/Nathaniel_Tuvera/squares_on_rails_app/config/routes.rb
+++ w03/d05/Nathaniel_Tuvera/squares_on_rails_app/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'squares#index'
   get '/squares'           => 'squares#index'
   get '/squares/new'       => 'squares#new'
   post '/squares'          => 'squares#create'
-  get '/squares/:id'       => 'squares#show'
-  get '/squares/:id:edit'  => 'squares#edit'
+  get '/squares/:id/edit'  => 'squares#edit'
   put '/squares/:id'       => 'squares#update'
+  get '/squares/:id'       => 'squares#show'
   delete '/squares/:id'    => 'squares#destroy'
 end
