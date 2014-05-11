4e0502142d75a9731489bfb8f0d29c933fcecb83
ntuvera <ntuvera@gmail.com>
2014-05-10 22:42:22 -0400

    finish full CRUD functionality

--- w03/d05/Nathaniel_Tuvera/planets_on_rails_app/db/seeds.rb
+++ w03/d05/Nathaniel_Tuvera/planets_on_rails_app/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+earth = Planet.create({name: 'Earth', image_url: 'http://www.photoshopgurus.com/forum/attachments/general-photoshop-board/3134d1302575637t-well-i-give-up-can-someone-show-me-how-do-technique-planet_glow_00.png', diameter: 7918, mass: 5.972, life: true})
