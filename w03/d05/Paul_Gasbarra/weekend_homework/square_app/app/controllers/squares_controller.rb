38646e4915a3d1d0779ebe8edc305b5031ae5071
paulgasbarra <paulgasbarra@gmail.com>
2014-05-11 13:01:07 -0400

    Quick commit for review

--- w03/d05/Paul_Gasbarra/weekend_homework/square_app/app/controllers/squares_controller.rb
+++ w03/d05/Paul_Gasbarra/weekend_homework/square_app/app/controllers/squares_controller.rb
@@ -12,9 +12,30 @@ class SquaresController < ApplicationController
     length = params[:side_length]
     radius = params[:border_radius]
     color = params[:color]
-    Square.create(side_length:length, border_radius:radius, color:color)
-    redirect_to '/squares'
+    @square = Square.create(side_length:length, border_radius:radius, color:color)
+    redirect_to '/squares/#{@square.id}'
+  end
 
+  def show
+    @square = Square.find(params[:id])
+  end
+
+  def edit
+    @square = Square.find(params[:id])
+  end
+
+  def update
+    edited_square = Square.find(params[:id])
+    edited_square.update(params.require(:square).permit(:side_length))
+    # = params(:side_length)
+    # # border_radius = params(:border_radius)
+    # # color = params(:color)
+    redirect_to "/squares/#{edited_square.id}"
+  end
+
+  def destroy
+    Square.delete(params[:id])
+    redirect_to '/squares'
   end
 
 end
