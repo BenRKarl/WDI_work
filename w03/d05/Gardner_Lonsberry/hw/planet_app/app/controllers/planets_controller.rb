7b99fe6dd0e91e489f68122764a8bc587147814e
glonsberry <gardnerlonsberry@gmail.com>
2014-05-11 19:33:49 -0400

    all pages added. pre-create tables in index

--- w03/d05/Gardner_Lonsberry/hw/planet_app/app/controllers/planets_controller.rb
+++ w03/d05/Gardner_Lonsberry/hw/planet_app/app/controllers/planets_controller.rb
@@ -1,1 +1,23 @@
 class PlanetsController < ApplicationController
+
+# HTTP Verb       PATH         CONTROLLER#ACTION                 BEHAVIOR
+#   GET         /planets         planets#index        display a list of all planets
+  def index
+    @planets = Planet.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+
+end
+
