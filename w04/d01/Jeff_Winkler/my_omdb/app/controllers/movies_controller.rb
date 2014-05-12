93c51448064be301094cfd89a7460d22c3c4d4e2
jwink <jeff_winkler@yahoo.com>
2014-05-12 15:29:02 -0400

    movies

---
+++ w04/d01/Jeff_Winkler/my_omdb/app/controllers/movies_controller.rb
@@ -0,0 +1,16 @@
+class MoviesController < ApplicationController
+  def index
+  end
+
+  def search
+
+    @movies = [Movie.new("The Matrix"), Movie.new("The Other Matrix")]
+  end
+
+  class Movie
+    attr_reader :name
+    def initialize name
+      @name=name
+    end
+  end
+end
