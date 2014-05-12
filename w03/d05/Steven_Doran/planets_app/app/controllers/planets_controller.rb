d2a572717e291fdb292474a9cac46f333fc2b25c
pantsonsteven <pantsonsteven@gmail.com>
2014-05-12 07:33:51 -0400

    squares app operational

--- w03/d05/Steven_Doran/planets_app/app/controllers/planets_controller.rb
+++ w03/d05/Steven_Doran/planets_app/app/controllers/planets_controller.rb
@@ -8,12 +8,27 @@ class PlanetsController <ApplicationController
   end
 
   def create
-    new_planet = Planet.create(params.require(:planet).permit(:name), params.require(:planet).permit(:image_url), params.require(:planet).permit(:diameter), params.require(:planet).permit(:mass), params.require(:planet).permit(:mass), params.require(:planet).permit(:life))
+    new_planet = Planet.create(params.require(:planet).permit(:name, :image_url, :diameter, :mass, :life))
     redirect_to "/planets/#{new_planet.id}"
+  end
+
+  def edit
+    @planet = Planet.find(params[:id])
+  end
+
+  def update
+    edited_planet = Planet.find(params[:id])
+    edited_planet.update(params.require(:@planet).permit(:name, :image_url, :diameter, :mass, :life))
+    redirect_to "/planets/#{edited_planet.id}"
   end
 
   def show
     @planet = Planet.find(params[:id])
+  end
+
+  def destroy
+    Planet.delete(params[:id])
+    redirect_to '/planets'
   end
 
 end
---
+++ w03/d05/Steven_Doran/squares_app/app/helpers/squares_controller_helper.rb
@@ -0,0 +1,2 @@
+module SquaresControllerHelper
+end
