d739ced23fef7328f978dbe30beb2e8ff01040ee
jwink <jeff_winkler@yahoo.com>
2014-05-12 15:56:43 -0400

    before tests

--- w04/d01/Jeff_Winkler/my_omdb/app/controllers/movies_controller.rb
+++ w04/d01/Jeff_Winkler/my_omdb/app/controllers/movies_controller.rb
@@ -4,13 +4,15 @@ class MoviesController < ApplicationController
 
   def search
 
-    @movies = [Movie.new("The Matrix"), Movie.new("The Other Matrix")]
+    @movies = Movie.search params.fetch(:name)
+    @movies = @movies.sort_by {|movie| movie.title}
   end
 
-  class Movie
-    attr_reader :name
-    def initialize name
-      @name=name
-    end
+  def show
+    @movie = Movie.find params.fetch(:title), params.fetch(:year)
+
+
+
   end
+
 end
