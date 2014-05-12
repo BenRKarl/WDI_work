d44b9375cfb6195741e23329ff2046e4d8021a39
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-12 17:29:34 -0400

    morning work

--- w04/d01/Kristen_Tonga/morning_exercise_chefs/config/routes.rb
+++ w04/d01/Kristen_Tonga/morning_exercise_chefs/config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-
-  get '/' => 'squares#index'
-  get 'squares' => 'squares#index'
-  get 'squares/new' => 'squares#new'
-  post 'squares' => 'squares#create'
-  get 'squares/:id' => 'squares#show'
-  get 'squares/:id/edit' => 'squares#edit'
-  put 'squares/:id' => 'squares#update'
-  delete 'squares/:id' => 'squares#destroy'
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
