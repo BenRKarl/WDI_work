771806feedef6058e5ea348797800b6a03328776
Aldric Giacomoni <trevoke@gmail.com>
2014-05-12 12:10:41 -0400

    Add student show

--- w04/d01/INSTRUCTORS/attendance/app/controllers/students_controller.rb
+++ w04/d01/INSTRUCTORS/attendance/app/controllers/students_controller.rb
@@ -2,4 +2,9 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = Student.find params.fetch(:id)
+    @check_ins = @student.check_ins
+  end
 end
--- w04/d01/INSTRUCTORS/attendance/spec/controllers/students_controller_spec.rb
+++ w04/d01/INSTRUCTORS/attendance/spec/controllers/students_controller_spec.rb
@@ -8,4 +8,13 @@ describe StudentsController do
       expect(assigns(:students)).to match_array [s1, s2]
     end
   end
+
+  describe '#show' do
+    it 'has a student' do
+      s1 = Student.create name: 'Lichard'
+      get :show, id: s1.id
+      expect(assigns(:student)).to eq s1
+    end
+    #TODO write a test to verify we have check_ins
+  end
 end
