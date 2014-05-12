71397d99e260866fc9826e0ba2fa41cb4a4bc50f
glonsberry <gardnerlonsberry@gmail.com>
2014-05-11 20:43:35 -0400

    #create works. Need to gsub mass and diameter. Create table on index.

--- w03/d05/Gardner_Lonsberry/hw/planet_app/app/controllers/planets_controller.rb
+++ w03/d05/Gardner_Lonsberry/hw/planet_app/app/controllers/planets_controller.rb
@@ -1,15 +1,35 @@
 class PlanetsController < ApplicationController
 
 # HTTP Verb       PATH         CONTROLLER#ACTION                 BEHAVIOR
+
 #   GET         /planets         planets#index        display a list of all planets
   def index
     @planets = Planet.all
   end
 
+#   GET         /planets/new      planets#new         return an HTML form for creating a new planet 
   def new
+    @planet = Planet.create
+    @planets = Planet.all
+#show a form
   end
 
+#   POST        /planets          planet#create       create a new planet
+  def create
+    @planet       = Planet.create
+    @planets      = Planet.all
+    new_planet    = Planet.create(params.require(:planets).permit(:planet_name, :planet_url, :diameter, :mass, :life) ) 
+    @new_planet   = new_planet
+    @planet_name  = params[:planet_name]
+    @planet_url   = params[:planet_url]
+    @diameter     = params[:diameter] 
+    @mass         = params[:mass]
+    @life         = params[:life]
+    redirect_to "/planets/#{new_planet.id}"
+  end
+  
   def show
+    # redirect_to "/planets" 
   end
 
   def edit
@@ -18,6 +38,14 @@ class PlanetsController < ApplicationController
   def delete
   end
 
+
+def diameter_id=(num)
+  #if we got a string, strip commas out of it
+  num.gsub!(',','') if num.is_a?(String)
+  #then call to_i and store the value as usual.
+  self[:planet_id] = num.to_i
+
+end
 
 end
 
