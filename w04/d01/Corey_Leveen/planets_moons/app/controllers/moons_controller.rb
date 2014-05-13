9b8a30b1a7f65d2ab40fbff6a488ccbf6bc9f370
mntj <mntj11@gmail.com>
2014-05-13 00:53:42 -0400

    moon views

--- w04/d01/Corey_Leveen/planets_moons/app/controllers/moons_controller.rb
+++ w04/d01/Corey_Leveen/planets_moons/app/controllers/moons_controller.rb
@@ -1,18 +1,34 @@
 class MoonsController < ApplicationController
 
   def new
+
   end
 
   def create
+      @moon = Moon.create(moon_params )
+      redirect_to "/moons/#{moon.id}"
   end
 
   def edit
+    @planet = Planet.find(params[:planet_id])
+    @moon = Moon.find(params[:id])
   end
 
   def update
+    moon = Moon.find(params[:id])
+    moon.update(moon_params )
+    redirect_to "/planets/#{planet.id}"
   end
 
   def destroy
+    Moon.delete(params[:id])
+    redirect_to "/planets"
+  end
+
+  private
+
+  def moon_params 
+    params.require(:moon).permit(:name, :planet_id)
   end
 
 end
