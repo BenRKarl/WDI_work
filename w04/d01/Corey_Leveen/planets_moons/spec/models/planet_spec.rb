534597f0f5dca763b03fa037c613cd42daf18d5e
mntj <mntj11@gmail.com>
2014-05-13 08:58:46 -0400

    added test

--- w04/d01/Corey_Leveen/planets_moons/spec/models/planet_spec.rb
+++ w04/d01/Corey_Leveen/planets_moons/spec/models/planet_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Planet do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'should produce an appropriate response to a to_s method' do
+    planet = Planet.create( { name: "Betelgeuse", mass: "1000", diameter: "1000", life: true} )
+    actual = planet.to_s
+    expect(actual).to eq("Betelgeuse is a planet with a mass of 1000, a diameter of 1000, and its life status is true. It has 0 moons.")
 end
