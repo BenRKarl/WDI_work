ccbed73e952a2e8fa8dee04a9b81555286d1952a
HeidiW <foywilliams@gmail.com>
2014-05-13 19:49:55 -0400

    Working planets_moons

---
+++ w04/d02/Heidi_Williams-Foy/planets_moons/app/controllers/planets_controller.rb
@@ -0,0 +1,52 @@
+class PlanetsController < ApplicationController
+
+#planets GET    /planets(.:format)   planets#index
+def index
+  @planet = Planet.all
+end
+
+#POST   /planets(.:format)  planets#create
+def create
+	@planet = Planet.create(planet_params)
+	redirect_to "/planets/#{@planet.id}"
+end
+
+#new_planet GET    /planets/new(.:format)  planets#new
+def new
+	@planet = Planet.new
+end
+
+#edit_planet GET    /planets/:id/edit(.:format)  planets#edit
+def edit
+	@planet = Planet.find(params[:id])
+end
+
+ #planet GET    /planets/:id(.:format)  planets#show
+ def show
+ 	@planet = Planet.find(params[:id])
+ end
+
+#PUT    /planets/:id(.:format)  planets#update
+def update
+  @planet = Planet.find(planet_params[:id])
+  planet.update(planet_params)
+  redirect_to "/planets/#{planet.id}"
+end
+
+#DELETE /planets/:id(.:format)  planets#destroy
+def destroy
+	planet.delete(params[:id])
+	planet.update(planet.params)
+	redirect_to "/planets"
+end
+
+private
+   def planet_params
+    params.require(:planet).permit(:name, :image_url, :diameter, :mass, :life)
+   end
+end
+
+
+                 
+
+
