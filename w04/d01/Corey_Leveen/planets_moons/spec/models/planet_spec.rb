293c99a370eb6517a7159b1568d906471560b680
mntj <mntj11@gmail.com>
2014-05-13 09:36:26 -0400

    specced

--- w04/d01/Corey_Leveen/planets_moons/spec/models/planet_spec.rb
+++ w04/d01/Corey_Leveen/planets_moons/spec/models/planet_spec.rb
@@ -6,4 +6,5 @@ describe Planet do
     planet = Planet.create( { name: "Betelgeuse", mass: "1000", diameter: "1000", life: true} )
     actual = planet.to_s
     expect(actual).to eq("Betelgeuse is a planet with a mass of 1000, a diameter of 1000, and its life status is true. It has 0 moons.")
+  end
 end
