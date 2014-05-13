5b91cb9b1e4e1b4f542d4e3967dc859efa773b98
thannes30 <thannes30@yahoo.com>
2014-05-13 07:39:39 -0400

    added test to planet_spec

--- w04/d01/Tim_Hannes/afternoon/planet_moon_app/spec/models/planet_spec.rb
+++ w04/d01/Tim_Hannes/afternoon/planet_moon_app/spec/models/planet_spec.rb
@@ -3,3 +3,13 @@ require 'spec_helper'
 describe Planet do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Planet do
+  it 'has a name' do
+    planet = Planet.create({name: 'Venus'})
+    actual = planet.name
+    expectation = 'Venus'
+    expect(actual).to_eq(expectation)
+  end
+
+#I'm not sure what a to_s test would look like?
