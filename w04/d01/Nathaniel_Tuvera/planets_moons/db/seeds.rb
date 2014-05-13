3d97000731bb297f91f852f9cfe5b2738945018d
ntuvera <ntuvera@gmail.com>
2014-05-13 08:57:29 -0400

    added functionality and links -- still no rspec test

--- w04/d01/Nathaniel_Tuvera/planets_moons/db/seeds.rb
+++ w04/d01/Nathaniel_Tuvera/planets_moons/db/seeds.rb
@@ -1,7 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+earth = Planet.create({name: 'Earth', image_url: 'http://www.photoshopgurus.com/forum/attachments/general-photoshop-board/3134d1302575637t-well-i-give-up-can-someone-show-me-how-do-technique-planet_glow_00.png', diameter: 7918, mass: 5.972, life: true})
