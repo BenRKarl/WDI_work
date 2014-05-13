87d306720bb8315d5268f1e7e743697ff911edcf
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-13 14:44:07 -0400

    Make test REALLY pass

--- w04/d01/Nessa_Nguyen/planets_n_moons/app/models/planet.rb
+++ w04/d01/Nessa_Nguyen/planets_n_moons/app/models/planet.rb
@@ -3,13 +3,12 @@ class Planet < ActiveRecord::Base
 
   def to_s  
     planet = Planet.create({name: name, life: life})
-    planet.moons << Moon.new
-    if planet.life == true
+    if self.life == true
       life_form = ""
     else
       life_form = "not "
     end    
 
-    "#{planet.name} is a planet with #{ planet.moons.length } moon(s), #{life_form}capable of sustaining life."    
+    "#{self.name} is a planet with #{ self.moons.length } moon(s), #{life_form}capable of sustaining life."    
   end
 end
