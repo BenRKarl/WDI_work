5db96ae6c79c4aea60bbaa26ba9fc65fa705702e
pantsonsteven <pantsonsteven@gmail.com>
2014-05-13 12:11:06 -0400

    morning commit

--- w04/d02/Steven_Doran/class_proto/db/schema.rb
+++ w04/d02/Steven_Doran/class_proto/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513135400) do
+ActiveRecord::Schema.define(version: 20140513141853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20140513135400) do
   create_table "protos", force: true do |t|
     t.string   "name"
     t.date     "birthday"
-    t.string   "zodiac_sign"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
