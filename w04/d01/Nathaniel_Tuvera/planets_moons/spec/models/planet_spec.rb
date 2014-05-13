db745952df2d882671dd310feb1fc822b4bc8b5c
ntuvera <ntuvera@gmail.com>
2014-05-13 15:39:10 -0400

    clearing status, middle of skeleton $ fa icons

--- w04/d01/Nathaniel_Tuvera/planets_moons/spec/models/planet_spec.rb
+++ w04/d01/Nathaniel_Tuvera/planets_moons/spec/models/planet_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-  describe Planets do
+  describe @planet do
     describe "display string: Planet has X number of moons." do
     # wish code that works
-    moon = Moon.create(:name => 'Luna')
+    moon = Array.new
+    moon << Moon.create(:name => 'Luna')
     puts "Planet has #{moon.count} number(s) of moons."
-    actual = @moon.count
+    actual = @moon
     expectation = "Planet has 1 number(s) of moons."
     # set expectation
-    expect(actual).to eq(expectation)
+    expected(actual).to eq(expectation)
     end
   end
-end
