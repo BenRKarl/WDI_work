61456bd68e26ee64033634f7328d650eaab0cc4d
wakelank <wakelank@gmail.com>
2014-05-16 15:22:36 -0400

    andrew

---
+++ w04/d02/Wake_Lankard/squares2_app/app/controllers/squares_controller.rb
@@ -0,0 +1,50 @@
+class SquaresController < ApplicationController
+
+
+  def index
+    @squares = Square.all
+  end
+
+  def new
+    @square = Square.new
+  end
+
+  def create
+    square_params = params.require(:square).permit(:side_length, :border_radius, :color)
+    new_square = Square.create(square_params)
+    # length = params.require(:square).permit(:side_length)
+    # length[:length] = length[:side_length].to_i
+    # radius = params.require(:square).permit(:border_radius)
+    # new_square = Square.create(side_length: length, border_radius: radius)
+
+    redirect_to "/squares/#{ new_square.id }"
+  end
+
+  def edit
+    @square = Square.find(params[:id])
+
+  end
+
+  def update
+    # length = params.require(:square).permit(:length)
+    # radius = params.require(:square).permit(:radius)
+    square_params = params.require(:square).permit(:side_length, :border_radius, :color)
+    id = (params[:id])
+    square = Square.find(id)
+    # Square.update({ side_length: length, border_radius: radius })
+    square.update(square_params)
+
+    redirect_to "/squares/#{id}"
+
+  end
+
+  def show
+    @square = Square.find(params[:id])
+  end
+
+  def destroy
+    Square.delete(params[:id])
+
+    redirect_to "/"
+  end
+end
--- w05/Wake_Lankard/README.md
+++ w05/Wake_Lankard/README.md
@@ -1,3 +1,5 @@
 Name: Wake_Lankard
 Email: wakelank@gmail.com
-GitHub: 
+GitHub: https://github.com/wakelank/Tinsl
+Heroku: http://tinsl.herokuapp.com/
+Trello: https://trello.com/b/JCslX0Gh/tinsl
