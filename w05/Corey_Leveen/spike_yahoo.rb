2b2a1c336b5e20f4eef233100546208b0a49817d
mntj <mntj11@gmail.com>
2014-05-17 18:15:53 -0400

    got yahoo working

--- w05/Corey_Leveen/spike_yahoo.rb
+++ w05/Corey_Leveen/spike_yahoo.rb
@@ -15,6 +15,20 @@ arr
 # gem install 'yahoo-finance'
 # require 'yahoo_finance'
 
-# Returns trading data for Google for the last 8 days in an array of objects
-YahooFinance.historical_quotes("GOOG", Time::now-(24*60*60*10), Time::now, { raw: false, period: :daily })
+# Returns trading data for Google for the last 7 days in an array of objects
+YahooFinance.historical_quotes("GOOG", Time::now-(24*60*60*10), Time::now, { raw: false, period: :daily }).take(7)
 
+# Returns an array of average share price for a company for the week
+def price_history(ticker)
+
+# Assigns an array of price objects for the last seven days to obj_arr
+ obj_arr = YahooFinance.historical_quotes("#{ticker}", Time::now-(24*60*60*10), Time::now, { raw: false, period: :daily }).take(7)
+
+  avg_arr = []
+
+  obj_arr.each do |x|
+    avg_arr << (x.high + x.low)/2.to_i
+  end
+
+  avr_arr
+end
