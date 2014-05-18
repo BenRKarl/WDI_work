6ae1d948f0530aff9913323c732c5cef7ac7e888
mntj <mntj11@gmail.com>
2014-05-18 18:53:06 -0400

    finished spiking

--- w05/Corey_Leveen/spike_yahoo.rb
+++ w05/Corey_Leveen/spike_yahoo.rb
@@ -1,5 +1,3 @@
-spike_yahoo.rb
-
 
 # This returns the dates of the past week in an array, sorted oldest to newest
 i = 7
@@ -18,6 +16,8 @@ last_weeks_dates
 # Returns trading data for Google for the last 7 days in an array of objects
 YahooFinance.historical_quotes("GOOG", Time::now-(24*60*60*10), Time::now, { raw: false, period: :daily }).take(7)
 
+
+###################################################################################
 # Returns an array of average share price for a company for the week
 def price_history(ticker)
 
