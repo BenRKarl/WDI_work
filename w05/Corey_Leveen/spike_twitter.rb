193d2a8d7498893268df30c11e8e470b379d3c41
mntj <mntj11@gmail.com>
2014-05-18 17:42:01 -0400

    more on twitter

--- w05/Corey_Leveen/spike_twitter.rb
+++ w05/Corey_Leveen/spike_twitter.rb
@@ -81,6 +81,32 @@ class Tweet_array # < ActiveRecord::Base
       :consumer_key    => ENV['TWITTER_KEY'],
       :consumer_secret => ENV['TWITTER_SECRET']
     }
+
+    client = Twitter::REST::Client.new(config)
+
+
+    # Returns the number of tweets containing 'bloomberg' on 05-12
+    #client.search("bloomberg since:2014-05-12 until:2014-05-13").count
+
+    # Returns an array in chronological order of the dates of the past 7 days
+    i = 7
+    dates_arr = []
+    loop do
+      dates_arr << Date.today.prev_day(n=i).to_s
+      i -= 1
+      break if i < 1
+    end
+    # Stores the number of tweets from each day last week into tweet_count_arr
+    count = 0
+    tweet_count_arr = []
+    while count < 7
+    client.search(query + " since:#{dates_arr[count]} until: ")
+
+  def self.search(query)
+    config = {
+      :consumer_key    => ENV['TWITTER_KEY'],
+      :consumer_secret => ENV['TWITTER_SECRET']
+    }
     client = Twitter::REST::Client.new(config)
 
     # Returns an array in chronological order of the dates of the past 7 days
@@ -102,5 +128,7 @@ class Tweet_array # < ActiveRecord::Base
     end
      @tweet_count_arr
   end
+
+
 
 end
