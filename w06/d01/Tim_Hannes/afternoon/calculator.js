adf8aa65c7fea53d85aaf88519abc71a40462055
thannes30 <thannes30@yahoo.com>
2014-05-27 18:39:03 -0400

    afternoon commit

---
+++ w06/d01/Tim_Hannes/afternoon/calculator.js
@@ -0,0 +1,22 @@
+function add(x,y){
+  return(x + y);
+}
+
+function subtract(x,y){
+  return(x - y);
+}
+
+function multiply(x,y){
+  return(x * y);
+}
+
+function divide(x,y){
+  return(x / y);
+}
+
+
+console.log(add(5,5))
+console.log(subtract(5,2))
+console.log(multiply(10, 5))
+console.log(divide(15,5))
+
