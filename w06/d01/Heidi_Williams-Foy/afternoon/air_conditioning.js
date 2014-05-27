c900cbb1fdc627ab9d8584069f9dffcbfa651d5e
HeidiW <foywilliams@gmail.com>
2014-05-27 16:11:20 -0400

    condition air exercise complete

--- w06/d01/Heidi_Williams-Foy/afternoon/air_conditioning.js
+++ w06/d01/Heidi_Williams-Foy/afternoon/air_conditioning.js
@@ -3,3 +3,12 @@
 - Write a `conditionAir` function takes a desired temperature as a parameter
   - If the current temperture is above the the desired temperature... display "Turn on the A/C please"
   - If the current temperture is below the the desired temperature... display "Turn off the A/C please"*/
+
+  function conditionAir(desiredTemperature){
+   if (desiredTemperature > 70){
+ 	var note = 'Turn on the A/C please';
+   } else if (desiredTemperature < 70){
+   	var note = 'Turn off the A/C please';
+   }
+   return note;
+  }
