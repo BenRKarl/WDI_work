80f0d80685f4f842abb460a590656df0364c1002
mntj <mntj11@gmail.com>
2014-05-27 18:16:24 -0400

    strings fail and calculator maybe

--- w06/d01/Corey_Leveen/js/strings.js
+++ w06/d01/Corey_Leveen/js/strings.js
@@ -10,3 +10,24 @@ function removeVowels(str) {
   var obj = new String(str);
   return obj.replace(/[aeiou]/gi, '');
 }
+
+function theGreat(str) {
+  var obj = new String(str);
+  return obj.replace('the', 'the great');
+}
+
+
+// doesn't work yet
+function capitals(str) {
+  var obj = new String(str);
+  arr = obj.split(" ");
+  arr.forEach(function (str, indx){
+    arr[indx].replace(arr[indx][0], str[0].toUpperCase());
+    console.log("str[0] = "+ str[0].toUpperCase());
+    console.log(arr[indx]);
+    return arr.join(" ");
+  })
+  console.log(arr);
+}
+
+capitals('big cow in a pasture')
