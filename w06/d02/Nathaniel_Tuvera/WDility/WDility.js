6c050c3785935acb3d36197c1999d455a434a008
ntuvera <ntuvera@gmail.com>
2014-05-28 16:30:45 -0400

    w06_d02_hw part II-3

---
+++ w06/d02/Nathaniel_Tuvera/WDility/WDility.js
@@ -0,0 +1,32 @@
+var WDility = {
+
+  each: function(array, func) {
+    for (i in array) {
+      func(array[i]);
+    }
+    return array
+  },
+  first: function(array) {
+
+
+
+  },
+  last: function(array) {
+
+
+  },
+  map: function(array, func) {
+
+
+  },
+  select: function(array,func) {
+
+
+  }
+
+};
+
+
+
+
+// var myArray = [1,2,3,4];
