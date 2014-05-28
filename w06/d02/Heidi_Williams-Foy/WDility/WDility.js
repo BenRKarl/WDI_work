e103900e7bbe8c5e50df0b38f521091461ef8208
HeidiW <foywilliams@gmail.com>
2014-05-28 16:52:16 -0400

    WDidility commit 1

---
+++ w06/d02/Heidi_Williams-Foy/WDility/WDility.js
@@ -0,0 +1,17 @@
+var WDility = {
+	first: function(){
+	},
+	last: function(){
+	},
+	each: function(array, func){
+		for (var i in array){
+			func(array[i]);
+		}
+	 	return array
+	 },
+	map:function(){
+
+	},
+	select:function(){
+	}
+};
