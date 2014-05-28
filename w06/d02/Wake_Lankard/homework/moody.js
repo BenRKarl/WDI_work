d481e5720a206da01b9b51e2f6a44a495c161575
wakelank <wakelank@gmail.com>
2014-05-28 16:21:07 -0400

    started homework

---
+++ w06/d02/Wake_Lankard/homework/moody.js
@@ -0,0 +1,19 @@
+function Person(name, age, mood){
+  this.name = name;
+  this.age = age;
+  this.mood = mood;
+}
+
+Person.prototype = {
+  
+  introduce: function(){
+    switch(this.mood) {
+      case 0:
+        output = this.name + ", " + this.age + ". Shut up.";
+        break;
+    }
+  }
+}
+
+myPerson = new Person("John", 23, 0);
+console.log(myPerson.introduce());
