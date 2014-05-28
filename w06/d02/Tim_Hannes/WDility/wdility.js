487b2ea3268be0cc11575e3ca11a3d9e96c0c791
thannes30 <thannes30@yahoo.com>
2014-05-28 18:35:05 -0400

    finished wdility

--- w06/d02/Tim_Hannes/WDility/wdility.js
+++ w06/d02/Tim_Hannes/WDility/wdility.js
@@ -15,10 +15,34 @@ var WDility = {
   },
 
   map: function(array, func){
+    var newArray = []
     for (var i in array){
-      func(array[i]);
+      newArray.push(func(array[i]));
     }
-    return array
+    return newArray
+  },
+
+  select: function(array, func){
+    var newArray = []
+    for (var i in array){
+      if (func(array[i]) == true)newArray.push(array[i]);
+    }
+    return newArray
+  },
+
+  unique: function(array){
+      uniqueArray = array.filter(function(elem, pos){
+        return array.indexOf(elem) == pos;
+      });
+    return uniqueArray
+  },
+
+  reduce: function(array){
+    sum = 0
+    for (var i in array){
+      sum += array[i]
+    }
+    return sum
   },
 
 };
--- w06/d02/Tim_Hannes/homework/moodyPerson.js
+++ w06/d02/Tim_Hannes/homework/moodyPerson.js
@@ -1,1 +1,9 @@
 // need an introduce method on person class
+var People = [
+  {name = 'Josh', age = 25, mood = 4}
+  {name = 'Matt', age = 24, mood = 2}
+  {name = 'Tom', age = 26, mood = 3}
+  {name = 'Ray', age = 24, mood = 7}
+  {name = 'Chris', age = 25, mood = 6}
+]
+
