f4827b457747ae7246108333b51ccef7ed12039b
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-28 18:35:13 -0400

    did the first, last, each, and map fuctions. working on select

--- w06/d02/Kristen_Tonga/WDility/wdility.js
+++ w06/d02/Kristen_Tonga/WDility/wdility.js
@@ -1,8 +1,11 @@
 var WDility = {
-  first: function(){
+  first: function(array){
+    return array[0];
   },
 
-  last: function(){
+  last: function(array){
+    last = array.length - 1;
+    return array[ last];
   },
 
   each: function(array, func){
@@ -12,10 +15,13 @@ var WDility = {
       return array
     },
 
-  map: function(){
+  map: function(array, func){
+    var newArray = array.map(func);
+    return newArray;
   },
 
   select: function(){
+
   }
 
 };
