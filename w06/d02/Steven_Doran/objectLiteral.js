e156a3a4fd00131c385002f1b8dffafae4b26814
pantsonsteven <pantsonsteven@gmail.com>
2014-05-28 15:50:05 -0400

    afternoon work

--- w06/d02/Steven_Doran/objectLiteral.js
+++ w06/d02/Steven_Doran/objectLiteral.js
@@ -8,3 +8,17 @@ var dog = {
         console.log('Hey dude. My name is ' + this.name + '.')
     }
 }
+
+
+function GameDie(){
+}
+
+
+GameDie.prototype = {
+    roll: function(){
+
+        console.log("You rolled a: " + (Math.floor(Math.random()*6) + 1));
+    }
+}
+
+var die = new
