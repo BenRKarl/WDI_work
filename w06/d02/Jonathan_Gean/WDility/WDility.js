413eb2418a9b141a2b392c44fe5f17dc2dfee397
jonathangean <jonathangean@gmail.com>
2014-05-28 16:49:33 -0400

    map

--- w06/d02/Jonathan_Gean/WDility/WDility.js
+++ w06/d02/Jonathan_Gean/WDility/WDility.js
@@ -7,5 +7,15 @@ var WDility = {
   },
  first: function(array){
   return array[0]
- }
+ },
+ last: function(array){
+  return array[array.length -1]
+ },
+ map: function(array, func) {
+      var new_array = []
+    for (i in array){
+     new_array.push(func(array[i]));
+    }
+    return new_array
+  },
 };
