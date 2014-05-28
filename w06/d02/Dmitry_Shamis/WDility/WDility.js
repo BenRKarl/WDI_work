cbf64f0196f55d3483b5dcb26d531fb79aaab353
dshamis317 <dshamis317@gmail.com>
2014-05-28 16:38:07 -0400

    first, last, each

--- w06/d02/Dmitry_Shamis/WDility/WDility.js
+++ w06/d02/Dmitry_Shamis/WDility/WDility.js
@@ -1,3 +1,15 @@
 var WDility = {
+  first: function (array) {
+    return array[0];
+  },
+  last: function (array) {
+    return array[array.length -1];
+  },
+  each: function (array, func) {
+    for (i in array){
+      func(array[i]);
+    }
+    return array
+  },
 
 };
