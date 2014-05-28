9a8263147d33c4ddf977ab23475c93083e47f8b6
pantsonsteven <pantsonsteven@gmail.com>
2014-05-28 16:33:51 -0400

    initial commit

---
+++ w06/d02/Steven_Doran/moodyPerson.js
@@ -0,0 +1,23 @@
+function Person(name, age, mood){
+    this.name = name;
+    this.age = age;
+    this.mood = mood;
+}
+
+Person.prototype = {
+    introduction: function(){
+        if (this.mood < 3) {
+            console.log("I'm " + this.name + " and I'm pretty crappy.");
+        } else if ((this.mood > 2) && (this.mood < 8)){
+            console.log("I'm " + this.name + " and I'm doing all right.");
+        } else if (this.mood > 7){
+            console.log("I'm " + this.name + " and I'm rockin'!");
+        }
+    }
+}
+
+var bill = new Person('bill', 22, 7);
+var susan = new Person('susan', 35, 1);
+var harriet = new Person('harriet', 52, 9);
+var gary = new Person('gary', 14, 6);
+var ginger = new Person('ginger', 76, 15);
--- w06/d02/Steven_Doran/objectLiteral.js
+++ w06/d02/Steven_Doran/objectLiteral.js
@@ -9,16 +9,45 @@ var dog = {
     }
 }
 
+/////////////////////////////
+/////////////////////////////
 
-function GameDie(){
+function GameDie(numSides){
+    this.numSides = numSides;
 }
 
 
 GameDie.prototype = {
     roll: function(){
-
-        console.log("You rolled a: " + (Math.floor(Math.random()*6) + 1));
+        var num = Math.floor(Math.random() * this.numSides) + 1;
+        console.log("You rolled a: " + num);
     }
 }
 
-var die = new
+var mySixSidedDie = new GameDie(6)
+var myEightSidedDie = new GameDie(8)
+
+
+/////////////////////////////
+/////////////////////////////
+
+function Computer(screenSize){
+    this.screenSize = screenSize;
+}
+
+Computer.prototype.osVersion = "Mountain Lion"
+
+
+var comp1 = new Computer("22 inches");
+var comp2 = new Computer("15 inches");
+var comp3 = new Computer("17 inches");
+var comp4 = new Computer("15 inches");
+
+
+Computer.prototype.osVersion = "Mavericks"
+
+
+
+
+
+
