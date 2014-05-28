837b2ca96033cc922de3cc06ecfd8d2751752c97
HeidiW <foywilliams@gmail.com>
2014-05-28 19:00:16 -0400

    WDility javascript library done

--- w06/d02/Heidi_Williams-Foy/WDility/WDility.js
+++ w06/d02/Heidi_Williams-Foy/WDility/WDility.js
@@ -1,7 +1,10 @@
 var WDility = {
-	first: function(){
+	first: function(array){
+		return array[0];
 	},
-	last: function(){
+	last: function(array){
+		length= array.length;
+		return array[ length - 1 ];
 	},
 	each: function(array, func){
 		for (var i in array){
@@ -9,9 +12,17 @@ var WDility = {
 		}
 	 	return array
 	 },
-	map:function(){
-
+	map:function(array, func) {
+		var newArray = array.map(func);
+		return newArray
 	},
-	select:function(){
+	select:function(array, func){
+		var newArray = [];
+		for (var i in array){
+			if (array[i] > 2) {
+				newArray.push(array[i]);
+			}
+		}
+		return newArray;
 	}
 };
