a4e4c611947039c6c579758525b5ec678ccca714
pantsonsteven <pantsonsteven@gmail.com>
2014-05-28 18:10:57 -0400

    adds all functionality except reduce only works if you're summing but whatever

--- w06/d02/Steven_Doran/WDility/WDility.js
+++ w06/d02/Steven_Doran/WDility/WDility.js
@@ -1,6 +1,6 @@
 var WDility = {
     each: function(array, func) {
-        for (var i in array) {
+        for (i in array) {
             func(array[i]);
         }
         return array
@@ -13,18 +13,40 @@ var WDility = {
     },
     map: function(array, func) {
         var newArray = [];
-        for (var i in array){
+        for (i in array){
             newArray[i] = func(array[i]);
         }
         return newArray;
     },
     select: function(array, func) {
         var newArray = [];
-        for (var i in array){
+        for (i in array){
             if (func(array[i])) {
                 newArray.push(array[i]);
             }
         }
+        return newArray;
+    },
+    reduce: function(array, func) {
+        var result = 0;
+        for (i in array) {
+          result = result + array[i];
+        }
+        return result;
+    },
+    unique: function(array) {
+        var newArray = [];
+        for (i in array) {
+            var counter = 0;
+            for (j in newArray) {
+                if (array[i] === array[j]) {
+                    counter += 1;
+                }
+            }
+            if (counter === 0) {
+                newArray.push(array[i]);
+            }
+        }  
         return newArray;
     }
 };
