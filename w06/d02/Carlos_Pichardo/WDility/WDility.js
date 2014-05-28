3bdba78130e5d0c816c2f945c86dda694ec48625
Pichardito <ctpichardo@gmail.com>
2014-05-28 17:49:45 -0400

    ruby methods converted to javascript

--- w06/d02/Carlos_Pichardo/WDility/WDility.js
+++ w06/d02/Carlos_Pichardo/WDility/WDility.js
@@ -1,1 +1,45 @@
-var WDility = {}
+var WDility = {
+
+each: function(array, func) {
+    for (i in array){
+      func(array[i]);
+    }
+    return array
+  },
+
+  first: function(array){
+  	return array[0];
+  	
+  },
+
+  last: function(array){
+  	return array[array.length - 1];
+  },
+
+  map: function(array, func){
+  	var arr = []
+  	for (i in array){
+  		arr.push(func(array[i]));
+	}
+  	return arr;
+  },
+
+  select: function(array, func){
+  	var arr = []
+  	for (i in array){
+  		if ((func(array[i])) == true) {
+  			arr.push(array[i]);
+  		}
+  	}
+  	return arr;
+  }
+
+  // reduce: function(array, function){
+
+  // },
+
+  // unique: function(array){
+
+  // }
+
+}
