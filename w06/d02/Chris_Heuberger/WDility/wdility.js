ba562e3c378f8d6c0028b2bb2711e0a216536185
ChrisBup <christopherheuberger@gmail.com>
2014-05-28 17:14:19 -0400

    commit 2

--- w06/d02/Chris_Heuberger/WDility/wdility.js
+++ w06/d02/Chris_Heuberger/WDility/wdility.js
@@ -1,74 +1,36 @@
-var WDility = {
 
+var WDility = {
+  each: function(array, func) {
+    for (i in array){
+      func(array[i]);
+    }
+    return array
+  },
+  first: function(array) {
+    return array[0];
+  },
+  last: function(array) {
+    var i = (array.length - 1);
+    return array[i];
+  },
+  map: function(array, func) {
+    var newArray = [];
+    for (i in array){
+      newArray.push(func(array[i]));
+    }
+    return newArray;
+  },
+  select: function(array, func) {
+    var newArray = [];
+    for (i in array) {
+      if (func(array[i])) {
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
+  }
 };
 
-// #WDility
-// - Build your own javascript library called WDility
-// - Recreate several **Ruby** methods in javascript
-
-
-// ###Specification:
-
-// - Your WDility.js must include a:
-//   - .first(array)
-//   - .last(array)
-//   - .each(array, function)
-//   - .map(array, function)
-//   - .select(array, function)
-// - Extra:
-//   - .reject(array, function)
-//   - .unique(array)
-
-// ####examples of use:
-
-// ```javascript
-
-// var my_array = [1, 2, 3, 4];
-
-// WDility.first(my_array);       // Should return the value 1
-
-// ```
-
-
-// ```javascript
-
-// var my_array = [1,2,3,4];
-
-// Wdility.map(my_array, function(elem){ return elem * 2}); // Should return this array: [2,4,6,8,10]
-
-
-// ```
-
-// ---
-
-// #####Commit 1
-// - Create a `WDility.js` file
-// - Start the file by naming your library as shown here:
-
-// ```javascript
-
-// ```
-
-// ---
-
-// #####Commit 2
-// - Build out a .each functionaity as shown here:
-
-// ```javascript
-
-// var WDility = {
-//   each: function(array, func) {
-//     for (i in array){
-//       func(array[i]);
-//     }
-//     return array
-//   },
-
-// };
-
-// ```
-
-// ---
 
 // #####Commit 2-5
 // - Implement:
