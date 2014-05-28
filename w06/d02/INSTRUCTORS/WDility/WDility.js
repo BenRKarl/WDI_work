a5473b141dcb381c666db602eda050f0b6f181fe
amadden80 <amadden80@gmail.com>
2014-05-28 16:17:43 -0400

    computers and wditily

---
+++ w06/d02/INSTRUCTORS/WDility/WDility.js
@@ -0,0 +1,13 @@
+
+var WDility = {
+  each: function(array, func) {
+    for (i in array){
+      func(array[i]);
+    }
+    return array
+  },
+  first: function(array){
+    return array[0]
+  }
+
+};
