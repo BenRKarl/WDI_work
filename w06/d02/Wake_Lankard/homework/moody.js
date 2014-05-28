7d8861cccbe3704cefeb9425fab4d01d623c8b0b
wakelank <wakelank@gmail.com>
2014-05-28 17:55:06 -0400

    completes moody people

--- w06/d02/Wake_Lankard/homework/moody.js
+++ w06/d02/Wake_Lankard/homework/moody.js
@@ -6,18 +6,47 @@ function Person(name, age, mood){
 
 Person.prototype = {
 
+
+
   introduce: function(){
-    console.log("I'm in.");
+    var output = "";
+    switch(this.mood) {
+      case 0:
+      case 1:
+      case 2:
+        output = this.name + ", " + this.age + ". Shut up.";
+        break;
+      case 3:
+      case 4:
+      case 5:     
+        output = "I'm " + this.name + " I'm " + this.age + ".  I'm fine."
+        break;
+      case 6:
+      case 7:
+      case 8:
+        output = "My name's " + this.name + " I'm " + this.age + "-years old, and I'm pretty good."
+        break;
+      case 9:
+      case 10:
+        output = "Hi! I'm " + this.name + ".  I'm " + this.age + ", and I am totally awesome.";
+        break;
+      default:
+        output = "I'm " + this.name + ". I am " + this.age + ". I have no idea how I feel right now.";
+
+
+    }
+    return output;
   }
 
-  // introduce: function(){
-  //   switch(this.mood) {
-  //     case 0:
-  //       output = this.name + ", " + this.age + ". Shut up.";
-  //       break;
-  //   }
-  // }
+}
+var people = [];
+people[0] = new Person("John", 23, 2);
+people[1] = new Person("Susan",34, 7);
+people[2] = new Person("Jane", 9, 10);
+people[4] = new Person("Frank", 14, 4);
+people[3] = new Person("Edward", 23, 11);
+
+for (i in people){
+  console.log(people[i].introduce())
 }
 
-myPerson = new Person("John", 23, 0);
-console.log(myPerson.introduce());
