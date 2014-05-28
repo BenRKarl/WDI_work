6c8286739c80fa07e7c315c02392839fe1c58c38
dshamis317 <dshamis317@gmail.com>
2014-05-28 17:41:07 -0400

    map, select

--- w06/d02/Dmitry_Shamis/WDility/WDility.js
+++ w06/d02/Dmitry_Shamis/WDility/WDility.js
@@ -3,13 +3,28 @@ var WDility = {
     return array[0];
   },
   last: function (array) {
-    return array[array.length -1];
+    return array[array.length - 1];
   },
   each: function (array, func) {
     for (i in array){
       func(array[i]);
     }
     return array
+  },
+  map: function (array, func) {
+    var new_array = []
+    for (i in array) {
+      new_array.push(func(array[i]));
+    }
+    return new_array
+  },
+  select: function (array, func) {
+    var new_array = []
+    for (i in array) {
+      if (func(array[i]) == true)
+      new_array.push(array[i]);
+    }
+    return new_array
   },
 
 };
