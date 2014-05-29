3f3817544b94729305fb55035b51d326b5a46873
glonsberry <gardnerlonsberry@gmail.com>
2014-05-28 23:54:03 -0400

    HW done

--- w06/d02/Gardner_Lonsberry/WDility/WDility.js
+++ w06/d02/Gardner_Lonsberry/WDility/WDility.js
@@ -3,22 +3,46 @@ var WDility = {
     for (i in array){
       func(array[i]);
             }
-    return array;
+    return array
   },
+  
   first: function(array){
-    return array[0];
-},
+    return array[0]
+  },
+  
   last: function(array){
     array[array.length-1];
-},
-  map: function(array, function(array){ 
-    return array
-},
-  select: function(elem){
-    return elem === elem;
-}
-}
+  },
+  
+  map: function(array, func){
+    var newArray = []
+    for (var i in array){
+      newArray.push(func(array[i]));
+    }
+    return newArray
+  },
+ select: function(array, func){
+    var newArray = []
+    for (var i in array){
+      if (func(array[i]) == true)newArray.push(array[i]);
+    }
+    return newArray
+  }, 
+ unique: function(array){
+      uniArray = array.filter(function(elem, pos){
+        return array.indexOf(elem) == pos;
+      });
+    return uniArray
+  }, 
+reduce: function(array){
+    sum = 0
+    for (var i in array){
+      sum += array[i]
+    }
+    return sum
+  },
 
+};
 
 
 
@@ -36,3 +60,17 @@ var WDility = {
 
 // var filtered = array.filter(function(x) { return x === x; });
 // console.log(filtered);
+
+
+
+// function sort_unique(arr) {
+//     arr = arr.sort(function (a, b) { return a*1 - b*1; });
+//     var ret = [arr[0]];
+//     for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+//         if (arr[i-1] !== arr[i]) {
+//             ret.push(arr[i]);
+//         }
+//     }
+//     return ret;
+// }
+// console.log(sort_unique(['237','124','255','124','366','255']));
