e47b4b0c84c631620ba141f667e7a5307c595307
HeidiW <foywilliams@gmail.com>
2014-05-28 20:18:15 -0400

    moody person

---
+++ w06/d02/Heidi_Williams-Foy/homework/moody_person.js
@@ -0,0 +1,22 @@
+//var myPersonArray = ['John', 'Mary', 'Ava', 'Maeve', 'Aidan'];
+
+function People(name, age, mood){
+  this.name = name,
+  this.age = age,
+  this.mood = mood
+}	
+
+People.prototype = {
+	introduce: function () {
+     if (this.mood === "happy") {
+     		console.log("Hi, I'm happy");
+     } else if (this.mood === "angry") {
+     		console.log(this.name + "Don't talk to me right now.");
+     } else {
+     		console.log("My name is " + this.name + ". I am " + this.age + " years old, and I am"  + this.mood + ".");
+     }
+	}
+}
+
+
+
