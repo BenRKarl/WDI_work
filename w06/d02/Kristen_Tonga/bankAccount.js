af37fc06546fb21b6c69c980f484808bd4244785
Kristen Tonga <kristen.tonga@gmail.com>
2014-05-28 20:02:55 -0400

    yay. hw done before 10. made bank acount work for deposits and withdrawls

--- w06/d02/Kristen_Tonga/bankAccount.js
+++ w06/d02/Kristen_Tonga/bankAccount.js
@@ -6,6 +6,18 @@ BankAccount.prototype = {
   makeDeposit: function(deposit){
     this.balance += deposit;
     return this.balance;
+  },
+
+  makeWithdrawl: function(withdrawl){
+    if ( this.balance - withdrawl > 0 ){
+      this.balance -= withdrawl;
+      var note = "Success!"
+      return this.balance;
+    } else {
+      var note = "You do not have enough money to make that withdrawl.";
+      return note;
+    }
+
   }
 }
 
