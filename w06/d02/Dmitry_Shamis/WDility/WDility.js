d2cd7f396efb36bcc519a9478b0b32baaa004252
dshamis317 <dshamis317@gmail.com>
2014-05-29 09:55:07 -0400

    morning exercises

--- w06/d02/Dmitry_Shamis/WDility/WDility.js
+++ w06/d02/Dmitry_Shamis/WDility/WDility.js
@@ -2,49 +2,68 @@ var WDility = {
   first: function (array) {
     return array[0];
   },
+
   last: function (array) {
     return array[array.length - 1];
   },
+
   each: function (array, func) {
-    for (i in array){
+    for (var i in array){
       func(array[i]);
     }
     return array
   },
+
   map: function (array, func) {
-    var new_array = []
-    for (i in array) {
-      new_array.push(func(array[i]));
+    var newArray = []
+    for (var i in array) {
+      newArray.push(func(array[i]));
     }
-    return new_array
+    return newArray
   },
+
   select: function (array, func) {
-    var new_array = []
-    for (i in array) {
+    var newArray = []
+    for (var i in array) {
       if (func(array[i]) == true)
-        new_array.push(array[i]);
+        newArray.push(array[i]);
     }
-    return new_array
+    return newArray
   },
+
   unique: function (array) {
-    var new_array = []
-    for (i in array) {
-      if (new_array.indexOf(array[i]) === -1) {
-        new_array.push(array[i]);
+    var newArray = []
+    for (var i in array) {
+      if (newArray.indexOf(array[i]) === -1) {
+        newArray.push(array[i]);
       }
     }
-    return new_array;
+    return newArray;
   },
+
   reduce: function (array, func) {
-    var new_array = []
-    for (i in array) {
-      new_array.push(array[i]);
+    var memo = array.shift();
+    for (var i in array){
+      memo = func(memo, array[i]);
     }
-      new_array.reduce(function(previousValue, currentValue, index, array) {
-        return previousValue + currentValue;
-      })
+    return memo
+  },
+
+  delete: function (array, value) {
+    var newArray = [];
+    for (var i in array) {
+      if (array[i] != value) {
+        newArray.push(array[i]);
+      }
     }
-  };
+    return newArray
+  },
+
+  deleteAt: function (array, index) {
+    array.splice(index, 1);
+    return array;
+  }
+};
 
 
 
