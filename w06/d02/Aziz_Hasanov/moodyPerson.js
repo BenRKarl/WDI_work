b179675b2cda087a0e0e6e3c09ca3d664f069c43
aziz-ka <hasanov.aziz@gmail.com>
2014-05-28 23:29:43 -0400

    homework is done

--- w06/d02/Aziz_Hasanov/moodyPerson.js
+++ w06/d02/Aziz_Hasanov/moodyPerson.js
@@ -1,24 +1,27 @@
-var people = ['bob', 'sam', 'joe', 'kim', 'ken'];
-
-// var people = [];
-
 function Person(name, age, mood) {
   this.name = name;
   this.age = age;
-  // this.mood = mood;
+  this.mood = mood;
 }
 
 Person.prototype = {
-  introduce: function(mood) {
-    for (i in people) {
-      if (mood == "happy") {
-        console.log("Hi, I am " + people[i] + "Pleased to meet you!");
-      }
-      else {
-        console.log("Hi... " + people[i] + "here...");
-      }
+  introduce: function() {
+    if (this.mood === "happy") {
+      console.log("Hi! I am " + this.name + " Pleased to meet you");
+    }
+    else{
+      console.log("Hi... " + this.name + " here...");
     }
   }
 }
 
-var new_person = new Person("aziz", 25, "happy");
+var aziz = new Person("aziz", 25, "happy");
+var kim = new Person("kim", 30, "angry");
+var sam = new Person("sam", 25, "unhappy");
+var bob = new Person("bob", 25, "sad");
+var joe = new Person("joe", 25, "happy");
+
+var people = [aziz, kim, sam, bob, joe];
+
+// var people = []
+// people.push(aziz, kim, sam, bob, joe);
