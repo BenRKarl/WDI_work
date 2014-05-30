f72469bd7231afc8e78c9bb0c441ef3847147198
thannes30 <thannes30@yahoo.com>
2014-05-30 17:06:45 -0400

    last friday afternoon demo

--- w06/d04/Tim_Hannes/jToDo/javascripts/scripts.js
+++ w06/d04/Tim_Hannes/jToDo/javascripts/scripts.js
@@ -8,3 +8,36 @@ $(function(){
     return false;
   });
 })
+
+// function Item(taskText){
+//   this.taskText = taskText;
+//   this.el = undefined;
+// }
+
+// Item.prototype.render = function(){
+//   var listItem = $('li').html(this.taskText);
+//   this.el = listItem;
+//   return this;
+// }
+
+
+// function createTask(){
+//   var userField = $('#item-field');
+//   var newItem = new Item(userField.val())
+//   var newNode = newItem.render().el;
+//   $('#todo-list').append(newNode);
+// }
+
+// $(function(){
+//   $('todo-form').on('submit', createTask);
+// })
+
+// // var toDoApp = {
+// //   Item: function Item(taskText){
+// //     this.taskText = taskText;
+// //     this.el = undefined;
+// //   },
+// //   createTask:
+
+
+// }
