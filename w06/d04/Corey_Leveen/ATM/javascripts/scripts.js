f2767910517a5b89cf7d3bfd1c1dae2ff8d29266
mntj <mntj11@gmail.com>
2014-05-31 18:10:00 -0400

    window.onload

---
+++ w06/d04/Corey_Leveen/ATM/javascripts/scripts.js
@@ -0,0 +1,16 @@
+var ATMApp = {
+  Deposit: function()
+}
+
+
+window.onload = function(){
+
+  var checkingInput = document.getElementById('checking-field');
+  var checkingWithdrawButton = document.getElementById('checking-withdraw-button');
+  var checkingDepositButton = document.getElementById('checking-deposit-button');
+
+  var savingsInput = document.getElementById('savings-field');
+  var savingsWithdrawButton = document.getElementById('savings-withdraw-button');
+  var savingsDepositButton = document.getElementById('savings-deposit-button');
+
+}
