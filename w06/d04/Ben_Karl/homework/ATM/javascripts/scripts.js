ce8e4e89cae459d79699ff42b57c42b69eec903c
Ben Karl <benrkarl@gmail.com>
2014-05-31 16:30:57 -0400

    functioning deposit and withdrawal

--- w06/d04/Ben_Karl/homework/ATM/javascripts/scripts.js
+++ w06/d04/Ben_Karl/homework/ATM/javascripts/scripts.js
@@ -1,35 +1,53 @@
-var atmApp = {
-  BankAccount: function(balance){
-    this.balance = balance;
-    this.balanceEl = undefined;
+var BankAccount = function BankAccount (balance){
+  this.balance = balance;
+  this.balanceEl = document.getElementById('account-balance');
+};
+
+BankAccount.prototype = {
+  render: function(balance){
+    this.balanceEl.innerHTML = "Account Balance: $" + balance;
   },
+
+  // currentBalance: function(){
+  //   return this.balance;
+  // },
 
   accountBalance: function(){
     return document.getElementById('account-balance');
   },
 
-  createAccount: function(balance){
-    var newAccount = new this.BankAccount(balance);
-    this.accountBalance().appendChild()
-  }
-}
+  startBalance: function(balance){
+    this.accountBalance().appendChild(render(this.balance).el);
+  },
 
-atmApp.BankAccount.prototype = {
-  render: function(){
-    var
+  // hasExistingBalance: function(){
+  //   return this.accountBalance().children.length != 0;
+  // },
+
+  accountDeposit: function(balance){
+    var newBalance = this.balance + balance;
+    this.balance = newBalance;
+    this.render(newBalance);
+  },
+
+  accountWithdrawal: function(balance){
+    var newBalance = this.balance - balance;
+    this.balance = newBalance;
+    this.render(newBalance);
   }
-}
+};
 
 window.onload = function(){
+  var account = new BankAccount(0);
   var depositButton = document.getElementById('deposit-button');
   var withdrawalButton = document.getElementById('withdrawal-button');
   var userInput = document.getElementById('account-input-field');
 
   depositButton.addEventListener('click', function(){
-    atmApp.accountDeposit(userInput.value);
+    account.accountDeposit(parseInt(userInput.value));
   });
 
   withdrawalButton.addEventListener('click', function(){
-    atmApp.accountWithdrawal(userInput.value);
+    account.accountWithdrawal(parseInt(userInput.value));
   });
 };
