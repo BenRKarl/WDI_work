d989f0415c669db6ba095e1c9278bb13ca69335b
pantsonsteven <pantsonsteven@gmail.com>
2014-06-01 15:49:32 -0400

    adds deposit and withdrawal functionality, color coding functionality

--- w06/d04/Steven_Doran/ATM/javascripts/scripts.js
+++ w06/d04/Steven_Doran/ATM/javascripts/scripts.js
@@ -1,30 +1,89 @@
 console.log('Hi user!')
 
-var atmApp = {
-   BankAccount: function (amount) {
-      this.balance = amount;
+BankAccount = function (name) {
+   this.name = name;
+   this.balance = Math.floor(Math.random() * 10000 ).toFixed(2);
+   this.el = document.querySelector('.account.' + name);
+};
+
+BankAccount.prototype = {
+   displayBalance: function(){
+      this.balanceEl().innerHTML = 'Current Balance: $' + this.balance;
    },
-   deposit: function (account, amount) {
-      // body...
+   balanceEl: function(){
+      return document.querySelector('.balance.' + this.name);
    },
-   withdrawal: function (account, amount) {
-      // body...
+   inputEl: function(){
+      return document.querySelector('.input.' + this.name);
    },
-   balance: function (account) {
-      // body...
+   inputValue: function(){
+      return this.inputEl().value;
+   },
+   depositButton: function(){
+      return document.querySelector('.button.deposit.' + this.name);
+   },
+   bindDeposit: function(){
+      var that = this;
+      this.depositButton().addEventListener('click', function(){
+         that.deposit();
+         that.inputEl().value = '';
+      });
+   },
+   deposit: function(){
+      this.displayNewBalanceDeposit();
+      this.accountColor();
+   },
+   displayNewBalanceDeposit: function(){
+      this.balanceEl().innerHTML = 'Current Balance: $' + this.newBalanceDeposit();
+   },   
+   newBalanceDeposit: function(){
+      this.balance = (parseFloat(this.balance) + parseFloat(this.inputValue())).toFixed(2);
+      return this.balance;
+   },
+   withdrawalButton: function(){
+      return document.querySelector('.button.withdrawal.' + this.name);
+   },
+   bindWithdrawal: function(){
+      var that = this;
+      this.withdrawalButton().addEventListener('click', function(){
+         that.withdrawal();
+         that.inputEl().value = '';
+      });
+   },  
+   withdrawal: function(){
+      this.displayNewBalanceWithdrawal();
+      this.accountColor();
+   },
+   displayNewBalanceWithdrawal: function(){
+      this.balanceEl().innerHTML = 'Current Balance: $' + this.newBalanceWithdrawal();
+   },   
+   newBalanceWithdrawal: function(){
+      this.balance = (parseFloat(this.balance) - parseFloat(this.inputValue())).toFixed(2);
+      return this.balance;
+   },
+   accountColor: function(){
+      if (this.balance > 0){
+         document.querySelector('.account.' + this.name).style.backgroundColor='lightgray';
+      } else if (this.balance <= 0){
+         document.querySelector('.account.' + this.name).style.backgroundColor='red';
+      };
    }
 };
 
-atmApp.BankAccount.prototype.render = function() {
-
-};
-
 window.onload = function() {
-   var checking = new atmApp.BankAccount(0.0);
-   var savings = new atmApp.BankAccount(0.0);
+   var checking = new BankAccount('checking');
+   var savings = new BankAccount('savings');
 
-   
+   checking.displayBalance();
+   savings.displayBalance();
 
+   checking.bindDeposit();
+   savings.bindDeposit();
 
+   checking.bindWithdrawal();
+   savings.bindWithdrawal();
+
+   checking.accountColor();
+   savings.accountColor();
 };
 
