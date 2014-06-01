7f006125c9eb139f6b00f971e31db6cdd0c5bc2f
glonsberry <gardnerlonsberry@gmail.com>
2014-06-01 18:07:02 -0400

    user prompt for start balance. balance displayed

---
+++ w06/d04/Gardner_Lonsberry/hw/atm_no_jquery/javascripts/scripts.js
@@ -0,0 +1,8 @@
+var welcomePrompt = alert("Welcome to your ATM.");
+
+
+var startBalance = prompt("How much money is in your account?");
+  if (startBalance != null && startBalance != '')
+    {
+        document.write("Your starting balance is " + startBalance);
+    }
