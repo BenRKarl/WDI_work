04ce87a34e63b21b4479b8679ad48c9f94038987
dshamis317 <dshamis317@gmail.com>
2014-06-01 16:49:58 -0400

    todo's and some hangman

---
+++ w06/d04/Dmitry_Shamis/homework/hangman/javascripts/scripts.js
@@ -0,0 +1,40 @@
+var hangman = {
+  startGame: function () {
+    var randomWord = wordList[(Math.floor(Math.random() * wordList.length))]
+    $('.word').append(randomWord);
+    var instructionsEl = $('<h3>').html("Type a letter and get going...");
+    var guessInputEl = $('<input>').addClass('guess-input');
+    var guessSubmitEl = $('<button>').addClass('guess-submit').html("Submit Guess");
+    var resetEl = $('<button>').addClass('reset').html("Reset");
+    var giveUpEl = $('<button>').addClass('give-up').html("Give Up");
+    $('.game-window').append(instructionsEl).append(guessInputEl).append(guessSubmitEl).append(resetEl).append(giveUpEl);
+  }
+}
+
+var wordList = ["amused", "beaming", "blissful", "blithe", "buoyant", "carefree", "cheerful", "cheery", "content", "contented",
+"delighted", "ecstatic", "elated", "enraptured", "euphoric", "exhilarated", "exultant", "glad", "gleeful", "gratified", "grinning",
+"jolly", "jovial", "joyful", "joyous", "jubilant", "lighthearted", "merry", "overjoyed", "pleased", "radiant", "rapturous",
+"satisfied", "smiling", "sunny", "thrilled", "untroubled"]
+
+$(function () {
+  $('.play').on('click', function () {
+    return hangman.startGame();
+  });
+
+
+  $('.guess-submit').on('click', function () {
+    var guess = $('.guess-input').val();
+    var word = $('.word').text();
+    var letters = word.split("");
+    for (i = 0; i < word.length; i++)
+      if (word.indexOf(guess) == word[i]) {
+        word[i].css('color', 'red');
+      }
+    });
+
+  $('.reset').on('click', function () {
+    $('.game-window').html("");
+    return hangman.startGame();
+  });
+});
+
