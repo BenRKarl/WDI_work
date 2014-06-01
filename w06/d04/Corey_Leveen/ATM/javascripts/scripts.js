5a27eec8149ddcdfc2d007cb455ce89ee931772b
mntj <mntj11@gmail.com>
2014-06-01 13:20:53 -0400

    bank account functional

--- w06/d04/Corey_Leveen/ATM/javascripts/scripts.js
+++ w06/d04/Corey_Leveen/ATM/javascripts/scripts.js
@@ -1,84 +1,62 @@
+
 function bankAccount(balance) {
   this.balance = balance;
 }
 
 bankAccount.prototype = {
-
   deposit: function(depositAmount) {
     this.balance += depositAmount;
   },
 
   withdraw: function(withdrawalAmount) {
-    this.balance -= withdrawalAmount;
+    if (withdrawalAmount > this.balance) {
+      console.log("Not enough funds.");
+    }
+    else {
+      this.balance -= withdrawalAmount;
+    }
   }
 };
 
 var checking = new bankAccount(100000);
 var savings = new bankAccount(100000);
 
-bankAccount.prototype.render = function(){
-  var bal = document.createElement('h3');
-  bal.innerHTML = this.balance;
-  return this;
-};
+window.onload = function() {
 
-
-  // var checking = {
-  //   var balance = 0;
-  //   this.balance = balance;
-  //   withdraw: function(withdrawAmount){
-  //     this.balance -= withdrawAmount;
-  //   },
-  //   deposit: function(depositAmount){
-  //     this.balance += depositAmount;
-  //   }
-  // };
-
-  // var savings = {
-  //   var balance = 0;
-  //   this.balance = balance;
-  //   withdraw: function(withdrawAmount){
-  //     this.balance -= withdrawAmount;
-  //   },
-  //   deposit: function(depositAmount){
-  //     this.balance += depositAmount;
-  //   }
-  // };
-
-// atmApp.prototype.render = function(){
-//   var cBal = document.createElement('h3');
-//   cBal.innerHTML = checking.balance;
-//   var sBal = document.createElement('h3');
-//   sBal = document.createElement('h3');
-//   sBal.innerHTML = savings.balance;
-//   return this;
-// }
-
-
-window.onload = function(){
+  var checkingBalance = document.getElementById('checking-balance');
   var checkingInput = document.getElementById('checking-field');
   var checkingWithdrawButton = document.getElementById('checking-withdraw-button');
   var checkingDepositButton = document.getElementById('checking-deposit-button');
 
+  var savingsBalance = document.getElementById('savings-balance');
   var savingsInput = document.getElementById('savings-field');
   var savingsWithdrawButton = document.getElementById('savings-withdraw-button');
   var savingsDepositButton = document.getElementById('savings-deposit-button');
 
-  checkingWithdrawButton.addEventListener('click', function(){
-    checking.withdraw(checkingInput.value);
-  checkingInput.value = "";
+  checkingBalance.innerHTML = "$"+checking.balance;
+  savingsBalance.innerHTML = "$"+savings.balance;
+
+  checkingWithdrawButton.addEventListener('click', function() {
+    checking.withdraw(parseInt(checkingInput.value, 10));
+    checkingBalance.innerHTML = "$"+checking.balance;
+    checkingInput.value = "";
   });
-  checkingDepositButton.addEventListener('click', function(){
-    checking.deposit.(checkingInput.value);
-  checkingInput.value = "";
+  checkingDepositButton.addEventListener('click', function() {
+    checking.deposit(parseInt(checkingInput.value, 10));
+    checkingBalance.innerHTML = "$"+checking.balance;
+    checkingInput.value = "";
   });
 
-  savingsWithdrawButton.addEventListener('click', function(){
-    savings.withdraw(savingsInput.value);
-  savingsInput.value = "";
+  savingsWithdrawButton.addEventListener('click', function() {
+    savings.withdraw(parseInt(savingsInput.value, 10));
+    savingsBalance.innerHTML = "$"+savings.balance;
+    savingsInput.value = "";
   });
-  savingsDepositButton.addEventListener('click', function(){
-    savings.deposit(savingsInput.value);
-  savingsInput.value = "";
+  savingsDepositButton.addEventListener('click', function() {
+    savings.deposit(parseInt(savingsInput.value, 10));
+    savingsBalance.innerHTML = "$"+savings.balance;
+    savingsInput.value = "";
   });
+
+
 };
