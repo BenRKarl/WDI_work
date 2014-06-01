9d8e01d5b80573529cd1af122ec85f7634e4118f
wakelank <wakelank@gmail.com>
2014-06-01 09:17:56 -0400

    completes non-jquery todo

--- w06/d04/Wake_Lankard/wlTodo/javascripts/scripts.js
+++ w06/d04/Wake_Lankard/wlTodo/javascripts/scripts.js
@@ -2,25 +2,66 @@
 var todoApp = {
    itemList: function(){
      return document.getElementById('todo_list');
-     
-
    },
 
   TodoItem: function(itemName){
     this.itemName = itemName;
   },
 
-  createItem: function(itemName){
+  createItem: function(itemName,parentList){
     var new_task = new this.TodoItem(itemName);
     new_task.render()
-    this.itemList().appendChild(new_task.itemElem);
+    parentList.appendChild(new_task.itemElem);
+  },
+
+  deleteItem: function(e){
+    var parent = e.target.parentElement.parentElement;
+    var item = e.target.parentElement;
+
+    parent.removeChild(item);
+  },
+
+  createSubItem: function(parentList){
+    var subField = document.createElement('input');
+    var subBtn = document.createElement('button');
+
+    subField.setAttribute('type','textfield');
+    subBtn.innerHTML = 'submit';
+    subBtn.addEventListener('click', function(){ 
+        todoApp.createItem(subField.value, parentList);
+        parentList.removeChild(subField);
+        parentList.removeChild(subBtn);
+    });
+    parentList.appendChild(subField);
+    parentList.appendChild(subBtn);
 
   }
+
+
 }
 
 todoApp.TodoItem.prototype.render = function(){
   var itemElem = document.createElement('li');
+  var subItemBtn = document.createElement('button');
+  var subItemList = document.createElement('ol');
+  var deleteBtn = document.createElement('button');
+
   itemElem.innerHTML = this.itemName;
+  subItemBtn.innerHTML = "Add sub task";
+  deleteBtn.innerHTML = "Delete";
+
+  subItemBtn.addEventListener('click', function(e){
+    var parentItem = e.target.nextSibling;
+    todoApp.createSubItem(parentItem);
+  });
+  deleteBtn.addEventListener('click', function(e){
+    todoApp.deleteItem(e);
+  });
+
+  itemElem.appendChild(deleteBtn);
+  itemElem.appendChild(subItemBtn);
+  itemElem.appendChild(subItemList);
+
   this.itemElem = itemElem;
 }
 
@@ -30,7 +71,8 @@ window.onload = function(){
 
   itemBtn.addEventListener('click', function(){
     var itemName = itemField.value;
-    todoApp.createItem(itemName);
+    var parentList = todoApp.itemList();
+    todoApp.createItem(itemName,parentList);
     itemField.value="";
   })
 }
