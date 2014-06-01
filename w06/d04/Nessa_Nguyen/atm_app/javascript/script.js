f5b291ced369f1bd096157f8fd85f89197b460a7
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-05-31 22:37:59 -0400

    Beautiful ATM app

--- w06/d04/Nessa_Nguyen/atm_app/javascript/script.js
+++ w06/d04/Nessa_Nguyen/atm_app/javascript/script.js
@@ -11,64 +11,80 @@ var atmApp = {
   },
 
   getAccount: function() {
-    var account = document.getElementById('account').account.value;
+    var account = document.getElementById('account_form').account.value;
     return account;
   },
 
-  getAmount: function() {
-    var input = document.getElementById('amount').value;
+  getDeposit: function() {
+    var input = document.getElementById('deposit').value;
+    if (input === '') { input = '0'; }
     var amount = parseFloat(input);
     return amount;
   },
 
-  updateBal: function(account, newBal) {    
+  getWithdrawal: function() {
+    var input = document.getElementById('withdrawal').value;
+    if (input === '') { input = '0'; }
+    var amount = parseInt(input);
+    return amount;      
+  },
+
+  updateBal: function(account, newBal) {
     var account_id =  account + '_bal' 
     var balance = document.getElementById(account_id);  
-    balance.innerHTML = newBal.toString();
+    balance.innerHTML = newBal.toString();            
   },
 
-  withdraw: function(account, amount) {
+  transact: function(account, deposit, withdrawal) {
+    var error = document.getElementById('error'); 
+    var balance = 0;        
     if (account === 'checking') {
       balance = atmApp.checkingBal();
-    } else {
+    } else if (account === 'saving') {
       balance = atmApp.savingBal();
+    } else {
+      var message = document.createTextNode('Please choose an account. ')
+      error.appendChild(message);
     }
-    return balance - amount;
+    if (withdrawal % 10 != 0) {
+      error.innerHTML = 'Cannot withdraw this amount. See note*.'     
+    } 
+
+    balance = balance + deposit - withdrawal;
+    if (withdrawal > atmApp.checkingBal() + atmApp.savingBal()) {
+      error.innerHTML = 'Cannot perform transaction. Withdrawal amount is higher than your combined balance.';
+    } else if (account === 'saving' && balance < 0) {       
+      error.innerHTML = 'Cannot perform transaction. Withdrawal amount is higher than your balance.'; 
+    } else if (account === 'checking' && balance < 0) {
+      var newSavingBal = atmApp.transact('saving', balance, 0);
+      atmApp.updateBal('saving', newSavingBal);
+      return 0;
+    } else {
+      return balance;         
+    }   
   },
 
-  deposit: function(account, amount) {
-    if (account === 'checking') {
-      balance = atmApp.checkingBal();
-    } else {
-      balance = atmApp.savingBal();
-    }
-    return balance + amount;
-  }
-}
+};
 
 window.onload = function() {
-  var amount = atmApp.getAmount();
-  var account = atmApp.getAccount();
-  var newBal = 0;
+  var buttonUpdate = document.getElementById('update_bal');
 
-  var buttonWithdraw = document.getElementById('withdraw');
-  buttonWithdraw.addEventListener('click', function(e) {
-    e.preventDefault();
-    newBal = atmApp.withdraw(account, amount);
 
-  });
+  buttonUpdate.addEventListener('click', function(e) {
+    e.preventDefault();   
+    var account = atmApp.getAccount();
 
-  var buttonDeposit = document.getElementById('deposit');
-  buttonDeposit.addEventListener('click', function(e) {
-    e.preventDefault();
-    newBal = atmApp.deposit(account, amount);
-  });    
-  
-  atmApp.updateBal(account, newBal); 
-  
-  if (atmApp.checkingBal === 0) {
-    document.getElementById('checking_bal').style.backgroundColor = 'red';
-  } else if (atmApp.savingBal === 0) {
-    document.getElementById('saving_bal').style.backgroundColor = 'red';
-  }
-}
+    var withdrawal = atmApp.getWithdrawal();
+    var deposit = atmApp.getDeposit();
+
+    newBal = atmApp.transact(account, deposit, withdrawal);
+    atmApp.updateBal(account, newBal);
+
+    if (atmApp.checkingBal() <= 0) {
+      document.getElementById('checking_bal').style.backgroundColor = 'red';
+    } else if (atmApp.savingBal() < 1) {
+      document.getElementById('saving_bal').style.backgroundColor = 'red';  
+    } 
+  }); // button close
+
+};
