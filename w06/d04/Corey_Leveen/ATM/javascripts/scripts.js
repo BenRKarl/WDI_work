1e87f631b12a5a187e121d09db23df84c2b561e0
mntj <mntj11@gmail.com>
2014-05-31 21:24:37 -0400

    attempted a refactor

--- w06/d04/Corey_Leveen/ATM/javascripts/scripts.js
+++ w06/d04/Corey_Leveen/ATM/javascripts/scripts.js
@@ -1,6 +1,55 @@
-var ATMApp = {
-  Deposit: function()
+// Needs major refactoring: make a bank account prototype
+function bankAccount(balance) {
+  this.balance = balance;
 }
+
+bankAccount.prototype = {
+  var bal = document.createElement('h3');
+  bal.innerHTML = this.balance;
+
+  deposit: function(depositAmount) {
+    this.balance += depositAmount;
+  },
+
+  withdraw: function(withdrawalAmount) {
+    this.balance -= withdrawalAmount;
+  }
+};
+
+var checking = new bankAccount(100000);
+var savings = new bankAccount(100000);
+
+
+  // var checking = {
+  //   var balance = 0;
+  //   this.balance = balance;
+  //   withdraw: function(withdrawAmount){
+  //     this.balance -= withdrawAmount;
+  //   },
+  //   deposit: function(depositAmount){
+  //     this.balance += depositAmount;
+  //   }
+  // };
+
+  // var savings = {
+  //   var balance = 0;
+  //   this.balance = balance;
+  //   withdraw: function(withdrawAmount){
+  //     this.balance -= withdrawAmount;
+  //   },
+  //   deposit: function(depositAmount){
+  //     this.balance += depositAmount;
+  //   }
+  // };
+
+// atmApp.prototype.render = function(){
+//   var cBal = document.createElement('h3');
+//   cBal.innerHTML = checking.balance;
+//   var sBal = document.createElement('h3');
+//   sBal = document.createElement('h3');
+//   sBal.innerHTML = savings.balance;
+//   return this;
+// }
 
 
 window.onload = function(){
@@ -13,4 +62,21 @@ window.onload = function(){
   var savingsWithdrawButton = document.getElementById('savings-withdraw-button');
   var savingsDepositButton = document.getElementById('savings-deposit-button');
 
-}
+  checkingWithdrawButton.addEventListener('click', function(){
+    checking.withdraw(checkingInput.value);
+  checkingInput.value = "";
+  });
+  checkingDepositButton.addEventListener('click', function(){
+    checking.deposit.(checkingInput.value);
+  checkingInput.value = "";
+  });
+
+  savingsWithdrawButton.addEventListener('click', function(){
+    savings.withdraw(savingsInput.value);
+  savingsInput.value = "";
+  });
+  savingsDepositButton.addEventListener('click', function(){
+    savings.deposit(savingsInput.value);
+  savingsInput.value = "";
+  });
+};
