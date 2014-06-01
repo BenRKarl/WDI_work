9e5cb6b4dac3d12a5c6ce1fe650bdba1d54a8ae5
wakelank <wakelank@gmail.com>
2014-06-01 14:51:18 -0400

    adds awesome fonts, explosions to jqtodo

--- w06/d04/Wake_Lankard/jqtodo/javascripts/scripts.js
+++ w06/d04/Wake_Lankard/jqtodo/javascripts/scripts.js
@@ -15,13 +15,36 @@ var toDoApp = {
     return false;
   },
   deleteTask: function(taskLi){
+   
+   toDoApp.blowup(taskLi);
    taskLi.remove();
-  }
+  },
+
+blowup: function(){
+  for (var i = 0; i < 100; i++) {
+    var newThing = $("<h1 class='things'>&star;</h1>");
+    $('body').append(newThing);
+    newThing.css('top', event.y + 'px');
+    newThing.css('left', event.x + 'px');
+  };
+
+  $('.things').each(function(idx, el){
+    $(el).animate({
+      top: ((Math.random()*200)-50) + '%',
+      left: ((Math.random()*200)-50) + '%',
+      opacity: 0
+    }, 2000, 'linear', function(){ $(this).remove(); }
+    );
+  });
+
+
+}
+
 };
 
 toDoApp.Task.prototype.render = function(){
   var listItem = $('<li>').html(this.taskText);
-  var deleteButton = $('<button>').html('Delete');
+  var deleteButton = $('<button>').html('<i class="fa fa-trash-o"></i>');
   deleteButton.click(function(){
     toDoApp.deleteTask(this.parentElement);
   });
@@ -29,6 +52,8 @@ toDoApp.Task.prototype.render = function(){
   this.deleteButton = deleteButton;
 
   return this;
+
+
 };
 
 
