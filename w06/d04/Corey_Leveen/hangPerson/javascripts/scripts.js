96b7a550c634166a87bdab08c1621f42bbb40c30
mntj <mntj11@gmail.com>
2014-06-01 23:45:24 -0400

    hangPerson further

--- w06/d04/Corey_Leveen/hangPerson/javascripts/scripts.js
+++ w06/d04/Corey_Leveen/hangPerson/javascripts/scripts.js
@@ -1,20 +1,14 @@
 var wordList = ["banana", "chevrolet", "beaches", "blueberries", "pigeons", "stairwell", "trees", "bacon", "tuesday", "movie", "clocks", "clouds", "sunrise", "sunday", "halloween", "pickle"]
 
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
 // The game selects a random word from a word list and makes that the secret word
 // The player guesses one letter at a time, trying to figure out what the word is
 // If the player guesses correctly, any instances of that letter are revealed in the secret word
 // If the player guesses incorrectly, they are penalized by taking away a guess
 // If a player reveals all of the letters of the secret word, they win
 // If a player makes 8 incorrect guesses before solving the secret word, they lose
-// var guess=prompt("Please guess the number");
-
-// if (guess == 5) {
-//   console.log("Correct!");
-// }
-// else {
-//   console.log("Nope.");
-// }
-
 
 function hangPersonGame(randomWord) {
   this.randomWord = randomWord;
@@ -23,10 +17,17 @@ function hangPersonGame(randomWord) {
 hangPersonGame.prototype = {
 
   begin: function() {
+
+    var arr = [];
+    for (var z = 0; z < this.randomWord.length; z++) {
+      arr.push("X");
+    }
+    var secret = arr.join("");
+    $('#secret-word').html(secret);
     var incorrectGuesses = 8;
 
     for (var i = 0; i < this.randomWord.length; i++) {
-    var guess=prompt("The secret word is " + this.randomWord.length + " letters long. Guess letter "+ (i+1));
+    var guess=prompt("The secret word is " + this.randomWord.length + " letters long. Guess letter "+ (i+1)+"." + " You have " + incorrectGuesses + " incorrect guesses remaining.");
     if (guess != this.randomWord[i]) {
       console.log("That's incorrect. You have " + incorrectGuesses + " wrong guesses left.");
       i--;
@@ -38,6 +39,8 @@ hangPersonGame.prototype = {
       }
     else {
       console.log("Correct! Letter "+ (i+1) + " is " + this.randomWord[i] + ".");
+      secret = secret.replaceAt(i, this.randomWord[i]);
+      $('#secret-word').html(secret);
       if ((i+1) == this.randomWord.length) {
         console.log("You guessed the word correctly - " + this.randomWord);
         break;
@@ -45,7 +48,6 @@ hangPersonGame.prototype = {
       }
     }
   }
-
 };
 
 $(function(){
