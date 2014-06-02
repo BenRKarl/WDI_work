7a67d2db6890847b49626b93777594f3299ba804
jonathangean <jonathangean@gmail.com>
2014-06-02 09:44:32 -0400

    w06d04

---
+++ w06/d04/Jonathan_Gean/jToDo/javascripts/scripts.js
@@ -0,0 +1,32 @@
+// $(function(){
+//   $('#todo-form').on('submit', function(e){
+//     e.preventDefault();
+//     var taskText = $('#item-field').val();
+//     var newItem = $('<li>').html(taskText);
+//     $('#todo-list').prepend(newItem);
+//   });
+// })
+
+function Item(taskText){
+  this.taskText = taskText;
+  this.el = undefined;
+}
+
+Item.prototype.render = function(){
+  var listItem = $('<li>').html(this.taskText);
+  this.el = listItem;
+  return this;
+};
+
+function createTask(){
+  var userField = $('#item-field');
+  var newItem = new Item(userField.val())
+  var newNode = newItem.render().el;
+  $('#todo-list').append(newNode);
+  return false
+}
+
+$(function(){
+  $('#todo-form').on('submit', createTask);
+})
+
