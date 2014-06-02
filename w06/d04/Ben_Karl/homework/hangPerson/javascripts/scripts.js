fc5ef2d8ee2d07441f9a811f0e70d6d9fc014461
Ben Karl <benrkarl@gmail.com>
2014-06-01 23:00:31 -0400

    finish hangman game setup, working on logic

--- w06/d04/Ben_Karl/homework/hangPerson/javascripts/scripts.js
+++ w06/d04/Ben_Karl/homework/hangPerson/javascripts/scripts.js
@@ -9,24 +9,61 @@ function HangWord(){
   this.el = undefined;
 }
 
+function GuessCounter(){
+  this.tries = 8;
+  this.el = $('<div>').html("Remaining Tries: " + this.tries);
+}
+
+function checkGuess(){
+  var guess = 3;
+}
+
+function guessIncrementer(GuessCounter){
+  if (false){
+    ///something for correct guess
+  } else {
+    GuessCounter.tries--;
+  }
+  return this;
+}
+
+// function changeColor(className){
+//   className.
+// }
+
 HangWord.prototype.render = function(){
-  var pEl = $('<p>').html(this.word);
-  this.el = pEl;
+  var divEl = $('<div id="word-div">');
+  for (var i = 0; i < this.word.length; i++){
+    var pEl = $('<p>');
+    pEl.html(this.word[i]);
+    divEl.append(pEl);
+  }
+
+  this.el = divEl;
   return this;
 }
 
 function hangPerson(){
   var wordContainer = $('#hang-word-container');
+  var guessAndTriesDiv = $('#field-and-tries');
+
   var hangWord = new HangWord();
   var hangEl = hangWord.render().el;
 
-  if (wordContainer.children().length > 0){
-    wordContainer.removeChild(wordContainer.firstElementChild);
-  }
+  var newGuessCounter = new GuessCounter();
+
+  var guessField = $('<input class="guess-field">');
+  guessField.attr('placeholder', 'Enter a letter');
+  var guessButton = $('<button class="guess-button">');
+  guessButton.html("Guess");
 
   wordContainer.append(hangEl);
+  guessAndTriesDiv.append(newGuessCounter.el);
+  guessAndTriesDiv.append(guessField);
+  guessAndTriesDiv.append(guessButton);
 }
 
 $(function(){
   $('#start-button').on('click', hangPerson)
+  $('#guess-button').on('click', checkGuess)
 });
