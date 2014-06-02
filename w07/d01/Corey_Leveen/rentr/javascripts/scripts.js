88f8cee2355f01cc5ecfc89338f577f8e20c0473
mntj <mntj11@gmail.com>
2014-06-02 13:39:58 -0400

    ajax further

--- w07/d01/Corey_Leveen/rentr/javascripts/scripts.js
+++ w07/d01/Corey_Leveen/rentr/javascripts/scripts.js
@@ -1,3 +1,25 @@
+
+
+var omdbApi = {
+  search: function(title, callback){
+    this.request(title, 's', callback);
+  },
+  lookup: function(omdbID, callback){
+    this.request(omdbID, 'i', callback);
+  },
+  request: function(query, parameter, callback){
+    var callback = callback || function(data){console.log(data)};
+    $.ajax({
+      url: 'http://omdbapi.com/?i=' + parameter + '=' + query,
+      success: callback
+    })
+  }
+}
+
+
+//omdbApi.search('star wars', function(data){console.log(data)})
+//omdbApi.search('star wars')
+
 $(function(){
   console.log('Ready to go')
 })
