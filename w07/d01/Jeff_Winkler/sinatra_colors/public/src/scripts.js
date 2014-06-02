660ecb6d0c92a7f078eb2263dd36f71fd8955e91
jwink <jeff_winkler@yahoo.com>
2014-06-02 18:43:47 -0400

    colors

--- w07/d01/Jeff_Winkler/sinatra_colors/public/src/scripts.js
+++ w07/d01/Jeff_Winkler/sinatra_colors/public/src/scripts.js
@@ -1,7 +1,39 @@
 
+function getPalettes(callback) {
+  $.ajax({
+    url: '/palettes.json',
+    success: callback
+  });
+}
+
+function verifyData(data) {
+  //console.log(data[0].colors);
+  $.each(data, function(index, value) {
+    var paletteDiv = $('<div>');
+    //$(paletteDiv).css({"height":"80px", "width":"300px", "background-color":"blue"});
+    $(paletteDiv).css("margin", "10px");
+    $(paletteDiv).css("display", "inline-block");
+    $(paletteDiv).html(value.title);
+    $('.container').append(paletteDiv);
+    console.log(value.title);
+    $.each(value.colors, function(i, color) {
+      var currColor = ('#'+color);
+      var colorDiv = $('<div>');
+      $(colorDiv).css({"height":"80px", "width":"60px", "background-color":currColor});
+      $(colorDiv).css("display", "inline-block");
+      $(paletteDiv).append(colorDiv);
+    });
+  });
+}
+
+
 function setEventHandlers(){
   var button = $('.random-palettes')[0];
-  $(button).click(function(){console.log("something is happenging");});
+  //$(button).click(getPalettes(verifyData));
+  $(button).on('click', function() {
+  getPalettes(verifyData);
+  });
+
 }
 
 $(function(){
