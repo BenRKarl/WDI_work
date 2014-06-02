5f94585074a241aeb90757f136026ebb7755b25f
mattmarcello <matt.marcello@gmail.com>
2014-06-02 16:27:55 -0400

    1 to the 1

--- w07/d01/ASSIGNMENT_FILES/color_picker/src/scripts.js
+++ w07/d01/ASSIGNMENT_FILES/color_picker/src/scripts.js
@@ -1,8 +1,18 @@
-
+function getPalettes(e){
+  $.ajax({
+    url: 'http://www.colourlovers.com/api/palettes/new?format=json',
+    success: function(data){
+      console.log(data);
+    }
+  })
+}
 
 function setEventHandlers(){
+  console.log('this is really happeneding');
   var button = $('.random-palettes')[0];
-  //write logic to set up click event handler on button;
+  $(button).click(getPalettes);
+
+
 }
 
 $(function(){
