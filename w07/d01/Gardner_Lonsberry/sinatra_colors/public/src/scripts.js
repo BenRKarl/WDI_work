8e906ff6ece9b5be8a2a6609adfc76464699dcb5
glonsberry <gardnerlonsberry@gmail.com>
2014-06-02 18:23:08 -0400

    initial hw commit

--- w07/d01/Gardner_Lonsberry/sinatra_colors/public/src/scripts.js
+++ w07/d01/Gardner_Lonsberry/sinatra_colors/public/src/scripts.js
@@ -1,9 +1,36 @@
+var colorAPI = {
+  search: function(title, callback){
+    this.request(title, 's', callback);
+    return this;
+  },
+  url: function(query, parameter){
+    return 'http://www.colourlovers.com/api/colors/new?' + parameter + '=' + query;
+  }, 
+  request: function (query, parameter, callback){
+  var callback = callback || function(data){console.log(data)};
+  var url = this.url(query, parameter)
+    $.ajax({
+      url: 'palettes.json',
+      dataType: 'json',
+      success: callback
+    })
+    return this;
+  }
+}
+
+
+
 
 function setEventHandlers(){
   var button = $('.random-palettes')[0];
-  $(button).click(function(){console.log("something is happenging");});
+  $(button).click(function(){
+
+  });
 }
+
 
 $(function(){
   setEventHandlers();
 });
+
+
