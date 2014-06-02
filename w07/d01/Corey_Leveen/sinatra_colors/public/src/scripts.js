a92cd804ce1f7281ebb48514ffeb1efd7547862b
mntj <mntj11@gmail.com>
2014-06-02 19:42:42 -0400

    sinatra_colors

--- w07/d01/Corey_Leveen/sinatra_colors/public/src/scripts.js
+++ w07/d01/Corey_Leveen/sinatra_colors/public/src/scripts.js
@@ -1,7 +1,18 @@
-
-function setEventHandlers(){
+function setEventHandlers() {
   var button = $('.random-palettes')[0];
-  $(button).click(function(){console.log("something is happenging");});
+  $(button).click(function() {
+    $.ajax({
+      url: '/palettes.json'
+    }).success(function(data) {
+      for (var i = 0; i < data.length; i++) {
+      var colorMainDiv = $('<div>').html(data[i].title).addClass('color-main-div');
+        for (var z = 0; z < 5; z++) {
+        var colorSubDiv = $('<div>').css('background-color', data[i].colors[z]);
+        $(colorMainDiv).append(colorSubDiv);
+        }
+      }
+    })
+  }
 }
 
 $(function(){
