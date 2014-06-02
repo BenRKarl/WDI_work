728a70bd65d84786d58c04cae7cb2486cbb46419
aziz-ka <hasanov.aziz@gmail.com>
2014-06-01 20:57:44 -0400

    wk6 d04

---
+++ w06/d04/Aziz_Hasanov/hearts/javascripts/scripts.js
@@ -0,0 +1,31 @@
+function randomColor() {
+  var maxColor = 16777215;
+  var hexColor = Math.floor(Math.random() * maxColor).toString(16);
+  return "#" + hexColor;
+}
+
+function letThereBeLove(numHearts) {
+
+  for (var i = 0; i < numHearts; i++) {
+    var newHeart = $("<h1 class='hearts'>I &hearts; Venera</h1>");
+    $("body").append(newHeart);
+  };
+
+  $(".hearts").each(function(id, el) {
+    // $(".hearts").css("color", randomColor();
+    $(el).animate({
+      top: ((Math.random() * 200) - 50) + "%",
+      left: ((Math.random() * 200) - 50) + "%",
+      // color: randomColor(),
+      opacity: 0
+    }, 2000, "linear", function() {
+      $(this).remove();
+    });
+  });
+}
+
+$(function() {
+  setInterval(function() {
+    letThereBeLove(10);
+  }, 500)
+})
