e7223af106aa65b75fb4b63b2c601c979b75194c
mntj <mntj11@gmail.com>
2014-06-02 16:53:02 -0400

    rentr

--- w07/d01/Corey_Leveen/rentr/javascripts/scripts.js
+++ w07/d01/Corey_Leveen/rentr/javascripts/scripts.js
@@ -4,22 +4,39 @@ var omdbApi = {
   search: function(title, callback){
     this.request(title, 's', callback);
   },
-  lookup: function(omdbID, callback){
-    this.request(omdbID, 'i', callback);
+  lookup: function(imdbID, callback){
+    this.request(imdbID, 'i', callback);
   },
   request: function(query, parameter, callback){
     var callback = callback || function(data){console.log(data)};
     $.ajax({
-      url: 'http://omdbapi.com/?i=' + parameter + '=' + query,
+      url: 'http://omdbapi.com/?' + parameter + '=' + query,
+      dataType: 'json',
       success: callback
     })
+    return this;
   }
 }
 
 
-//omdbApi.search('star wars', function(data){console.log(data)})
-//omdbApi.search('star wars')
-
 $(function(){
-  console.log('Ready to go')
+  $('#form').submit(function(e){
+    e.preventDefault();
+
+    omdbApi.search($('#input').val(), function(data){
+      var movieArray = data.Search;
+      for (var i = 0; i < movieArray.length; i++) {
+        var movieLi = $('<li>').html(movieArray[i].Title).addClass('film').data('imdbID', movieArray[i].imdbID);
+
+        movieLi.on('click', function(e){
+          var imdbID = $(e.target).data('imdbID');
+          omdbApi.lookup(imdbID, function(data){
+            $('.poster-image').attr('src', data.Poster);
+          })
+        })
+        $('.search-results').append(movieLi);
+      };
+    })
+  })
 })
+
