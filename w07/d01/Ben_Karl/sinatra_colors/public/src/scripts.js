ccf2cc69674e034a5830265bc81df6ff6f98abae
Ben Karl <benrkarl@gmail.com>
2014-06-02 17:48:45 -0400

    todays exercises

--- w07/d01/Ben_Karl/sinatra_colors/public/src/scripts.js
+++ w07/d01/Ben_Karl/sinatra_colors/public/src/scripts.js
@@ -1,7 +1,22 @@
 
 function setEventHandlers(){
   var button = $('.random-palettes')[0];
-  $(button).click(function(){console.log("something is happenging");});
+  $(button).click(
+    pullPalette()
+  );
+}
+
+function apiCall(){
+  $.ajax({
+    url: '/palettes.json',
+    success: function(data){
+      console.log(data);
+    }
+  })
+}
+
+function pullPalette(){
+$('.random-palettes').on('click', apiCall)
 }
 
 $(function(){
