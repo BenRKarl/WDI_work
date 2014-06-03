1dd125f5c71ab4d0d2b0f1c30e61349b74263251
mntj <mntj11@gmail.com>
2014-06-03 14:10:46 -0400

    sinatra reloaded

--- w07/d02/Corey_Leveen/people_with_joy/app/controllers/people_controller.rb
+++ w07/d02/Corey_Leveen/people_with_joy/app/controllers/people_controller.rb
@@ -10,4 +10,29 @@ class PeopleController < ApplicationController
   def edit
     @person = Person.find(params[:id])
   end
+
+  def create
+    person = Person.create(person_params)
+    redirect_to person_path(person)
+  end
+
+  def update
+    person = Person.create(person_params)
+    person.update(person_params)
+    redirect_to person_path(person)
+  end
+
+  def new
+  end
+
+  def destroy
+    Person.delete(person_params)
+    redirect_to people_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :email)
+  end
 end
--- w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
+++ w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
@@ -30,6 +30,10 @@ describe PeopleController do
         get :show, :id=>@andre.id
       end
 
+      it 'responds successfully' do
+        expect(response.code).to eq('200')
+      end
+
       it 'assigns @person' do
         actual = assigns(:person)
         expected = @andre
@@ -43,7 +47,6 @@ describe PeopleController do
 
     # EDIT
     describe 'GET edit' do
-
       before :each do
         get :edit, :id=>@andre.id
       end
@@ -61,7 +64,66 @@ describe PeopleController do
       it 'renders the edit template' do
         expect(response).to render_template('edit')
       end
-
     end # GET edit
+
+
+    # POST
+    describe 'POST update' do
+
+      before :each do
+        post :update, {:id => @andre.id, :person => {name: 'nondre'}}
+      end
+
+      it "responds with a redirect" do
+        actual = response.code
+        expected = '302'
+        expect(actual).to eq(expected)
+      end
+
+      it 'updates person record' do
+        @andre.reload
+        actual = @andre.name
+        expected = 'nondre'
+        expect(actual).to eq(expected)
+      end
+
+      it 'redirects to show' do
+        response.should redirect_to person_path(@andre)
+      end
+    end # POST update
+
+    describe 'DELETE destroy' do
+      before :each do
+        delete :destroy, {:id => @andre.id}
+      end
+
+      it 'destroy person record' do
+        expect { delete :destroy, {:id => @andre.id}}
+        .to change(Person, :count)
+        .by(-1)
+      end
+    end
   end # given a person
+
+  describe 'POST create' do
+    before :each do
+      post :create, {:person => {name: 'Kathew Lamp', email: 'kloool@lampmail.com'}}
+    end
+
+    it "responds with a redirect" do
+      actual = response.code
+      expected = '302'
+      expect(actual).to eq(expected)
+    end
+
+    it "inserts a person record" do
+      actual = Person.last.name
+      expected = 'Kathew Lamp'
+      expect(actual).to eq(expected)
+    end
+
+    it "redirects to show" do
+      response.should redirect_to person_path(Person.last)
+    end
+  end
 end # People Controller
