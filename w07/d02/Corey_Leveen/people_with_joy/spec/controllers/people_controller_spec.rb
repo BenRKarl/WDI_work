3171fa830c96832db25538c8bcb71ec3e99f02f8
mntj <mntj11@gmail.com>
2014-06-03 11:51:30 -0400

    specs

--- w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
+++ w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
@@ -18,6 +18,50 @@ describe PeopleController do
         expect(response.code).to eq('200')
       end
 
-    end
-  end
-end
+      it 'assigns @people' do
+        actual = assigns(:people)
+        expected = [@andre]
+        expect(actual).to eq(expected)
+      end
+    end # GET index
+
+    describe 'GET show' do
+      before :each do
+        get :show, :id=>@andre.id
+      end
+
+      it 'assigns @person' do
+        actual = assigns(:person)
+        expected = @andre
+        expect(actual).to eq(expected)
+      end
+
+      it 'renders the show template' do
+        expect(response).to render_template('show')
+      end
+    end # GET show
+
+    # EDIT
+    describe 'GET edit' do
+
+      before :each do
+        get :edit, :id=>@andre.id
+      end
+
+      it 'responds successfully' do
+        expect(response.code).to eq('200')
+      end
+
+      it "assigns @person" do
+        actual = assigns(:person)
+        expected = @andre
+        expect(actual).to eq.(expected)
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template('edit')
+      end
+
+    end # GET edit
+  end # given a person
+end # People Controller
