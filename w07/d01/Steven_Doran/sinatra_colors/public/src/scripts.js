1d2b9f7f75d32ab2069eb026e69c286653a0cd04
pantsonsteven <pantsonsteven@gmail.com>
2014-06-03 18:11:08 -0400

    adds colorbox functionality

--- w07/d01/Steven_Doran/sinatra_colors/public/src/scripts.js
+++ w07/d01/Steven_Doran/sinatra_colors/public/src/scripts.js
@@ -12,16 +12,51 @@ function setEventHandlers(){
    });
 }
 
+function displayColorBox(e){
+   var colorBox = $('<div>').addClass('color-box');
+   $('.container').append(colorBox);
+
+   // console.log($(this).data().colors.length);
+   
+   var colorBoxArray = $(this).data().colors;
+   var numColumns = $(this).data().colors.length;
+   console.log(numColumns);
+   var stripWidth = ((900/$(this).data().colors.length)/900)*100;
+   // console.log(((900/$(this).data().colors.length)/900)*100);
+
+   for (var i = 0; i < numColumns; i++) {
+      var colorStrip = $('<div>').addClass('color-strip')
+      colorStrip.css('width', stripWidth + '%');
+      $(colorBox).append(colorStrip);
+      colorStrip.css('background-color', '#' + colorBoxArray[i]);
+
+   };
+
+
+
+
+
+   $(document).mouseup(function(e){ //remove colorBox
+      if (!colorBox.is(e.target) && colorBox.has(e.target).length === 0){
+         $(colorBox).remove();
+      };
+
+   });
+}
+
 function displayPalettes(data){
    var containerDiv = $('.container');
-   console.log(data);
+
    for (var i in data){
 
       var palettesDiv = $('<div>').addClass('palettes');
       $(containerDiv).append(palettesDiv);
 
       var colorsDiv = $('<div>').addClass('colors');
+      colorsDiv.data('colors', data[i].colors);
+
       $(palettesDiv).append(colorsDiv);
+      
 
       var colorArray = data[i].colors;
 
@@ -30,6 +65,8 @@ function displayPalettes(data){
          $(hueDiv).css('background-color', '#' + colorArray[j] );
          $(colorsDiv).append(hueDiv);
       };
+
+      colorsDiv.click(displayColorBox)
 
       var title = data[i].title;
       var titleDiv = $('<div>').addClass('title').html('Title: ' + title);
@@ -49,4 +86,3 @@ $(function(){
 
 
 
-//tite, username, colors[0..4]
