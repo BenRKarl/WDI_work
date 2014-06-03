1725b8ca9d958f50163c2fb1ecf14651708a41b7
wakelank <wakelank@gmail.com>
2014-06-03 17:20:47 -0400

    completes classwork

--- w07/d02/Wake_Lankard/people_with_joy/spec/models/person_spec.rb
+++ w07/d02/Wake_Lankard/people_with_joy/spec/models/person_spec.rb
@@ -4,13 +4,16 @@ require 'spec_helper'
 
   describe Person do 
 
-    describe 'huh' do
-      it 'works' do
-        expect(2).to eq(2)
-      end
+    describe '#to_s' do
+      it 'should return a formated name' do
+        lich = Person.new({name: 'Lichard', email:'lich@glichmail.com'})
+        actual = lich.to_s
+        expected = 'Lichard (lich@glichmail.com)'
+        expect(actual).to eq(expected)
     end
-
   end
+
+end
 
 
 
