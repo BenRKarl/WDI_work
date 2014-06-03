5b992474768352ee6530ea917d0ea26d96117722
Ben Karl <benrkarl@gmail.com>
2014-06-02 22:11:16 -0400

    finished homework including palettes and new hangman functionality with ghetto images

--- w07/d01/Ben_Karl/sinatra_colors/public/src/scripts.js
+++ w07/d01/Ben_Karl/sinatra_colors/public/src/scripts.js
@@ -1,22 +1,34 @@
+function pullPalette(){
+$('.random-palettes').on('click', apiCall)
+}
+
+function apiCall(callback){
+  var callback = function(data){
+    extractPalette(data);
+  }
+  $.ajax({
+    url: '/palettes.json',
+    success: callback
+  })
+}
+
+function extractPalette(data){
+  var randomPalette = data[Math.floor(Math.random() * data.length)];
+  var paletteName = randomPalette.title;
+  var paletteColors = randomPalette.colors;
+  $('.color').remove();
+  $('.palette-title').html(paletteName + " hath been unleashed!");
+  for (var i = 0; i < paletteColors.length; i++){
+    colorDiv = $('<div>').addClass('color').css('backgroundColor', "#" + paletteColors[i]);
+    $('.palette-div').append(colorDiv);
+  }
+}
 
 function setEventHandlers(){
   var button = $('.random-palettes')[0];
   $(button).click(
     pullPalette()
   );
-}
-
-function apiCall(){
-  $.ajax({
-    url: '/palettes.json',
-    success: function(data){
-      console.log(data);
-    }
-  })
-}
-
-function pullPalette(){
-$('.random-palettes').on('click', apiCall)
 }
 
 $(function(){
