4e693c6d51d0c89eed4e581cb72c166f8fcfc650
jsmythd1 <jeffdrakos@gmail.com>
2014-06-02 23:28:16 -0400

    homework

--- w07/d01/Jeff_Drakos/sinatra_colors/public/src/scripts.js
+++ w07/d01/Jeff_Drakos/sinatra_colors/public/src/scripts.js
@@ -1,7 +1,38 @@
+function getPalettes(callback){
+  $.ajax({
+    url: '/palettes.json',
+    dataType: 'json',
+    success: callback
+  })
+  return this;
+}
+
+function makeDiv(data){
+  $.each(data, function(index, palette){
+    var divPaletteHolder = $('<div>');
+    var mainDiv = $('.palette');
+    $(mainDiv).append(divPaletteHolder);
+
+    $.each(palette.colors, function(index, color){
+
+      var divColorHolder = $('<div>');
+      var currentHex = "#" + color;
+      $(divColorHolder).css("background-color", currentHex);
+      $(divPaletteHolder).append(divColorHolder);
+      $(divColorHolder).css({
+        "height" : "40px",
+        "width" : "40px",
+        "display" : "inline-block"
+      });
+    });
+  });
+}
 
 function setEventHandlers(){
   var button = $('.random-palettes')[0];
-  $(button).click(function(){console.log("something is happenging");});
+  $(button).click(function(){
+    getPalettes(makeDiv);
+  });
 }
 
 $(function(){
