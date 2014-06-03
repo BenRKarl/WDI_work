06a50435bea5eedee7f4d1238b8267d322258de3
mntj <mntj11@gmail.com>
2014-06-03 17:18:11 -0400

    joy

--- w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
+++ w07/d02/Corey_Leveen/people_with_joy/spec/controllers/people_controller_spec.rb
@@ -1,21 +1,25 @@
 require 'spec_helper'
 
 describe PeopleController do
-  describe 'given a person' do
 
+  describe 'given a person' do
     before :each do
       @andre = Person.new()
       @andre.name = 'Andre'
       @andre.save
     end
 
+
+    # --- INDEX ---
     describe 'GET index' do
       before :each do
         get :index
       end
 
       it 'responds successfully' do
-        expect(response.code).to eq('200')
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
       end
 
       it 'assigns @people' do
@@ -23,15 +27,20 @@ describe PeopleController do
         expected = [@andre]
         expect(actual).to eq(expected)
       end
+
     end # GET index
 
+
+    # --- SHOW ---
     describe 'GET show' do
       before :each do
-        get :show, :id=>@andre.id
+        get :show, :id => @andre.id
       end
 
       it 'responds successfully' do
-        expect(response.code).to eq('200')
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
       end
 
       it 'assigns @person' do
@@ -43,38 +52,42 @@ describe PeopleController do
       it 'renders the show template' do
         expect(response).to render_template('show')
       end
+
     end # GET show
 
-    # EDIT
+
+    # --- EDIT ---
     describe 'GET edit' do
       before :each do
-        get :edit, :id=>@andre.id
+        get :edit, :id => @andre.id
       end
 
       it 'responds successfully' do
-        expect(response.code).to eq('200')
+        actual = response.code
+        expected = '200'
+        expect(actual).to eq(expected)
       end
 
-      it "assigns @person" do
+      it 'assigns @person' do
         actual = assigns(:person)
         expected = @andre
-        expect(actual).to eq.(expected)
+        expect(actual).to eq(expected)
       end
 
       it 'renders the edit template' do
         expect(response).to render_template('edit')
       end
+
     end # GET edit
 
-
-    # POST
+    # --- UPDATE ---
     describe 'POST update' do
 
       before :each do
         post :update, {:id => @andre.id, :person => {name: 'nondre'}}
       end
 
-      it "responds with a redirect" do
+      it "response with a redirect" do
         actual = response.code
         expected = '302'
         expect(actual).to eq(expected)
@@ -87,30 +100,49 @@ describe PeopleController do
         expect(actual).to eq(expected)
       end
 
-      it 'redirects to show' do
+      it "redirects to show" do
         response.should redirect_to person_path(@andre)
       end
+
     end # POST update
 
     describe 'DELETE destroy' do
-      before :each do
-        delete :destroy, {:id => @andre.id}
-      end
-
-      it 'destroy person record' do
-        expect { delete :destroy, {:id => @andre.id}}
-        .to change(Person, :count)
-        .by(-1)
+     it "destory person record" do
+        expect { delete :destroy, {:id => @andre.id} }
+          .to change(Person, :count).by(-1)
       end
     end
+
   end # given a person
 
-  describe 'POST create' do
+
+
+
+  # --- NEW ---
+  describe 'GET new' do
     before :each do
-      post :create, {:person => {name: 'Kathew Lamp', email: 'kloool@lampmail.com'}}
+      get :new
     end
 
-    it "responds with a redirect" do
+    it 'responds successfully' do
+      actual = response.code
+      expected = '200'
+      expect(actual).to eq(expected)
+    end
+
+    it 'renders the new template' do
+      expect(response).to render_template('new')
+    end
+
+  end # GET edit
+
+  # --- CREATE ---
+  describe 'POST create' do
+    before :each do
+      post :create, {:person => {name: 'Kathew Lamp', email: 'klool@lampmail.com'}}
+    end
+
+    it "response with a redirect" do
       actual = response.code
       expected = '302'
       expect(actual).to eq(expected)
@@ -125,5 +157,8 @@ describe PeopleController do
     it "redirects to show" do
       response.should redirect_to person_path(Person.last)
     end
+
   end
-end # People Controller
+
+end # PeopleController
+
