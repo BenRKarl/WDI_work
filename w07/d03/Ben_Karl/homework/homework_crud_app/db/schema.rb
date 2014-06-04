b71e069a7d2a061d56393d5e1d44981d69dbf85a
Ben Karl <benrkarl@gmail.com>
2014-06-04 17:01:50 -0400

    accidentally modified the crud add from class starting over with my own app from yesterdays homework

--- w07/d03/Ben_Karl/homework/homework_crud_app/db/schema.rb
+++ w07/d03/Ben_Karl/homework/homework_crud_app/db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140603141540) do
+ActiveRecord::Schema.define(version: 20140603221441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "people", force: true do |t|
+  create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
