7e09664623ad77d03b047731d10ae9f372c03e6e
wakelank <wakelank@gmail.com>
2014-06-04 18:03:10 -0400

    cut and pastes the skeliton code into scripts file

---
+++ w07/d03/Wake_Lankard/LOTRDOOM/javascripts/scripts.js
@@ -0,0 +1,102 @@
+// Dramatis Personae
+
+var hobbits = [
+  "Frodo Baggins",
+  "Samwise 'Sam' Gamgee",
+  "Meriadoc \"Merry\" Brandybuck",
+  "Peregrin 'Pippin' Took"
+];
+
+var buddies = [
+  "Gandalf the Grey",
+  "Legolas",
+  "Gimli",
+  "Strider",
+  "Boromir"
+];
+
+var baddies = [
+  "Sauron",
+  "Saruman",
+  "The Uruk-hai",
+  "Orcs"
+];
+
+var lands = ["The Shire", "Rivendell", "Mordor"];
+
+function makeMiddleEarth(lands) {
+    // create a section tag with an id of middle-earth
+    // add each land as an article tag
+    // inside each article tag include an h1 with the name of the land
+}
+
+makeMiddleEarth(lands);
+
+function makeHobbits(hobbits) {
+  // display an unordered list of hobbits in the shire
+  // give each hobbit a class of hobbit
+}
+
+function keepItSecretKeepItSafe() {
+  // create a div with an id of 'the-ring'
+  // add the ring as a child of Frodo
+}
+
+function makeBaddies(baddies) {
+  // display an unordered list of baddies in Mordor
+}
+
+function makeBuddies(buddies) {
+  // create an aside tag
+  // display an unordered list of buddies in the aside
+  // Make the Gandalf text node have a grey background
+}
+
+function leaveTheShire() {
+  // assemble the hobbits and move them to Rivendell
+}
+
+function beautifulStranger() {
+  // change the buddy 'Strider' textnode to "Aragorn"
+}
+
+function forgeTheFellowShip() {
+  // move the hobbits and the buddies to Rivendell
+  // create a new div called 'the-fellowship'
+  // add each hobbit and buddy one at a time to 'the-fellowship'
+  // after each character is added make an alert that they have joined your party
+}
+
+function theBalrog() {
+  // change the 'Gandalf' textNode to 'Gandalf the White'
+  // apply style to the element
+  // make the background 'white', add a grey border
+}
+
+function hornOfGondor() {
+  // pop up an alert that the horn of gondor has been blown
+  // Boromir's been killed by the Uruk-hai!
+  // put a linethrough on boromir's name
+  // Remove the Uruk-Hai from the Baddies on the page
+  // Remove Boromir from the Fellowship
+  // Put Boromir in the Footer
+}
+
+function itsDangerousToGoAlone(){
+  // take Frodo and Sam out of the fellowship and move them to Mordor
+  // add a div with an id of 'mount-doom' to Mordor
+}
+
+function weWantsIt() {
+  // Create a div with an id of 'gollum' and add it to Mordor
+  // Remove the ring from Frodo and give it to Gollum
+  // Move Gollum into Mount Doom
+}
+
+function thereAndBackAgain() {
+  // remove Gollum and the Ring from the document
+  // remove all the baddies from the document
+  // Move all the hobbits back to the shire
+}
+
+
