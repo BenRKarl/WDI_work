183c2cfc7ab68e6548641abc2c31406f6c57a28a
mntj <mntj11@gmail.com>
2014-06-05 12:35:08 -0400

    people with joy again

---
+++ w07/d04/Corey_Leveen/people_with_joy/app/assets/javascripts/scripts.js
@@ -0,0 +1,85 @@
+console.log("hey there");
+
+
+// ***** Model *****
+function Person(personJSON) {
+  this.name = personJSON.name;
+  this.email = personJSON.email;
+  this.id = personJSON.id;
+}
+
+
+// ****** View ******
+function PersonView(model) {
+  this.model = model;
+  this.el = undefined;
+}
+
+PersonView.prototype.render = function() {
+  var newElement = $('<div>').html(this.model.name);
+  this.el = newElement;
+  // return this to allow for method chaining
+  return this;
+};
+
+
+
+
+// ***** Collection *****
+function PeopleCollection() {
+  this.models = {};
+}
+
+PeopleCollection.prototype.add = function(personJSON) {
+  var newPerson = new Person(personJSON);
+  this.models[personJSON.id] = newPerson;
+  $(this).trigger('addFlare'); //shoot up in the air that add flare
+  return this;
+}
+
+PeopleCollection.prototype.create = function(paramObject) {
+  var that = this;
+  $.ajax({
+    url: '/people',
+    method: 'post',
+    dataType: 'json',
+    data: {person: paramObject},
+    success: function(data) {
+      that.add(data);
+    }
+  })
+}
+
+PeopleCollection.prototype.fetch = function() {
+  var that = this;
+
+  $.ajax({
+    url: '/people',
+    dataType: 'json',
+    success: function(data){
+      for (idx in data) {
+        that.add(data[idx]);
+      }
+    }
+  })
+};
+
+function clearAndDisplayPeopleList() {
+  $('.people').html('');
+
+    for (idx in peopleCollection.models) {
+      var person = peopleCollection.models[idx];
+      var personView = new PersonView(person);
+      $('.people').append(personView.render().el)
+    }
+}
+
+var peopleCollection = new PeopleCollection();
+
+
+$(function() {
+  peopleCollection.fetch();
+  $(peopleCollection).on('addFlare', function(){
+    clearAndDisplayPeopleList();
+  })
+})
