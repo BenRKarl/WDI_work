2e561724ba800274045c7563cfc3383903913b09
jdwmurph <jdwmurph@gmail.com>
2014-06-05 00:26:07 -0400

    homework done

---
+++ w07/d03/John_Murphy/homework/td_ruby_app/app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.json { render :json => @users.to_json}
+      format.html
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    User.delete(user)
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
+
+end
