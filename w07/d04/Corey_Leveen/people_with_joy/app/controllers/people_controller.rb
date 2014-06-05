d17656f0b759d95b339e0c16aaee16d6a46e14cb
mntj <mntj11@gmail.com>
2014-06-05 17:59:03 -0400

    updated my copy of ppl with joy

--- w07/d04/Corey_Leveen/people_with_joy/app/controllers/people_controller.rb
+++ w07/d04/Corey_Leveen/people_with_joy/app/controllers/people_controller.rb
@@ -6,27 +6,32 @@ class PeopleController < ApplicationController
       format.html
     end
   end
+
   def show
     @person = Person.find(params[:id])
   end
+
   def edit
     @person = Person.find(params[:id])
   end
+
   def create
     person = Person.create(person_params)
     respond_to do |format|
       form.html { redirect_to person_path(person) }
       form.json { render :json => person.to_json }
     end
-    redirect_to person_path(person)
   end
+
   def update
     person = Person.find(params[:id])
     person.update(person_params)
     redirect_to person_path(person)
   end
+
   def new
   end
+
   def destroy
     Person.delete(params[:id])
     redirect_to people_path
--- w07/d04/Corey_Leveen/people_with_joy/config/routes.rb
+++ w07/d04/Corey_Leveen/people_with_joy/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :people
+  root "welcome#index"
   resources :welcome
 end
