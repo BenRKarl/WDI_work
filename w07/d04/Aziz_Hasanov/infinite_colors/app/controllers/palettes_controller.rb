c3a70248b2a20178ed2e9862555bfe4c50719bac
aziz-ka <hasanov.aziz@gmail.com>
2014-06-05 17:36:39 -0400

    classwork

---
+++ w07/d04/Aziz_Hasanov/infinite_colors/app/controllers/palettes_controller.rb
@@ -0,0 +1,10 @@
+class PalettesController < ApplicationController
+  def index
+    offset = params[:offset] || 0
+    palettes = Palette.limit(5).offset(offset)
+    respond_to do |format|
+      format.html # renders palettes/index/html.erb
+      format.json {render json: palettes.to_json}
+    end
+  end
+end
