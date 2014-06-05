7e331e18316e75ffea62ced34632d2f57c4c9d75
paulgasbarra <paulgasbarra@gmail.com>
2014-06-05 13:07:53 -0400

    commit me

---
+++ w07/d03/Paul_Gasbarra/middle_earth/javascripts/scripts.js
@@ -0,0 +1,35 @@
+var hobbits = [
+  "Frodo Baggins",
+  "Samwise 'Sam' Gamgee",
+  "Meriadoc \"Merry\" Brandybuck",
+  "Peregrin 'Pippin' Took"
+];
+
+var buddies = [
+  "Gandalf the Grey",
+  "Legolas",
+  "Gimli",
+  "Strider",
+  "Boromir"
+];
+
+var baddies = [
+  "Sauron",
+  "Saruman",
+  "The Uruk-hai",
+  "Orcs"
+];
+
+var lands =["The Shire", "Rivendell", "Mordor"];
+
+function makeMiddleEarth(lands){
+  middleEarth = $('<section>').attr('id', 'middleEarth')
+  for (var i = lands.length - 1; i >= 0; i--) {
+    lands[i] = $('<arti
+    .append(lands[i])
+  };
+    ).append()
+}
+
+lands[i] = $('<artimakeM
+.append(iddleEar)th(lands)
