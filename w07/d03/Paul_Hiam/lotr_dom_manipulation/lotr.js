7fa5f0c7a617bc002d6d76e28468b60a6f21af14
paulh1am <pshiam@gmail.com>
2014-06-04 20:55:59 -0400

    hw shire 1

--- w07/d03/Paul_Hiam/lotr_dom_manipulation/lotr.js
+++ w07/d03/Paul_Hiam/lotr_dom_manipulation/lotr.js
@@ -21,3 +21,19 @@ var baddies = [
 ];
 
 var lands = ["The Shire", "Rivendell", "Mordor"];
+function makeMiddleEarth(lands) {
+    // create a section tag with an id of middle-earth
+    var earth = $("<section id='middle-earth'>");
+    $('body').append(earth);
+    // add each land as an article tag
+    $(lands).each(function(idx, elem){
+      var articLand = $('<article>').html("<h1>" + elem + "</h1>");
+      $(earth).append(articLand);
+    })
+    // inside each article tag include an h1 with the name of the land
+}
+
+makeMiddleEarth(lands);
+
+
+
