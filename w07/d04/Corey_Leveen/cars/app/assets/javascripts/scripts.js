d1684ef632fb7827765cec3ca84eb68f7d1ca02f
mntj <mntj11@gmail.com>
2014-06-05 18:39:03 -0400

    almost finished with cars

--- w07/d04/Corey_Leveen/cars/app/assets/javascripts/scripts.js
+++ w07/d04/Corey_Leveen/cars/app/assets/javascripts/scripts.js
@@ -8,35 +8,95 @@
 
 console.log('Hello from your scripts.js file!');
 
-function Car(make, year) {
-  this.make = make;
-  this.year = year;
+
+// MODEL
+function Car(carJSON) {
+  this.make = carJSON.make;
+  this.year = carJSON.year;
+  this.id = personJSON.id;
+}
+
+
+// VIEW
+function CarView(model) {
+  this.model = model;
   this.el = undefined;
 }
 
-Car.prototype.render = function() {
-  this.el = this.name;
+CarView.prototype.render = function() {
+  var newElement = $('<li>').html(this.model.make);
+  this.el = newElement;
   return this;
 };
 
 
-// collection
+// COLLECTION
 function CarsCollection() {
   this.models = {};
-};
+}
 
+CarsCollection.prototype.add = function(carJSON) {
+  var newCar = new Car(carJSON);
+  this.models[carJSON.id] = newCar;
+  $(this).trigger('addFlare');
+  return this;
+}
+
+CarsCollection.prototype.create = function(paramObj) {
+  var that = this;
+  $.ajax({
+    url: '/cars',
+    method: 'post',
+    dataType: 'json',
+    data: { car: paramObj },
+    success: function(data) {
+      that.add(data);
+    }
+  })
+}
 
 CarsCollection.prototype.fetch = function() {
   var that = this;
-
   $.ajax({
     url: '/cars',
     dataType: 'json',
-    success: function(data) {
-      $(data).each(function(idx, ele){
-        var newCar = new Car(ele.id, ele.make, ele.year);
-        that.models[ele.id] = newCar;
-      })
+    success: function(data){
+      for (i in data) {
+        that.add(data[i]);
+      }
     }
-  });
+  })
 };
+
+
+
+function clearAndDisplayCars() {
+
+  $('.cars').html('');
+
+  for (i in carsCollection.models) {
+    var car = carsCollection.models[i];
+    var carView = new CarView(car);
+    $('.cars').append(carView.render().el);
+  }
+}
+
+var carsCollection = new CarsCollection();
+
+$(function(){
+
+  carsCollection.fetch();
+
+  $(carsCollection).on('addFlare', function(){
+    clearAndDisplayCars();
+  });
+
+  $('.make-form').on('submit', function(e){
+    e.preventDefault();
+    var newMake = $('.make-form input[make="make"]').val();
+    $('.make-form input[make="make"]').val('');
+    carsCollection.create({make: newMake});
+  })
+})
+
+
