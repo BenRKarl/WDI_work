c1d968481ac0bbf793e3989b398d1711b02f8f92
mntj <mntj11@gmail.com>
2014-06-06 13:39:48 -0400

    mtar

--- w07/d05/Corey_Leveen/mtar/app/assets/javascripts/scripts.js
+++ w07/d05/Corey_Leveen/mtar/app/assets/javascripts/scripts.js
@@ -1,2 +1,37 @@
 
 console.log('Let Us Put Stuff Here')
+
+var mta = {
+  networkData: {},
+  getLineData: function( lineName, callback) {
+    // Code will go here to make ajax request and place the info into the mta's networkData
+    $.ajax({
+      url: '/lines/'+lineName,
+      dataType: 'json',
+      success: function(data) {
+
+
+        callback()
+      }
+    })
+
+    // method chaining?
+
+  }
+};
+
+function displayStationList (lineName, listCssSelector) {
+  // Code to clear and display a line's stations
+}
+
+$(function(){
+  $('.line').on('click', function(e){
+
+    var target = e.target;
+    var lineName = target.innerHTML.trim();
+
+    mta.getLineData(lineName, function(){
+      displayStationList(lineName, '.stations-list')
+    })
+  })
+})
