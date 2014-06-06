0612c035c175bca3b573e3fb30b3384cfe868b6d
HeidiW <foywilliams@gmail.com>
2014-06-06 13:35:39 -0400

    clean up

--- w07/d05/Heidi_Williams-Foy/mtar/app/assets/javascripts/scripts.js
+++ w07/d05/Heidi_Williams-Foy/mtar/app/assets/javascripts/scripts.js
@@ -1,2 +1,38 @@
 
 console.log('Let Us Put Stuff Here')
+
+var mta = {
+  networkData: {},
+  getLineData: function( lineName, callback){
+  	var that = this;
+
+	$.ajax({
+		url: '/lines/' + lineName,
+		dataType: 'json',
+		success: function(data){
+			that[lineName] = data
+			console.log(that.networkData)
+	    }
+	})
+}
+
+};
+
+function displaysStationList(lineName, listCssSelector){
+	//Code to clear and display a line's stations.
+	for (var = i; 0 < types.length, i++ )
+		
+	//constructor view or to the DOM
+	//display everything
+}
+
+$(function(){
+	$('.line').on('click', function(e){
+		//Obtain line's name
+		//var lineName = ?
+
+		mta.getLineData(lineName, function(){
+			displayStationList(lineName, '.stations-list')
+		})
+	})
+})
