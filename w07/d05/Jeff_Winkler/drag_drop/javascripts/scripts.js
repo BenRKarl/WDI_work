51e7a4d1841658d6d0b22f2db27609a9a3323660
jwink <jeff_winkler@yahoo.com>
2014-06-06 16:37:15 -0400

    afternoon

--- w07/d05/Jeff_Winkler/drag_drop/javascripts/scripts.js
+++ w07/d05/Jeff_Winkler/drag_drop/javascripts/scripts.js
@@ -5,4 +5,31 @@ $(function() {
               .draggable();
 
   sheen.appendTo($('#sheen-bin')).hide().fadeIn(3000);
+  $('#fade').droppable({
+    drop: function(e, dropped){
+      dropped.draggable.fadeOut(3000);
+      $(this).animate({backgroundColor: "gray"}, 3000);
+    }
+  });
+
+  $('#shrink').droppable({
+    drop: function(e, dropped) {
+      dropped.draggable.animate({
+        width: '-=10px',
+        height: '-=10px'
+      }, 1000);
+      }
+    });
+
+  $('#enlarge').droppable({
+    drop: function(e, dropped) {
+      dropped.draggable.animate({
+        width: '+=10px',
+        height: '+=10px'
+      }, 1000);
+      }
+    });
+
+
+
 });
