257d922a0780e987ed3ac91489774ae1830b168c
jwink <jeff_winkler@yahoo.com>
2014-06-06 12:23:50 -0400

    mta

--- w07/d05/Jeff_Winkler/mtar/app/assets/javascripts/scripts.js
+++ w07/d05/Jeff_Winkler/mtar/app/assets/javascripts/scripts.js
@@ -1,2 +1,35 @@
 
 console.log('Let Us Put Stuff Here')
+
+var mta = {
+  networkData: {},
+  getLineData: function(lineName, callback) {
+    var that = this;
+    $.ajax({
+      url: '/lines/'+lineName,
+      dataType: 'json',
+      success: function(data) {
+        that.networkData[lineName] = data.stations;
+        callback();
+      }
+    });
+  }
+};
+
+function displayStationList(lineName, listCssSelector){
+  $(listCssSelector).html('');
+  var stationArray = mta.networkData[lineName];
+  $.each(stationArray, function(index, station) {
+    var $addressLi = $('<li>').text(station.address);
+    $(listCssSelector).append($addressLi);
+  });
+}
+
+$(function(){
+  $('.line').on('click', function(){
+    lineName = $(this).data().name;
+    mta.getLineData(lineName, function(){
+      displayStationList(lineName, '.stations-list');
+    });
+  });
+});
