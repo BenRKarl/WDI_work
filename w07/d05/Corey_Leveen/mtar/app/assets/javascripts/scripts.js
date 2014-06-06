ec612d3d2cb61325b34b4716eeb144b7a25a7c92
mntj <mntj11@gmail.com>
2014-06-06 14:17:33 -0400

    mtar working

--- w07/d05/Corey_Leveen/mtar/app/assets/javascripts/scripts.js
+++ w07/d05/Corey_Leveen/mtar/app/assets/javascripts/scripts.js
@@ -3,35 +3,61 @@ console.log('Let Us Put Stuff Here')
 
 var mta = {
   networkData: {},
-  getLineData: function( lineName, callback) {
-    // Code will go here to make ajax request and place the info into the mta's networkData
+
+  getLineData: function(lineName, callback) {
+    var that = this;
+    var url = '/lines/'+ lineName;
+
+
     $.ajax({
-      url: '/lines/'+lineName,
+      url: url,
       dataType: 'json',
       success: function(data) {
 
-
+        that.networkData[lineName] = data;
         callback()
       }
     })
-
-    // method chaining?
-
+    return this;
   }
+
 };
 
 function displayStationList (lineName, listCssSelector) {
-  // Code to clear and display a line's stations
+  var $lineStationList = $(listCssSelector);
+  $lineStationList.html('');
+  var lineData = mta.networkData[lineName];
+  $(lineData.stations).each(function(i, ele) {
+    var $newLi = $('<li>').html(ele.address);
+    $lineStationList.append($newLi);
+  })
 }
 
+
+
+
+
+
 $(function(){
+
   $('.line').on('click', function(e){
 
-    var target = e.target;
-    var lineName = target.innerHTML.trim();
+    var lineName = e.target.innerText;
 
     mta.getLineData(lineName, function(){
       displayStationList(lineName, '.stations-list')
     })
   })
+
+
+  $('.line').each(function(idx, element){
+    var color = $(element).data('color');
+    $(element).css('opacity', 0);
+    $(element).css('backgroundColor', color);
+    $(element).animate({
+      opacity: 1,
+    }, 500)
+  })
+
+
 })
