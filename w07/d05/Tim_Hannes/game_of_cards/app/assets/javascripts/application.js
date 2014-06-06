4beea896ad399b37d8474ffb339ad07f59eb2c79
thannes30 <thannes30@yahoo.com>
2014-06-06 15:33:35 -0400

    drag and drop

--- w07/d05/Tim_Hannes/game_of_cards/app/assets/javascripts/application.js
+++ w07/d05/Tim_Hannes/game_of_cards/app/assets/javascripts/application.js
@@ -1,58 +1,99 @@
-  // $(document).ready(function(){
-  //   $("button").click(function(){
-  //     $.ajax({
-  //       url:"cards",
-  //       succes:function(result){
-  //         $("#list-of-cards").html(result);
-  //         }
-  //       }
-  //     )
-  //   })
-  // })
+//   function CardModel(jsonData){
+//     this.name = jsonData.name;
+//     this.suit = jsonData.suit;
+//   }
 
-  function CardModel(jsonData){
-    this.name = jsonData.name;
-    this.suit = jsonData.suit;
+//   function CardView(model){
+//     this.model = model;
+//     this.el    = undefined;
+//   }
+
+//   CardView.prototype.render = function(){
+//     var $div = $('<div>').addClass('card').addClass(this.model.suit)
+//     var $topLeft = $('<div>').addClass('top left').text(this.model.suit + " " + this.model.name);
+//     var $bottomRight = $('<div>').addClass('bottom right').text(this.model.suit + " " + this.model.name);
+//     $div.append($topLeft, $bottomRight);
+//     this.el = $div;
+//     return this;
+//   }
+
+
+//   function CardCollection(){
+//     this.models = [];
+//   }
+
+//  CardCollection.prototype.fetch = function(){
+//   var that = this;
+//   $.ajax({
+//     url: '/cards',
+//     dataType: 'json',
+//     success: function(data){
+//       $.each(data, function(i, datum){
+//         var cardModel = new CardModel(datum);
+//         that.models.push(cardModel);
+//         var cardView  = new CardView(cardModel);
+//         $('#cards').append(cardView.render().el);
+//       });
+//     }
+//   })
+// }
+
+//   $(function(){
+//     var collection = new cardCollection();
+//     $('#show-cards').click(function(){
+//       collection.fetch();
+//     });
+//   })
+
+
+function CardModel(jsonData){
+  this.name = jsonData.name;
+  this.suit = jsonData.suit;
+}
+
+
+function CardView(model){
+  this.model  = model;
+  this.el     = undefined;
+}
+
+CardView.prototype.render = function(){
+  var $div = $('<div>').addClass('card').addClass(this.model.suit);
+  if (this.model.suit === "diamonds"){
+    var $topLeft = $('<div>').addClass('top left').html("&diams;"+" "+this.model.name);
+    var $bottomRight = $('<div>').addClass('bottom right').html("&diams;"+" "+this.model.name);
+  } else {
+    var $topLeft = $('<div>').addClass('top left').html("&"+this.model.suit+";"+" "+this.model.name);
+    var $bottomRight = $('<div>').addClass('bottom right').html("&"+this.model.suit+";"+" "+this.model.name);
   }
+  $div.append($topLeft, $bottomRight);
+  this.el = $div;
+  return this;
+}
 
-  function CardView(model){
-    this.model = model;
-    this.el    = undefined;
-  }
+function CardCollection(){
+  this.models = [];
+}
 
-  CardView.prototype.render = function(){
-    var $div = $('<div>').addClass('card').addClass(this.model.suit)
-    var $topLeft = $('<div>').addClass('top left').text(this.model.suit + " " + this.model.name);
-    var $bottomRight = $('<div>').addClass('bottom right').text(this.model.suit + " " + this.model.name);
-    $div.append($topLeft, $bottomRight);
-    this.el = $div;
-    return this;
-  }
-
-
-  function CardCollection(){
-    this.models = [];
-  }
-
-  CardCollection.prototype.fetch = function() {
-    var that = this;
-    $.ajax({
-      url: '/cards',
-      dataType: 'json',
-      success: function(data){
-        $.each(data, function(i, datum){
-          var cardModel = new CardModel(datum);
-          that.models.push(cardModel);
-          var cardView = new CardView(cardModel);
-          $('#cards').append(cardView.render().el);
-        });
-      }
-    })
-  }
-
-  $(function(){
-    var collection = new cardCollection();
-    $('#show-cards').click(function(){
-      collection.fetch();
-    })
+CardCollection.prototype.fetch = function(){
+  var that = this;
+  $.ajax({
+    url: '/cards',
+    dataType: 'json',
+    success: function(data){
+      $.each(data, function(i, datum){
+        var cardModel = new CardModel(datum);
+        that.models.push(cardModel);
+        var cardView  = new CardView(cardModel);
+        $('#cards').append(cardView.render().el);
+      });
+    }
   })
+}
+
+$(function(){
+  var collection = new CardCollection();
+  $('#show-cards').click(function(){
+    collection.fetch();
+  });
+})
