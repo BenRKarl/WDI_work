61f2133356bc4dc5e40f94416bd8253e5d2907d5
paulh1am <pshiam@gmail.com>
2014-06-06 14:25:20 -0400

    k

--- w07/d05/Paul_Hiam/mtar/app/assets/javascripts/scripts.js
+++ w07/d05/Paul_Hiam/mtar/app/assets/javascripts/scripts.js
@@ -1,19 +1,24 @@
 
-console.log('Let Us Put Stuff Here')
+console.log('Let Us Put Stuff Here');
 
 var mta = {
   networkData: {},
   getLineData: function(lineName, callback){
+    var that = this;
     //code will make ajax req & place info into the mtas networkData
+
     $.ajax({
       url:'/lines/'+lineName,
       dataType: 'json',
       success: function(data){
         //put line's stations in mta.networkData
-        //after that eval callback
+        
+        that.networkData[lineName] = data;
+        //or = data.stations
         callback()
+        //after that eval callback
       }
-    })
+    });
     //method chaining
     return this;
   }
@@ -21,20 +26,38 @@ var mta = {
 
 function displayStationList(lineName, listCssSelector){
   // Code to clear and display a line's stations(exits)
+  var $lineStationList = $(listCssSelector);
+  $lineStationList.html('');
+  var lineData = mta.networkData[lineName];
+  $(lineData.stations).each(function(idx, ele){
+    var $newLi = $('<li>').html(ele.address);
+    $lineStationList.append($newLi);
+
+  })
 }
 
+
 $(function(){
+  $('.line').each(function(idx, element){
+    var color = $(element).data('color');
+    $(element).css('opacity', 0);
+    $(element).css('backgroundColor', color);
+  
+    $(element).animate({
+      opacity:1}, 5000);
+  })
+
 
   $('.line').on('click', function(e){
     var target = $(e.target);
-    var lineName = target.html().trim();
-    
+    var lineName = target.html().trim();  
     //obtain line name
-    //var lineName = ?
+  
   
     mta.getLineData(lineName, function(){
+    
     displayStationList(lineName, '.stations-list')
-    // })
+     })
   })
 
 })
