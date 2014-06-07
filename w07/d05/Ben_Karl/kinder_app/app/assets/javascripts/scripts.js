f28f3b548c1c4209db33c0b4304aa801a42271ba
Ben Karl <benrkarl@gmail.com>
2014-06-07 15:51:02 -0400

    add post functionality for kitten and some styles for droppable containers

--- w07/d05/Ben_Karl/kinder_app/app/assets/javascripts/scripts.js
+++ w07/d05/Ben_Karl/kinder_app/app/assets/javascripts/scripts.js
@@ -16,6 +16,19 @@ Kitten.prototype.render = function(){
   return this;
 };
 
+Kitten.prototype.meowThisKitten = function(paramObject){
+  var that = this;
+  $.ajax({
+    url: '/kittens',
+    method: 'post',
+    dataType: 'json',
+    data: { kitten: paramObject },
+    success: function(data){
+      console.log(data + " was added to the database.");
+    }
+  });
+}
+
 function randomKittenUrl(){
   var url = 'http://placekitten.com/';
   var height = (Math.floor(Math.random() * (400 - 200 + 1)) + 200).toString();
@@ -23,8 +36,23 @@ function randomKittenUrl(){
   return url + width + '/' + height;
 };
 
-// function meowOrNotMeow(event, kitten){
+function meowOrNotMeow(event, kitten){
+  if (event === Meow){
+    kitten.meowThisKitten({url: kitten.url})
+    location.reload();
+  } else {
+    location.reload();
+  }
+}
 
-// }
+$('.meow').droppable({
+  drop: function(e, dropped){
+    debugger;
+  }
+})
 
-
+$('.not-meow').droppable({
+  drop: function(e, dropped){
+    debugger;
+  }
+})
--- w07/d05/Ben_Karl/kinder_app/config/routes.rb
+++ w07/d05/Ben_Karl/kinder_app/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   root 'kittens#index'
   get '/kittens/random' => 'kittens#index'
+  resources :kittens, only: [:index, :create]
 
 end
