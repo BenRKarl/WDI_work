38447e2484f16877cfdbb5ffe10a60b0b69785d2
mntj <mntj11@gmail.com>
2014-06-07 15:49:50 -0400

    jasmine testing drill complete: all tests passing.

--- w07/d05/Corey_Leveen/jasmineTesting/spec/calcSpec.js
+++ w07/d05/Corey_Leveen/jasmineTesting/spec/calcSpec.js
@@ -1,7 +1,31 @@
 describe("Calculator", function() {
 
-  it('works', function(){
-    expect(true).toBe(true);
+  it('can add two numbers', function(){
+    var a = 5;
+    var b = 2;
+    var sum = calculator.add(a, b);
+    expect(sum).toEqual(7);
+  });
+
+  it('can subtract two numbers', function(){
+    var a = 5;
+    var b = 2;
+    var difference = calculator.subtract(a, b);
+    expect(difference).toEqual(3);
+  });
+
+  it('can multiply two numbers', function(){
+    var a = 5;
+    var b = 2;
+    var product = calculator.multiply(a, b);
+    expect(product).toEqual(10);
+  });
+
+  it('can add two numbers', function(){
+    var a = 5;
+    var b = 2;
+    var quotient = calculator.divide(a, b);
+    expect(quotient).toEqual(2.5);
   });
 
 });
