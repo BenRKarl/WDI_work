aa87f592b1c7dc36d22a0aca3c99268e2d1d43e6
wakelank <wakelank@gmail.com>
2014-06-06 21:18:37 -0400

    passes multiplitcation test

--- w07/d05/Wake_Lankard/jasmine_test/spec/SpecCalculator.js
+++ w07/d05/Wake_Lankard/jasmine_test/spec/SpecCalculator.js
@@ -6,14 +6,20 @@ describe("Calculator", function(){
  it("2 + 3 = 5", function(){
   actual   = calculator(2, 3, "add");
   expected = 5;
-  expect(actual).toBe(expected)
+  expect(actual).toBe(expected);
   });
 
   it("2 - 3 = -1", function(){
     actual   = calculator(2, 3, "subtract");
     expected = -1;
-    expect(actual).toBe(expected)
+    expect(actual).toBe(expected);
   });
+
+  it(" 2 * 3 = 6", function(){
+    actual = calculator(2, 3, "multiply");
+    expected = 6;
+    expect(actual).toBe(expected);
+  })
 
 
 
