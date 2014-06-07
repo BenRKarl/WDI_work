6a2114019692995b95ce4aebf8b95bfcbbbda822
Ben Karl <benrkarl@gmail.com>
2014-06-07 14:04:41 -0400

    refactored kitten kinder added Kitten class render function pretty pleased with how smooth it went.

--- w07/d05/Ben_Karl/kinder_app/app/assets/javascripts/scripts.js
+++ w07/d05/Ben_Karl/kinder_app/app/assets/javascripts/scripts.js
@@ -1,16 +1,30 @@
-
 $(function(){
   var kittenBin = $('.kitten-container');
-  var kittenImg = $('<img>').attr('src', randomKittenUrl());
-  kittenImg.addClass('kitten').draggable();
-  kittenBin.append(kittenImg);
-})
+  var aNewKitten = new Kitten(randomKittenUrl());
+  kittenBin.append(aNewKitten.render().el);
+});
+
+function Kitten(url){
+  this.url = url;
+  this.el = undefined;
+};
+
+Kitten.prototype.render = function(){
+  var kittenImg = $('<img>').attr('src', this.url)
+        .addClass('kitten').draggable();
+  this.el = kittenImg;
+  return this;
+};
 
 function randomKittenUrl(){
   var url = 'http://placekitten.com/';
   var height = (Math.floor(Math.random() * (400 - 200 + 1)) + 200).toString();
   var width = (Math.floor(Math.random() * (400 - 200 + 1)) + 200).toString();
   return url + width + '/' + height;
-}
+};
+
+// function meowOrNotMeow(event, kitten){
+
+// }
 
 
