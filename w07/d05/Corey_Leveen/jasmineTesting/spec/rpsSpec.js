0dcc03a21523993c0bd43f0784a2eb83cc69e2b3
mntj <mntj11@gmail.com>
2014-06-07 13:42:57 -0400

    jasmine

---
+++ w07/d05/Corey_Leveen/jasmineTesting/spec/rpsSpec.js
@@ -0,0 +1,11 @@
+// Set up a new Jasmine ready testing environment
+// Here is the goal:
+// calculator.add(2, 3) should return 5
+// calculator.subtract(2, 3) should return -1
+// calculator.multiply(2, 3) should return 6
+// FOLLOW THIS PATTERN (This is the exercise)
+// Write a failing test (RED)
+// Write code to pass the test (GREEN)
+// Commit
+// Refactor your code
+// Commit
