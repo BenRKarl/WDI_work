825c805696bd3149526295abde28ea399c09e4db
atrapp <andrea.trapp@gmx.net>
2014-06-08 06:13:31 -0400

    w07 d05 jasmine_testing_drill refactored

--- w07/d05/Andrea_Trapp/homework/jasmine_testing_drill/src/Calculator.js
+++ w07/d05/Andrea_Trapp/homework/jasmine_testing_drill/src/Calculator.js
@@ -1,20 +1,37 @@
 
+// *****   refactored code   *****
 function Calculator(){};
 
 Calculator.prototype.add = function(x, y){
-  result = x + y;
-  return result;
+  return x + y;
 };
 
 Calculator.prototype.subtract = function(x, y){
-  result = x - y;
-  return result;
+  return x - y;
 };
 
 Calculator.prototype.multiply = function(x, y){  
-  result = x * y;
-  return result;
+  return x * y;
 };
+
+
+
+// function Calculator(){};
+
+// Calculator.prototype.add = function(x, y){
+//   result = x + y;
+//   return result;
+// };
+
+// Calculator.prototype.subtract = function(x, y){
+//   result = x - y;
+//   return result;
+// };
+
+// Calculator.prototype.multiply = function(x, y){  
+//   result = x * y;
+//   return result;
+// };
 
 
 
