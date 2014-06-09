d047d34a3584a547536dd7fd2b80395010e6b86d
glonsberry <gardnerlonsberry@gmail.com>
2014-06-09 17:26:17 -0400

    cleaning up before hw

--- w08/d01/Gardner_Lonsberry/instagram/app.rb
+++ w08/d01/Gardner_Lonsberry/instagram/app.rb
@@ -6,13 +6,21 @@ use OmniAuth::Builder do
   provider :instagram, 'fef2fced51f64c8ebc0c599e63daccf0','d29b980ec4754a0d93bdf8919c47706e'
     end
 
+use OmniAuth::Builder do
+  provider :twitter, '6sCmu0xqmi7KGRmK3f0Pcv1TG', 'CDXBzy60IZL1cEXwsj8AHsB3CH6DEkSrFwM4kUFYg7eK8YM5wM'
+end
 
   get '/' do
     erb :index
-    
+  end 
+
 
   get '/auth/instagram' do
     erb :index
   end
+
+get '/auth/:provider/instagram/' do
+  erb :welcome
+end
 
 
