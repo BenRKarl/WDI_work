aff754a694ec78d442f2344112b632e7dbdf697f
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-06-09 19:57:59 -0400

    Working clock

---
+++ w08/d01/Nessa_Nguyen/omniauth_trial/app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.authenticate(params[:username], params[:password])
+      session[:user_id] = user.user_id
+      redirect_to root_path
+    else
+      "Log in failed. Try again"
+      redirect_to login_path
+    end    
+  end
+
+  def log
+    data = request.env['omniauth.auth']
+  render :json => data.to_json
+ end
+end
