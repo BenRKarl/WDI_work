a6af2482a604c061640fce2f4f10740af7feb9c4
wdi <wdi>
2014-06-09 09:42:57 -0400

    wk7d5 homework rps, ruby kitten

---
+++ w07/d05/Rebecca_Strong/ruby_kitten/app/assets/javascripts/scripts.js
@@ -0,0 +1,79 @@
+//need a model for the kitten
+function Kitten(kittenJSON){
+  this.url = kittenJSON.url;
+  this.id = kittenJSON.id;
+}
+
+Kitten.prototype.create = function(url){
+   $.ajax({
+    url: '/kittens',
+    method: 'post',
+    dataType: 'json',
+    data: {kitten: url},
+    success: function(){}
+   })
+}
+
+function preloadKittenPicture() {
+  var picWidth  = Math.floor((Math.random() * 100) + 100);
+  var picHeight = Math.floor((Math.random() * 100) + 100);
+  var url = "http://placekitten.com/"+picWidth+"/"+picHeight;
+  kitten_hidden = $('<img>').attr('src', url).addClass('kitten_hidden').draggable();
+  return url;
+}
+
+
+var kitten_hidden;
+
+$(function(){
+
+  var picWidth  = Math.floor((Math.random() * 100) + 100);
+  var picHeight = Math.floor((Math.random() * 100) + 100);
+  var url = "http://placekitten.com/"+picWidth+"/"+picHeight;
+  var kitten = $('<img>').attr('src', url).addClass('kitten').draggable();
+
+  kitten.appendTo($('#kitten')).hide().fadeIn(3000);
+
+  $('#not_meow').droppable({
+    drop: function(e, dropped){
+      $(this).animate({backgroundColor: 'white'}, 500).animate({backgroundColor: 'red'}, 1500);
+      dropped.draggable.fadeOut(50);
+      url = preloadKittenPicture();
+      kitten_hidden.appendTo($('#kitten')).hide().fadeIn(50);
+    }
+  })
+
+  $('#meow').droppable({
+    drop: function(e, dropped){
+      $(this).animate({backgroundColor: 'white'}, 500).animate({backgroundColor: 'green'}, 1500);
+      dropped.draggable.fadeOut(50);
+      var newKitten = new Kitten({url: url});
+      newKitten.create({url: url});
+      url = preloadKittenPicture();
+      kitten_hidden.appendTo($('#kitten')).hide().fadeIn(50);
+    }
+  })
+
+})
+
+// $(function(){
+//   var kitten = $('<img>').attr('src', 'http://placekitten.com/200/300').addClass('kitten').draggable();
+//   kitten.appendTo($('#kitten-bin')).hide().fadeIn(3000);
+
+// $('#fade').droppable({
+//   drop: function(e, dropped){
+//     $(this).animate({backgroundColor: 'red'}, 500).animate({backgroundColor: 'green'}, 500);
+//     dropped.draggable.fadeOut(2000);
+//   }
+})
+
+$('#shrink').droppable({
+  drop: function(e, dropped){
+  $(dropped.draggable).animate({
+    width: '-=25px',
+    height: '-=25px'
+    }, 1000)
+  }
+})
+
+});
