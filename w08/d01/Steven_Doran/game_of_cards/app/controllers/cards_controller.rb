5f7b96dacb2021c84f6b2fec5c1199c74dee378e
pantsonsteven <pantsonsteven@gmail.com>
2014-06-09 17:34:39 -0400

    facebook oauth

--- w08/d01/Steven_Doran/game_of_cards/app/controllers/cards_controller.rb
+++ w08/d01/Steven_Doran/game_of_cards/app/controllers/cards_controller.rb
@@ -29,3 +29,33 @@ end
 
 # jacks_and_queens = Card.where('name = ? OR name = ?', 'Jack', 'Queen')
 # threes_sevens = Card.where(name: '3'..'7')
+
+def random_straight
+
+  cards = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+
+  suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades'].sample
+  seed = Card.where(suit: suit).sample(1)
+  straing = []
+
+  if cards.index(seed) < 9
+    seed_index = card.index(seed[0].name)
+    seed_range = (seed_index..(seed_index + 4)).to_a
+    seed_range.each do |index|
+      straight << Card.where('name = ? AND suit = ?,' cards[index].to_s, seed[0].suit.to_s)
+    end
+  else
+    puts "Straight is not possible"
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
