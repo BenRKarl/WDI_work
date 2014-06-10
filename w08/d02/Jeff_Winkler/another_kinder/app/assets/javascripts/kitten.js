87d1de68c07be20565bbe4a884dcfe0c187c184f
jwink <jeff_winkler@yahoo.com>
2014-06-10 16:48:04 -0400

    tuesday

---
+++ w08/d02/Jeff_Winkler/another_kinder/app/assets/javascripts/kitten.js
@@ -0,0 +1,35 @@
+
+function randomUrl() {
+  var width = Math.floor(Math.random()*200+100);
+  var height = Math.floor(Math.random()*200+100);
+  return "http://placekitten.com/"+width+"/"+height;
+}
+
+
+
+
+function KittenModel(url) {
+  this.url = randomUrl();
+}
+
+
+function KittenView(model) {
+  this.model = model;
+  this.el = undefined;
+}
+
+KittenView.prototype.render = function() {
+  var img = $('<img>').attr('src', this.model.url)
+            .addClass('kitten')
+            .hide();
+  this.el = img;
+  return this;
+}
+
+function KittenCollection() {
+  this.kittens = {};
+}
+
+
+
+
