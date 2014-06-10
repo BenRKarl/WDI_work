b50bca13650ebbbd3c66586a21975edea391fa1e
ntuvera <ntuvera@gmail.com>
2014-06-10 08:56:10 -0400

    9am clear

--- w08/d01/Nathaniel_Tuvera/starter/clock.js
+++ w08/d01/Nathaniel_Tuvera/starter/clock.js
@@ -1,6 +1,9 @@
+var angle = 0
 $(function(){
-    var x = 0
-    $('#second-hand').animate({rotate: '20deg'}, 500);
+  setInterval(function(){
+    angle+=3
+    $('#second-hand').rotate(angle)
+  }, 500);
   // Set up a setInterval that runs every second (1000ms).
   // It should call an animate function that moves the clock hands.
   // It may be necessary to keep track of elapsed seconds in a variable.
