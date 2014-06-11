d2f3975a4cfe2cd36aad9d1f06836b902dd9123a
mntj <mntj11@gmail.com>
2014-06-10 21:54:33 -0400

    seuss regex

--- w08/d02/Corey_Leveen/seuss_regex/regexp.rb
+++ w08/d02/Corey_Leveen/seuss_regex/regexp.rb
@@ -16,3 +16,20 @@ text = f.read
 f.close
 
 binding.pry
+
+
+#1 - text.scan(/(Sam)[^-]/).length  === 6
+
+#2 - can't figure this out..
+
+#3 - sammy = text.gsub("Sam-I-am", "Sammy jam")
+# save_as("sammy_jam", sammy)
+# Not sure if this saved ^^^
+
+#4 affirmative = text.gsub("not", "")
+# save_as("affirmative", affirmative)
+
+
+# Create a file, "list", that lists all the places and animals whom Sam refuses to eat with (the bigot).
+
+# Create a new file, "affirmative", in which the narrator constantly agrees to eat (ie, remove the nots).
