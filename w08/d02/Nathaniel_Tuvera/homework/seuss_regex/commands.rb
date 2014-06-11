eee8b6ffa5fb8e06c28a3ecba0cb5544bb08e461
ntuvera <ntuvera@gmail.com>
2014-06-10 23:48:59 -0400

    sam-i-am commands written out, not sure how to use and save to file

--- w08/d02/Nathaniel_Tuvera/homework/seuss_regex/commands.rb
+++ w08/d02/Nathaniel_Tuvera/homework/seuss_regex/commands.rb
@@ -1,1 +1,33 @@
 text.scan(/(Sam)[^-]/).count
+
+arr   = text.scan(/not eat.*/)
+arr2  = arr.map { |name| name.split }
+list  = arr2.map { |name| name.pop }
+
+# test = text.scan(/Sam-I-[aA]m/)
+# test2 = test.each {|name| name.gsub('Sam-I-am', 'Sammy Jam')}
+text.gsub(/Sam-I-[aA]m/, 'Sammy Jam')
+
+arr   = text.scan(/.*will eat.*/)
+arr2  = arr.map { |name| name.split }
+list  = arr2.map { |name| name }
+
+text.gsub(/Sam/,'Matt')
+text.gsub(/Sam-I-[aA]m/, 'Matt-I-Say')
+text.gsub(/[gG]reen[ ]\w{4}[ ]\w{3}[ ]\w{3}/i, 'coconut oil and lamb')
+
+
+# name_arr2 = name_arr.each { |name| name.gsub!("Sam-I-am", "Sammy Jam") }
+
+# text.scan(/.*will eat.*/)
+
+
+
+
+# list = arr2.map do |name|
+#     name.pop
+#   end
+# arr2 = arr.map do |name|
+#         text.scan(/.*not eat.*/).split
+#       end
+
