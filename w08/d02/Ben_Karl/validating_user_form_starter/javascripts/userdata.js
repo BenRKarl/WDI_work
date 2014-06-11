77be0d3d3e09a82929d5fea4daa66ffda7f832a9
Ben Karl <benrkarl@gmail.com>
2014-06-10 21:26:53 -0400

    display clear and validation for username complete

--- w08/d02/Ben_Karl/validating_user_form_starter/javascripts/userdata.js
+++ w08/d02/Ben_Karl/validating_user_form_starter/javascripts/userdata.js
@@ -1,38 +1,76 @@
 //Validate the Name. It should return true if the name is greater than or equal 3 characters. If not, it should show the error and return false.
-function validateName(name){}
+function validateName(name){
+  regex = /[a-zA-Z]{3}/
+  if (regex.test(name)){
+    return true;
+  } else {
+    $("#user_name_error").show();
+    return false;
+  }
+
+}
 
 // Validate the Age. It should return true if the age is a number. If not, it should show the error and return false.
-function validateAge(age){}
+function validateAge(age){
+  return true;
+}
 
 // Validate the Phone Number. It should return true if the phone number matches the regular expression. If not, it should show the error and return false.
-function validatePhone(phone){}
+function validatePhone(phone){
+  return true;
+}
 
 // Validate the Email. It should return true if the email matches the regular expression. If not, it should show the error and return false.
-function validateEmail(email){}
+function validateEmail(email){
+  return true;
+}
 
 //Write a function so that when the clear button is clicked, any data being displayed is erased.
-function clearData(){}
+function clearData(){
+  var ul = $('#display_data');
+  ul.html("");
+}
 
 //Display function blanks out what is currently being displayed in the display_data area then displays the submitted user data as a list
-function display(){}
-
+function display(name, age, phone, email){
+  var ul = $('#display_data');
+  ul.html("");
+  var userInfo = [name, age, phone, email]
+  var title = ['Name: ', 'Age: ', 'Phone: ', "Email: "];
+  for (var i = 0; i < userInfo.length; i++){
+    var newEl = $('<li>');
+    newEl.html(title[i] + userInfo[i]);
+    ul.append(newEl);
+  }
+}
 
 $(document).ready(function(){
+  var userNameField = $('#user_name');
+  var userAgeField = $('#user_age');
+  var userPhoneField = $('#user_ph');
+  var userEmailField = $('#user_email');
+  var $add_user_data_form = $("#add_user_data");
+  var $clear_button = $('#clear');
   //Store the appropriate items you will be selecting from the DOM into variables for easy usage
-
   // Initially hide the errors.  Check the HTML to see how to select all the items that are error messages.
-
-
+  $('span').hide();
   //set event handler
   $add_user_data_form.submit(function(){
     //Get values from user inputs
-
+    var userNameText = userNameField.val();
+    var userAgeText = userAgeField.val();
+    var userPhoneText = userPhoneField.val();
+    var userEmailText = userEmailField.val();
     // If all of the items validate, you should update the name, age, phone and email of the user and display it
-    if(validateName() && validateAge() && validatePhone() && validateEmail()) {
-      display();
+    if(validateName(userNameText) && validateAge(userAgeText) && validatePhone(userPhoneText) && validateEmail(userEmailText)) {
+      display(userNameText, userAgeText, userPhoneText, userEmailText);
     }
     return false; //This stops the form from processing (page reload). An alternative to preventDefault();
   });
+
+  $clear_button.click(function(){
+    clearData();
+  })
 
 });
 
