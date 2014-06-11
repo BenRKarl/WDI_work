b38442def175f3d9b1f1f5811a95d6ace830b969
wdi <wdi>
2014-06-11 15:33:22 -0400

    pre-project

--- w08/d02/Rebecca_Strong/morning/the_definitive_kinder/app/controllers/kittens_controller.rb
+++ w08/d02/Rebecca_Strong/morning/the_definitive_kinder/app/controllers/kittens_controller.rb
@@ -1,4 +1,14 @@
 class KittensController < ApplicationController
   def index
   end
+
+  def create
+    kitten = Kitten.create(params_kitten)
+    # where _kitten is a private method
+    render :json => kitten.to_json
+end
+
+def kitten_params
+  params.require(:kitten).permit(:url)
+end
 end
