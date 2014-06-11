73f6092b23839ecf6e82f2cfe9d44a2b44602624
glonsberry <gardnerlonsberry@gmail.com>
2014-06-11 06:48:36 -0400

    hw submission

--- w08/d02/Gardner_Lonsberry/validating_user_form_starter/javascripts/userdata.js
+++ w08/d02/Gardner_Lonsberry/validating_user_form_starter/javascripts/userdata.js
@@ -1,20 +1,41 @@
 //Validate the Name. It should return true if the name is greater than or equal 3 characters. If not, it should show the error and return false.
-function validateName(name){}
+function validateName(name){
+  if ( name.length >= 3 ) {
+    alert('true');
+  }
+}
 
 // Validate the Age. It should return true if the age is a number. If not, it should show the error and return false.
-function validateAge(age){}
+function validateAge(age){
+  if parseInt(age > 0) {
+    alert('age is good');
+  } else if {
+  age != 
+  }
+  
+}
 
 // Validate the Phone Number. It should return true if the phone number matches the regular expression. If not, it should show the error and return false.
-function validatePhone(phone){}
+function validatePhone(phone){
+
+}
 
 // Validate the Email. It should return true if the email matches the regular expression. If not, it should show the error and return false.
-function validateEmail(email){}
+function validateEmail(email){
+[\w+]+@\w+\.com?
+[\w+]+@\w+\.com?
+[\w+]+@\w+\.com?
+}
 
 //Write a function so that when the clear button is clicked, any data being displayed is erased.
-function clearData(){}
+function clearData(){
+
+}
 
 //Display function blanks out what is currently being displayed in the display_data area then displays the submitted user data as a list
-function display(){}
+function display(){
+  
+}
 
 
 $(document).ready(function(){
@@ -33,26 +54,28 @@ $('#user_ph_error').hide()
   $add_user_data_form.submit(function(){
     //Get values from user inputs
 var user_name = $('#user_name').val();
+var user_age = $('#user_age').val();
+var user_ph = $('#user_ph').val();
+var user_email = $('#user_email').val();
 // $('#user_name').attr('#user_name');
 // $('user_name').html(value);
 // $("#user_name").keyup(function(){
 
 
-$(".submit").on("click", function(){
+// $(".submit").on("click", function(){
 
-})
+// })
 
-    drop: function(e, dropped){
-      admireKitten(dropped.draggable.attr('src'));
-    },
-    hoverClass: "drop-hover"
-  })
+//     drop: function(e, dropped){
+//       admireKitten(dropped.draggable.attr('src'));
+//     },
+//     hoverClass: "drop-hover"
+//   })
     // If all of the items validate, you should update the name, age, phone and email of the user and display it
     if(validateName() && validateAge() && validatePhone() && validateEmail()) {
       display();
     }
     return false; //This stops the form from processing (page reload). An alternative to preventDefault();
   });
-
 });
 
