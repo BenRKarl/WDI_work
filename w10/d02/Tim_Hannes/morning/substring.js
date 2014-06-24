0feec9c04deb43966984d3d85111325bf74f4675
thannes30 <thannes30@yahoo.com>
2014-06-24 17:36:01 -0400

    tuesday afternoon commit pre hw

---
+++ w10/d02/Tim_Hannes/morning/substring.js
@@ -0,0 +1,24 @@
+function findSubstring(string,substring){
+  string = 'hello';
+  substring = string.lastIndexOf(string,0)
+}
+
+
+function findSubstring(string, substring){
+  while (i < string.length)
+    if string.charAt[i] === string.length-1;
+      console.log(i)
+    end
+    else
+      console.log(false)
+  end
+}
+
+function findSubstring(string, substring){
+  for (var i = 0; i < string.length - substring.length-1; i++){
+    if (substring == string.substr(i, substring.length)){
+      return i;
+    }
+  }
+  return false;
+}
