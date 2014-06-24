4d1b003149334ef6c28c92ac2f27c7c72c2848d3
Rebecca50 <rebeccastrong50@gmail.com>
2014-06-24 09:02:19 -0400

    homework w10day 1juice bar

---
+++ w08/d04/Rebecca_Strong/morning/email_bot/app/controllers/people_controller.rb
@@ -0,0 +1,18 @@
+class PeopleController < ActionController::Base
+
+  def index
+    @people = Person.all
+  end
+
+  def create
+    url = params.[:url]
+    doc = httparty.get(url)
+    emails = doc.scan(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$./)
+    emails.each do |email|
+      person = Person.find_or_create_by(email: email)
+      site = Site.find_or_create_by(url: url)
+      person.sites << site
+  end
+    redirect_to "/people"
+  end
+end
---
+++ w10/d01/Rebecca_Strong/backbone_book/javascripts/app.js
@@ -0,0 +1,64 @@
+// ** Model **
+var Book = Backbone.Model.extend({
+defaults:{
+  title: 'n/a',
+  author: 'n/a',
+  paper: {type: 'cloth'}
+  // sets the type of paper for all the objects, to set it for only one, then
+//   paper: function(){type: 'cloth'}
+}
+});
+
+
+
+// ** View **
+var BookPainter = Backbone.View.extend({
+  initialize: function(){
+    this.listenTo(this.model, 'change', this.render)
+  },
+
+  tagName: 'span',
+// establishes the type of el. Here, not a div, but a span.
+template: _.template('<h2><%= title %></h2><h3><%= author %></h3>'),
+
+  render: function(){
+    this.$el.empty();
+    //above means give me a primed, clean canvas on which to work
+
+this.$el.html( this.template( this.model.attributes))
+//relates to line 19, template: _.template('<h2>.....')
+
+return this;
+
+    // var titleH2 = $('<h2>').html(this.model.get('title'));
+    // var authorH3 = $('<h3>').html(this.model.get('author'));
+
+    // this.$el.append(titleH2);
+    // this.$el.append(authorH3);
+    // this.$el.html(this.model.get('title'));
+
+
+    // return this;
+  }
+});
+
+
+var classicBook;
+var classicBookPainter;
+
+
+$(function(){
+//make me a book
+var classicBook = new Book({title: 'The Lichard in the Rye', author: 'Lichard DeGray'});
+
+//Painting on existing DOM element
+
+var classicBookPainter = new BookPainter({el: $('.book-sock'), model: classicBook});
+//where $book_sock = the 'easel'/canvas -- which overrides the 'span' el above -- and the model = classicBook
+classicBookPainter.render();
+
+
+})
+
+
+
