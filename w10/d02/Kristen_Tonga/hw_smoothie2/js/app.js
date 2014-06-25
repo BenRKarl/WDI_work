84ccc248f1d652ff3d3c866f81f8e4098c851c26
Kristen Tonga <kristen.tonga@gmail.com>
2014-06-25 07:17:50 -0400

    added form for new juice

--- w10/d02/Kristen_Tonga/hw_smoothie2/js/app.js
+++ w10/d02/Kristen_Tonga/hw_smoothie2/js/app.js
@@ -1,9 +1,28 @@
 // **** PAGE LOAD ****
 $(function(){
   console.log("sigh and let go... ")
+
+  juiceCollection = new JuiceCollection();
+  juiceListView = new JuiceListView({
+    collection: juiceCollection,
+    el: $('.juice-collection')
+  });
+
   var rad = new Juice({name: 'Rad Rool'});
+  // rad.get('ingredient').add({ingredient: 'radish'});
+  // rad.get('ingredient').add({ingredient: 'rutabaga'});
+  // rad.get('ingredient').add({ingredient: 'rasberry'});
   var radView = new JuiceView({model: rad});
   $('.juice-test').append(radView.render().el);
+  juiceCollection.add(rad)
+
+  $('.new-juice').on('submit', function(e){
+    e.preventDefault();
+    var nameField = $('.juice-name');
+    var juiceName = nameField.val();
+    nameField.val('')
+    juiceCollection.add({name: juiceName});
+  })
 
 
 })
--- w10/d02/Kristen_Tonga/hw_smoothie2/js/backbone/juice.js
+++ w10/d02/Kristen_Tonga/hw_smoothie2/js/backbone/juice.js
@@ -46,6 +46,9 @@ var JuiceView = Backbone.View.extend({
 
 var JuiceListView = Backbone.View.extend({
   initialize: function(){
+    this.listenTo(this.collection, 'all', this.render);
+  },
+  render: function(){
     var that = this;
     this.$el.empty();
     _.each(this.collection.models, function(juice){
