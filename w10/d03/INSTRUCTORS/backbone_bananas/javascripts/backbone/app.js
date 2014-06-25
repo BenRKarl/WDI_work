ff93dc89139108b3388bce88ae0cd55a2df9e833
amadden80 <amadden80@gmail.com>
2014-06-25 15:57:11 -0400

    backbone

--- w10/d03/INSTRUCTORS/backbone_bananas/javascripts/backbone/app.js
+++ w10/d03/INSTRUCTORS/backbone_bananas/javascripts/backbone/app.js
@@ -4,15 +4,27 @@ var Bananapp = Bananapp || { Models: {}, Collections: {}, Views: {} };
 
 
 Bananapp.initialize = function(){
-  // Create a collection
-  //  Create a listview
 
-  // Set up events for the form
+  var monkeys = new Bananapp.Collections.MonkeyCollection();
+  var listView = new Bananapp.Views.MonkeyListView({
+    el: $('.monkeys'),
+    collection: monkeys
+  });
+
+  $('form.monkey-form').on('submit', function(e){
+    e.preventDefault();
+    var nameField = $('form.monkey-form input');
+    var newName = nameField.val();
+    nameField.val('');
+    monkeys.add({name: newName});
+  })
+
 };
-
-
 
 
 $(function(){
   Bananapp.initialize();
 })
+
+
+
