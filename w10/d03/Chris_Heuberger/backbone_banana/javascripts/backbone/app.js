483c0e9c58d140f5c5963f3fe198b61ac526ecec
ChrisBup <christopherheuberger@gmail.com>
2014-06-25 15:20:41 -0400

    create gutenbergPressSQL

---
+++ w10/d03/Chris_Heuberger/backbone_banana/javascripts/backbone/app.js
@@ -0,0 +1,23 @@
+var Bananapp = Bananapp || { Models: {}, Collections: {}, Views: {} };
+
+Bananapp.initialize = function(){
+  // Create a collection
+  // Create a listView
+  // Set up events for the form
+};
+
+var Monkey = Backbone.Model.extend({});
+
+var MonkeyCollection = Backbone.Collection.extend({
+  model: Monkey
+});
+
+var MonkeyView = Backbone.View.extend({
+  model: Monkey
+});
+
+
+
+$(function(){
+  Bananapp.initialize
+})
