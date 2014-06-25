a393ec4209cecc4ee570d2a1c96b0b0492f121e1
ChrisBup <christopherheuberger@gmail.com>
2014-06-25 19:19:28 -0400

    check

--- w10/d03/Chris_Heuberger/backbone_banana/javascripts/backbone/app.js
+++ w10/d03/Chris_Heuberger/backbone_banana/javascripts/backbone/app.js
@@ -1,23 +1,28 @@
 var Bananapp = Bananapp || { Models: {}, Collections: {}, Views: {} };
 
+
+
+
 Bananapp.initialize = function(){
-  // Create a collection
-  // Create a listView
-  // Set up events for the form
+
+  var monkeys = new Bananapp.Collections.MonkeyCollection();
+  var listView = new Bananapp.Views.MonkeyListView({
+    el: $('.monkeys'),
+    collection: monkeys
+  });
+
+  $('form.monkey-form').on('submit', function(e){
+    e.preventDefault();
+    var nameField = $('form.monkey-form input');
+    var newName = nameField.val();
+    nameField.val('');
+    monkeys.add({name: newName});
+  })
+
 };
-
-var Monkey = Backbone.Model.extend({});
-
-var MonkeyCollection = Backbone.Collection.extend({
-  model: Monkey
-});
-
-var MonkeyView = Backbone.View.extend({
-  model: Monkey
-});
-
 
 
 $(function(){
-  Bananapp.initialize
+  Bananapp.initialize();
 })
+
