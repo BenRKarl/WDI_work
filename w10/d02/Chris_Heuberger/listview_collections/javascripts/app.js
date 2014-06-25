ffa6602c268f29ee40bfac612fdeb262f7e6b63c
ChrisBup <christopherheuberger@gmail.com>
2014-06-25 13:43:03 -0400

    homework update

--- w10/d02/Chris_Heuberger/listview_collections/javascripts/app.js
+++ w10/d02/Chris_Heuberger/listview_collections/javascripts/app.js
@@ -1,38 +1,108 @@
-$(function(){
-  var juice = new Juice();
-  strawberry = new Ingredient({name: 'strawberry', amount: 12});
-  juice.on('add', function(){console.log('something added')});
-  juiceCollection.add(strawberry);
-  juiceCollection.on('remove', function(){console.log('removed')});
-  var cabbage = new Ingredient({name: 'cabbage', amount: 1});
-  juice.add(cabbage);
-  juiceListView = new JuiceListView({collection: juice, el: $('#new-juice')});
-  var tumeric = new Ingredient({name: "tumeric", amount: 6});
-  juiceListView.collection.add(tumeric);
-  juice1 = new Juice({name: 'One', juice: juice});
-  juiceView1 = new JuiceView({model: juice1});
-  juices = new JuiceCollection();
-  juicesView = new JuiceListView({collection: juices, el: $('#juices')});
-  juices.add(juice1);
-  juice2 = new Juice({name: 'Two', juice: juice});
-  juices.add(juice2);
+
+var Ingredient = Backbone.Model.extend({});
+
+var IngredientView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template($('#ingredient_template').html()),
+  render: function() {
+    this.$el.html(this.template({ingredient: this.model.toJSON()}));
+    return this;
+  }
 });
 
-var juiceCollection;
-var juiceListView;
-var juice1;
+var Recipe = Backbone.Collection.extend({
+  model: Ingredient
+});
+
+
+var RecipeView = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.collection, 'all', this.render);
+  },
+  tagName: 'ul',
+  render: function(){
+    var that = this;
+    this.$el.empty();
+    _.each(this.collection.models, function(ingredient) {
+      var ingredientView = new IngredientView({model: ingredient});
+      that.$el.append(ingredientView.render().el);
+    });
+    return this;
+  }
+});
+
+var Juice = Backbone.Model.extend({
+  initialize: function() {
+    this.set('recipe', new Recipe());
+  }
+});
+
+var JuiceCollection = Backbone.Collection.extend({
+  model: Juice
+});
+
+var JuiceView = Backbone.View.extend({
+  template: _.template($('#juice_template').html()),
+  render: function() {
+    var that = this;
+    this.$el.html(this.template({juice: this.model.toJSON()}));
+    var currRecipeView = new RecipeView({collection: this.model.get('recipe'), el: this.$el.find('ul')});
+    currRecipeView.render();
+
+    this.$el.find('form').on('submit', function(e) {
+      e.preventDefault();
+      that.model.get('recipe').add({name: that.$el.find('input.ingred').val(),
+                                    amount: that.$el.find('input.amount').val()});
+      that.$el.find('input').val('');
+      that.$el.find('input.amount').val('');
+      return false;
+
+    });
+    return this;
+  },
+  events: {
+    'click button.drink_button': 'drinkJuice'
+  },
+  drinkJuice: function() {
+    this.model.destroy();
+    this.remove();
+  }
+});
+
+
+var JuiceListView = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(this.collection, 'all', this.render);
+  },
+  tagName: 'ul',
+  render: function(){
+    var that = this;
+    this.$el.empty();
+    _.each(this.collection.models, function(juice) {
+      var juiceView = new JuiceView({model: juice});
+      that.$el.append(juiceView.render().el);
+    });
+    return this;
+  }
+});
+
+
 
 $(function(){
   juiceCollection = new JuiceCollection();
-  juiceListView = new JuiceListView({
-    collection: juiceCollection,
-    el: $('.juice-bar')
+  juiceListView = new JuiceListView({collection: juiceCollection,
+                                     el: $('#juices')});
+  $('#juice_button').on('click', function(){
+    juiceCollection.add({name: $('#juice_input').val()});
+    $('#juice_input').val('');
+    return false;
   });
-  $('.new-juice').on('submit', function(e){
-    e.preventDefault();
-    var nameField =  $('.juice-name');
-    var juiceName = nameField.val();
-    nameField.val('')
-    juiceCollection.add({name: juiceName});
-  })
-})
+});
+
+
+
+
+
+
+
+
