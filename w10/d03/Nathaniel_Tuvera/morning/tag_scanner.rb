0c662ca514d246beca809e62da7c9bf83022cc14
ntuvera <ntuvera@gmail.com>
2014-06-25 09:49:17 -0400

    morning exercise

--- w10/d03/Nathaniel_Tuvera/morning/tag_scanner.rb
+++ w10/d03/Nathaniel_Tuvera/morning/tag_scanner.rb
@@ -1,3 +1,10 @@
-def html_tags
+def html_tags(string)
+  tag_regex = (/<[a-zA-z]*[0-9]*>/i)
+  if string.scan(tag_regex)
+    # puts 'there are tags'
+    return string.scan(tag_regex)
+  else
+    puts 'no tags here'
+  end
 
 end
