4e19707438f6cc7920ae8aad9cb3203fdf1b870b
ntuvera <ntuvera@gmail.com>
2014-06-25 09:49:45 -0400

    SAVED morning exercise. duh

--- w10/d03/Nathaniel_Tuvera/morning/tag_scanner.rb
+++ w10/d03/Nathaniel_Tuvera/morning/tag_scanner.rb
@@ -1,10 +1,35 @@
 def html_tags(string)
-  tag_regex = (/<[a-zA-z]*[0-9]*>/i)
+  tag_regex = /<[a-zA-z]*|[0-9]/i
   if string.scan(tag_regex)
-    # puts 'there are tags'
-    return string.scan(tag_regex)
+    return string.scan(tag_regex).each { |ele| ele.gsub!('<','') }
   else
     puts 'no tags here'
   end
+end
 
+require 'httparty'
+
+def html_tags_from_site(url)
+  response = HTTParty.get(url)
+  tag_regex = /<\w+/i
+  if response.scan(tag_regex)
+    return response.scan(tag_regex).each { |ele| ele.gsub!('<','') }
+  else
+    puts 'no tags here'
+  end
+end
+
+
+
+# Matt Solutions
+
+def html_tags(html)
+  html.scan(/<(\w+)/).flatten.uniq
+end
+
+require 'httparty'
+
+def find_tags(url)
+  html = HTTParty.get(url)
+  html_tags(html)
 end
