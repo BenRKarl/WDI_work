53fbb1470f5fa6f39644bd96d3a7d1b467df2992
mntj <mntj11@gmail.com>
2014-06-26 15:19:02 -0400

    gutenberg setup without book files. router calculator created

---
+++ w10/d04/Corey_Leveen/gutenbergPressSQL/public/javascripts/backbone/app.js
@@ -0,0 +1,18 @@
+var GutenbergApp = GutenbergApp || { Models : {}, Views : {}, Collections : {} }
+
+GutenbergApp.initialize = function(){
+  var authors = new GutenbergApp.Collections.AuthorCollection();
+  var authorsListView = new GutenbergApp.Views.AuthorListView({
+    collection : authors,
+    el : $('.author-lounge')
+  });
+
+  authors.fetch({success : function(){
+    authorListView.render();
+  }});
+
+};
+
+$(function(){
+  GutenbergApp.initialize();
+})
