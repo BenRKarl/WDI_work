1c64805d3cbcfe437825d5b770d1664daca170d5
jonathangean <jonathangean@gmail.com>
2014-06-27 09:22:07 -0400

    w10_d03

---
+++ w10/d04/Jonathan_Gean/gutenberg/public/javascripts/backbone/collections/BookCollection.js
@@ -0,0 +1,7 @@
+var GutenbergApp = GutenbergApp || { Models: {}, Views: {}, Collections: {} };
+
+GutenbergApp.Collections.BookCollection = Backbone.Collection.extend({
+  model: GutenbergApp.Models.Book
+  // url: '/authors/:author_id/books'
+
+});
--- w10/d04/Jonathan_Gean/gutenberg/public/javascripts/backbone/views/authors/AuthorView.js
+++ w10/d04/Jonathan_Gean/gutenberg/public/javascripts/backbone/views/authors/AuthorView.js
@@ -4,8 +4,7 @@ GutenbergApp.Views.AuthorView = Backbone.View.extend({
   template: _.template($('#author-template').html()),
   tagName: 'li',
   render: function(){
-    this.$el.html( this.template( this.model.attributes ) );
-    debugger;
+    this.$el.html( this.template( this.model.attributes));
     var bookListView = new GutenbergApp.Views.BookListView({
       collection: this.model.get('books'),
       el: this.$el.find('.books')
