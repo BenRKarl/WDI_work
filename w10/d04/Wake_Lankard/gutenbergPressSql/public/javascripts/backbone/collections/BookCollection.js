2a2fa9fdb264bb8b6dce986593bcc2361d573627
wakelank <wakelank@gmail.com>
2014-06-26 23:34:40 -0400

    this as just as far as I got

--- w10/d04/Wake_Lankard/gutenbergPressSql/public/javascripts/backbone/collections/BookCollection.js
+++ w10/d04/Wake_Lankard/gutenbergPressSql/public/javascripts/backbone/collections/BookCollection.js
@@ -2,5 +2,5 @@ var GutenbergApp = GutenbergApp || { Models: {}, Views: {}, Collections: {} };
 
 GutenbergApp.Collections.BookCollection = Backbone.Collection.extend({
  model: GutenbergApp.Models.Book
- url: '/books'
+
 });
