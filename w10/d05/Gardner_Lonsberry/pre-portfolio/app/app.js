98d4b2c3787d341c45cb6a92f1400f72fad30818
glonsberry <gardnerlonsberry@gmail.com>
2014-06-30 08:20:20 -0400

    cleaning up wdi repo

---
+++ w10/d05/Gardner_Lonsberry/pre-portfolio/app/app.js
@@ -0,0 +1,25 @@
+$(function() {
+    var screenIndex = 1,
+    numScreens = $('.screen').length,
+    isTransitioning = false,
+    transitionDur = 1000;
+}
+var screenIndex = 1,
+    numScreens = $('.screen').length,
+    isTransitioning = false,
+    transitionDur = 1000,
+    BV,
+    isTouch = Modernizr.touch;
+var screenIndex = 1,
+    numScreens = $('.screen').length,
+    isTransitioning = false,
+    transitionDur = 1000,
+    BV,
+    isTouch = Modernizr.touch;
+
+$bigImage
+    .css('position','relative')
+    .imagesLoaded(adjustImagePositioning);
+// and on window resize
+$(window).on('resize', adjustImagePositioning);
+)
