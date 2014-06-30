db0cdb49e07223184e02f1058c1f1be83ab65a70
mattmarcello <matt.marcello@gmail.com>
2014-06-30 17:26:07 -0400

    update some file

--- w11/d01/INSTRUCTORS/node_bananas/monkey.js
+++ w11/d01/INSTRUCTORS/node_bananas/monkey.js
@@ -5,4 +5,7 @@ var MonkeySchema = new Schema({
 });
 // db.model('Monkey', MonkeySchema) is what is
 // returned when i require this file, 'monkey.js'
+// It is an interface for dealing with our models.
+// It allows us to find records, create records,
+// and so on and so forth.
 module.exports = db.model('Monkey', MonkeySchema);
