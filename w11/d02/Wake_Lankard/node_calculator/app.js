1af8f46561ba68f07f3971257a1422c809c33483
wakelank <wakelank@gmail.com>
2014-07-01 10:11:41 -0400

    morning work

---
+++ w11/d02/Wake_Lankard/node_calculator/app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+global.path = require('path');
+
+var app = express();
+
+app.use(bodyParser());
+app.use(methodOverride());
+// app.use(express.static(path.join(__dirname, 'public')));
+calculator = {}
+calculator = function(op,x,y){
+  switch (op){
+    case "add":
+      return (x + y).toString();
+      break;
+    case "subtract":
+      return (x - y).toString();
+      break;
+    default:
+      return "illegal operator"
+  }
+}
+
+
+app.get('/:op/:x/:y', function(req, res){
+  var x = parseInt(req.params.x);
+  var y = parseInt(req.params.y);
+  var op = req.params.op;
+  console.log("add: " + op);
+  var result = calculator(op,x,y);
+  res.send(result);
+  console.log("result= " + result);
+});
+
+
+app.listen(8000, function(){
+  console.log('app is listening on port 8000');
+})
