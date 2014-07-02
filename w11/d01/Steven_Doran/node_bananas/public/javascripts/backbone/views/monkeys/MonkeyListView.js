b3062d647dea4f4eea6c0bfd653587307b3ceff2
pantsonsteven <pantsonsteven@gmail.com>
2014-07-02 09:01:58 -0400

    morning

--- w11/d01/Steven_Doran/node_bananas/public/javascripts/backbone/views/monkeys/MonkeyListView.js
+++ w11/d01/Steven_Doran/node_bananas/public/javascripts/backbone/views/monkeys/MonkeyListView.js
@@ -6,7 +6,7 @@ Bananapp.Views.MonkeyListView =  Backbone.View.extend({
   },
   render: function(){
     var that = this;
-    this.$el.empty()
+    this.$el.empty();
     _.each(this.collection.models, function(monkey){
       var monkView = new Bananapp.Views.MonkeyView({model: monkey});
       that.$el.prepend(monkView.render().el);
