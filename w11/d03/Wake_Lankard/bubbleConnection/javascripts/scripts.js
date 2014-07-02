37756b2016272d344d82dadfe3716c35501d38d9
wakelank <wakelank@gmail.com>
2014-07-02 15:44:37 -0400

    finishes the daily work

--- w11/d03/Wake_Lankard/bubbleConnection/javascripts/scripts.js
+++ w11/d03/Wake_Lankard/bubbleConnection/javascripts/scripts.js
@@ -1,4 +1,60 @@
-function projectData(data){
+// ************************************
+// *********** Global Stuff ***********
+// ************************************
+
+// -- global Audio Vars --
+var audioContext;
+var arraybuffer;
+var fftObject;
+var audioSource;
+var samples = 1024;
+
+// -- global var for svg --
+var svg;
+
+
+
+
+// ************************************
+// ********* Audio Handling ***********
+// ************************************
+
+function loadFile(mp3file) {
+    var reqest = new XMLHttpRequest();
+    reqest.open("GET", mp3file,true);
+    reqest.responseType = "arraybuffer";
+    reqest.onload = function() {
+      audioContext.decodeAudioData(reqest.response, function(buffer) {
+        audioBuffer = buffer;
+        setAudioHandlers();
+      });
+    };
+    reqest.send();
+}
+
+
+function setAudioHandlers() {
+
+    var audioSource = audioContext.createBufferSource();
+    audioSource.buffer = audioBuffer;
+    fftObject = audioContext.createAnalyser();
+    fftObject.fftSize = samples;
+    audioSource.connect(fftObject);
+    fftObject.connect(audioContext.destination);
+    audioSource.noteOn(0); // Drop the needle (play audio)
+
+    var data = new Uint8Array(samples);
+
+    setInterval(function(){
+      fftObject.getByteFrequencyData(data);
+      d3Project(data)
+    }, 10) // repeat rendering project
+}
+
+
+
+
+function d3Project(data){
 
   //sample
   Array.prototype.sample = function(){
@@ -41,13 +97,28 @@ function projectData(data){
 
 }
 
-window.onload = function(){
-  d3.select('svg')
-        .attr('width', '100%')
-        .attr('height', '75%')
-        .style('border', '1px solid black');
-  setInterval(function(){
-    projectData([50,100,150,10,20,44,66])
-  }, 500)
+// window.onload = function(){
+//   d3.select('svg')
+//         .attr('width', '100%')
+//         .attr('height', '75%')
+//         .style('border', '1px solid black');
+//   setInterval(function(){
+//     projectData([50,100,150,10,20,44,66])
+//   }, 500)
   
+// }
+
+// ************************************
+// **** Let's get it started... Ha ****
+// ************************************
+
+window.onload = function(){
+
+  svg = d3.select('body')
+          .append('svg')
+            .attr('width', '900px')
+            .attr('height', '500px');
+
+  audioContext = new webkitAudioContext();
+  loadFile('audio/gd710218ii_08_St_Stephen.mp3');
 }
--- w11/d03/Wake_Lankard/d3camp/javascripts/scripts.js
+++ w11/d03/Wake_Lankard/d3camp/javascripts/scripts.js
@@ -1,5 +1,23 @@
 function projectData(data){
 
-  var d3Body = d3.select('.campsite');
-  
+  // Select entire campsite
+  var campsite = d3.select('.campsite');
+
+  // build tents
+  var tents = campsite.selectAll('li')
+                        .data(data)
+                        .enter()
+                        .append('li');
+
+  // Fill tent
+
+  var tents = campsite.selectAll('li')
+                        .data(data)
+                        .text(function(d){
+                          return d + ' sleeps here'
+                        });
+
+    tents.exit().remove();
+
+
 }
