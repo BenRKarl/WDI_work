1baf3245f426917a5a9a701d6c3cdce3b909a97d
wakelank <wakelank@gmail.com>
2014-07-02 22:08:02 -0400

    bug fixes

--- w11/d03/Wake_Lankard/d3planets/node_server.js
+++ w11/d03/Wake_Lankard/d3planets/node_server.js
@@ -1,22 +1,20 @@
 var express   = require('express');
 var request   = require('request');
-var path      =require('path');
-//var methodOverride = require('method-override');
+var path      = require('path');
 
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(methodOverride);
 
 app.get('/planets' , function(req,res) {
   request('http://exoapi.com/api/skyhook/planets/all', function(err, response, body){
     if (!err && response.statusCode == 200){
       res.send(body);
-    }
+    };
   });
 });
 
-  app.listen(8000, function(){
-    console.log('running on 8000');
-  });
+app.listen(8000, function(){
+  console.log('running on 8000');
+});
 
--- w11/d03/Wake_Lankard/d3planets/public/javascripts/scripts.js
+++ w11/d03/Wake_Lankard/d3planets/public/javascripts/scripts.js
@@ -1,19 +1,3 @@
-function analyzeData(data){
-  console.log(data)
-  var planetsArr = data.response.results;
-  for (var i = 0; i < planetsArr.length; ++i){
-    var mass = planetsArr[i].mass;
-    var distance = planetsArr[i].mean_distance;
-    var name = planetsArr[i].name;
-    var discovery_year = planetsArr[i].disc_year;
-    var elem = $('<li>').html(name + ': ' + mass + " " + distance + " " + discovery_year);
-    debugger;
-    $('.planets-list').append(elem);
-  } 
-}
-
-//window.xAxisScale = d3.scale.linear().domain([maxGoalsAgainst+2 ,0] ).range([0, width]);
-
 function d3Project(data){
   var maxDistance = d3.max(data, function(planet){
     return planet.mean_distance;
@@ -22,26 +6,24 @@ function d3Project(data){
     return planet.mass;
   });
   var relxPos = d3.scale.linear().domain([0, maxDistance/50] ).range([0, 100]);
-  var relMass = d3.scale.linear().domain([1, maxMass]).range([1,20])
+  var relMass = d3.scale.linear().domain([1, maxMass]).range([1,20]);
 
   var svg = d3.select('svg');
 
   var circles = svg.selectAll('circle')
                         .data(data)
                         .enter()
-                        .append('circle');
-                          
-  var circles = svg.selectAll('circle')
-                        .data(data)
+                        .append('circle')
                         .attr('r',  function(d){ return relMass(d.mass) + 'px'})
                         .attr('cy', function(){ return (Math.random() * 100) + '%' })
                         .attr('cx', function(d){return relxPos(d.mean_distance) + '%'})
                         .css('background-color', '#689E92');
+                          
+
 }
 
 $(function(){
-  console.log('this works');
-  $('.body').html('loading...')
+  $('.body').html('loading...');
   $.ajax({
     url: '/planets',
     method:'get',
@@ -51,9 +33,10 @@ $(function(){
         .attr('width', '100%')
         .attr('height', '75%')
         .style('border', '1px solid black');
-  setInterval(function(){
-    d3Project(data.response.results)
-  }, 500)
+        var planetsArr = data.response.results;
+        d3Project(planetsArr);
+
+
     }
-  })
-})
+  });
+});
