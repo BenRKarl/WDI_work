ffab0bb9d2352f5a59ddf99ecbf5a655c75347c5
pantsonsteven <pantsonsteven@gmail.com>
2014-07-07 16:52:45 -0400

    afternoon

---
+++ w12/d01/Steven_Doran/factorial.rb
@@ -0,0 +1,14 @@
+4*3*2*1
+
+def factorial_recursive(n)
+  return n if n <= 2
+  return n * factorial_recursive(n-1)
+end
+
+def factorial_iterative(n)
+  product = 1
+  n.downto(2) do |x|
+    product *= x
+  end
+  product 
+end
