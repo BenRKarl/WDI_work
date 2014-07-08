05ad2f1c92e874e1dfaf6123edb4ce07f7f17b33
wakelank <wakelank@gmail.com>
2014-07-07 20:14:51 -0400

    Completes sll.rb

--- w12/d01/Wake_Lankard/sll.rb
+++ w12/d01/Wake_Lankard/sll.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'pry-nav'
+
 class Node
   attr_accessor :value, :next_node
 
@@ -21,26 +24,81 @@ class SinglyLinkedList
 
   def prepend(value)
     #prepend to the beginning
+    value = Node.new(value)
+    value.next_node = @head
+    @head = value
   end
 
   def append(value)
     # append to the end
+    value = Node.new(value)
+    last = @head
+    while last.next_node != nil
+      last = last.next_node
+    end
+    last.next_node = value
+  
+  
+
   end
 
   def remove
     # removes the first node
+    temp = @head
+    @head = @head.next_node
   end
 
   def last
     # get's the last node
+    last = @head
+    while last.next_node != nil
+      last = last.next_node
+    end
+    last
+
   end
 
   def length
     # calculates the length of the list
+    length = 1
+    node = @head
+    while node != last
+      node = node.next_node
+      length += 1
+    end
+    length
   end
 
   def find(input)
     # returns the node with that value or nil if none found
+    node = @head
+    while node != last
+      node = node.next_node
+      return node if node.value == input
+    end
+
   end
 
+  def to_s
+    node = @head
+    puts node
+    while node.next_node != nil
+      node = node.next_node
+      puts node
+    end
+
+  end
+
+
 end
+
+slist = SinglyLinkedList.new("one")
+slist.prepend("two")
+slist.prepend("three")
+slist.append("four")
+slist.remove
+puts "last: " + slist.last.to_s
+puts "len: " +slist.length.to_s
+puts "find four: "+ slist.find("four").to_s
+
+puts slist
